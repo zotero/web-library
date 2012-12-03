862edd96c3fe16c0a58ad0e478089d6e6988efdd
Faolan C-P <fcheslack@gmail.com>
2012-12-03 12:21:21 -0500

    add empty config file

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+$libraryType = 'user'; //user or group
+$libraryID = '<user or group id>';
+$librarySlug = '<user or group slug>';
+$apiKey = '<apiKey with some amount of permissions for library>';
+$displayName = "<user display name>";
+
