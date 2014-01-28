9dd0d85dcfbcbd1a7914c177ff2e32fcd5b25005
Faolan C-P <fcheslack@gmail.com>
2014-01-28 17:19:12 -0500

    make searchbox its own widget instead of controlpanel
    probably necessary to split it up for mobile, and no reason they need
    to depend on eachother

--- static/js/libraryUi/widgets/controlpanel.js
+++ static/js/libraryUi/widgets/controlpanel.js
@@ -12,55 +12,8 @@ Zotero.ui.widgets.controlPanel.init = function(el){
     library.listen("moveToTrash", Zotero.ui.widgets.controlPanel.moveToTrash), {widgetEl:el};
     library.listen("removeFromTrash", Zotero.ui.widgets.controlPanel.removeFromTrash, {widgetEl:el});
     library.listen("toggleEdit", Zotero.ui.widgets.controlPanel.toggleEdit, {widgetEl:el});
-    library.listen('clearLibraryQuery', Zotero.ui.widgets.controlPanel.clearLibraryQuery, {widgetEl:el});
     
     var container = J(el);
-    //set initial state of search input to url value
-    if(Zotero.state.getUrlVar('q')){
-        container.find("#header-search-query").val(Zotero.state.getUrlVar('q'));
-    }
-    
-    //clear libary query param when field cleared
-    var context = 'support';
-    if(undefined !== window.zoterojsSearchContext){
-        context = zoterojsSearchContext;
-    }
-    
-    //set up search type links
-    container.on('click', ".library-search-type-link", function(e){
-        e.preventDefault();
-        var typeLinks = J(".library-search-type-link").removeClass('selected');
-        var selected = J(e.target);
-        var selectedType = selected.data('searchtype');
-        var searchInput = J("#header-search-query").data('searchtype', selectedType);
-        selected.addClass('selected');
-        if(selectedType == 'simple'){
-            searchInput.attr('placeholder', 'Search Title, Creator, Year');
-        }
-        else if(selectedType == 'everything'){
-            searchInput.attr('placeholder', 'Search Full Text');
-        }
-    });
-    
-    //set up search submit for library
-    container.on('submit', "#library-search", function(e){
-        e.preventDefault();
-        Zotero.state.clearUrlVars(['collectionKey', 'tag', 'q', 'qmode']);
-        var query     = J("#header-search-query").val();
-        var searchType = J("#header-search-query").data('searchtype');
-        if(query !== "" || Zotero.state.getUrlVar('q') ){
-            Zotero.state.pathVars['q'] = query;
-            if(searchType != "simple"){
-                Zotero.state.pathVars['qmode'] = searchType;
-            }
-            Zotero.state.pushState();
-        }
-        return false;
-    });
-    
-    container.on('click', '.clear-field-button', function(e){
-        J("#header-search-query").val("").focus();
-    });
     
     Zotero.ui.widgets.controlPanel.updateDisabledControlButtons();
 };
@@ -78,15 +31,6 @@ Zotero.ui.widgets.controlPanel.selectedItemsChanged = function(evt){
         
     Zotero.ui.widgets.controlPanel.updateDisabledControlButtons(selectedItemKeys);
 };
-
-Zotero.ui.widgets.controlPanel.clearLibraryQuery = function(){
-    Zotero.state.unsetUrlVar('q');
-    Zotero.state.unsetUrlVar('qmode');
-    
-    J("#header-search-query").val("");
-    Zotero.state.pushState();
-    return;
-}
 
 /**
  * Update the disabled state of library control toolbar buttons depending on context
