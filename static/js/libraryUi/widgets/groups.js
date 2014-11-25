e0223b7e7db59709b89761fb0447691a129d5d01
Faolan C-P <fcheslack@gmail.com>
2014-11-25 12:50:03 -0500

    update groups widget from www

--- static/js/libraryUi/widgets/groups.js
+++ static/js/libraryUi/widgets/groups.js
@@ -6,7 +6,7 @@ Zotero.ui.widgets.groups.init = function(el){
     var groups = new Zotero.Groups();
     if(Zotero.config.loggedIn && Zotero.config.loggedInUserID){
         Zotero.ui.showSpinner(J(el), 'horizontal');
-        var groupsPromise = groups.fetchUserGroups(Zotero.config.loggedInUserID, Zotero.config.apiKey)
+        var groupsPromise = groups.fetchUserGroups(Zotero.config.loggedInUserID)
         .then(function(response){
             var groups = response.fetchedGroups;
             Zotero.ui.widgets.groups.displayGroupNuggets(el, groups);
@@ -24,37 +24,39 @@ Zotero.ui.widgets.groups.userGroupsDisplay = function(groups){
 
 Zotero.ui.widgets.groups.displayGroupNuggets = function(el, groups){
     Z.debug("Zotero.ui.widgets.groups.displayGroupNuggets", 3);
-    Z.debug(groups);
     var jel = J(el);
     jel.empty();
-    J.each(groups, function(ind, group){
-        Z.debug("Displaying group nugget");
-        var userID = Zotero.config.loggedInUserID;
-        var groupManageable = false;
-        var memberCount = 1;
-        if(group.apiObj.data.members) {
-            memberCount += group.apiObj.data.members.length;
-        }
-        if(group.apiObj.data.admins){
-            memberCount += group.apiObj.data.admins.length;
-        }
-        
-        if(userID && (userID == group.apiObj.data.owner || (J.inArray(userID, group.apiObj.data.admins) != -1 ))) {
-            groupManageable = true;
-        }
-        //Z.debug("manageable: " + groupManageable);
-        
-        var tdata = {
-            group:group,
-            groupViewUrl:Zotero.url.groupViewUrl(group),
-            groupLibraryUrl:Zotero.url.groupLibraryUrl(group),
-            groupSettingsUrl:Zotero.url.groupSettingsUrl(group),
-            groupLibrarySettingsUrl:Zotero.url.groupLibrarySettingsUrl(group),
-            groupMemberSettingsUrl: Zotero.url.groupMemberSettingsUrl(group),
-            memberCount:memberCount,
-            groupManageable: groupManageable
-        };
-        jel.append( J('#groupnuggetTemplate').render(tdata) );
-    });
+    if(groups.length == 0){
+        jel.append("<p>You are not currently a member of any groups.</p>");
+        J("#groups-blurb").removeClass('hidden');
+    } else {
+        J.each(groups, function(ind, group){
+            var userID = Zotero.config.loggedInUserID;
+            var groupManageable = false;
+            var memberCount = 1;
+            if(group.apiObj.data.members) {
+                memberCount += group.apiObj.data.members.length;
+            }
+            if(group.apiObj.data.admins){
+                memberCount += group.apiObj.data.admins.length;
+            }
+            
+            if(userID && (userID == group.apiObj.data.owner || (J.inArray(userID, group.apiObj.data.admins) != -1 ))) {
+                groupManageable = true;
+            }
+            
+            var tdata = {
+                group:group,
+                groupViewUrl:Zotero.url.groupViewUrl(group),
+                groupLibraryUrl:Zotero.url.groupLibraryUrl(group),
+                groupSettingsUrl:Zotero.url.groupSettingsUrl(group),
+                groupLibrarySettingsUrl:Zotero.url.groupLibrarySettingsUrl(group),
+                groupMemberSettingsUrl: Zotero.url.groupMemberSettingsUrl(group),
+                memberCount:memberCount,
+                groupManageable: groupManageable
+            };
+            jel.append( J('#groupnuggetTemplate').render(tdata) );
+        });
+    }
 };
 
