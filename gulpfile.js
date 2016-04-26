1543ad7f590bb6eedbfe828e5c5c40a21d81cc67
Faolan C-P <fcheslack@gmail.com>
2016-04-26 17:23:42 -0400

    allow both watchify and regular gulp builds
    gulp default builds js and less
    gulp js watches and does updating builds

--- gulpfile.js
+++ gulpfile.js
@@ -20,22 +20,26 @@ var customOpts = {
 	debug: true
 };
 var opts = assign({}, watchify.args, customOpts);
-var b = watchify(browserify(opts)); 
+var wb = watchify(browserify(opts)); 
+var bb = browserify(opts);
 
-b.transform('babelify', {
+var babelifyOpts = {
 	presets: ['es2015', 'react'],
 	plugins: ['transform-flow-strip-types']
-});
+};
+
+wb.transform('babelify', babelifyOpts);
+bb.transform('babelify', babelifyOpts);
 
 // add transformations here
 // i.e. b.transform(coffeeify);
 
-gulp.task('js', bundle); // so you can run `gulp js` to build the file
-b.on('update', bundle); // on any dep update, runs the bundler
-b.on('log', gutil.log); // output build logs to terminal
+gulp.task('js', wbundle); // so you can run `gulp js` to build the file
+wb.on('update', wbundle); // on any dep update, runs the bundler
+wb.on('log', gutil.log); // output build logs to terminal
 
-function bundle() {
-	return b.bundle()
+function wbundle() {
+	return wb.bundle()
 		// log errors if they happen
 		.on('error', gutil.log.bind(gutil, 'Browserify Error'))
 		.pipe(source('zotero-web-library.js'))
@@ -55,91 +59,25 @@ function bundle() {
 }
 
 
-gulp.task('less', function() {
-	gulp.src('./static/css/zotero-web-library.less')
-	.pipe(less())
-	.pipe(autoprefixer({
-		browsers: ['last 2 versions']
-	}))
-	.pipe(gulp.dest('./static/css/'));
-});
-
-
-
-
-
-
-
-
-/*
-const gulp = require('gulp');
-const sourcemaps = require('gulp-sourcemaps');
-const babel = require('gulp-babel');
-const concat = require('gulp-concat');
-const replace = require('gulp-replace-task');
-const rename = require('gulp-rename');
-const uglify = require('gulp-uglify');
-const plumber = require('gulp-plumber');
-const filter = require('gulp-filter');
-const autoprefixer = require('gulp-autoprefixer');
-const less = require('gulp-less');
-
-const sources = [
-	"library/libZoteroJS/build/libzoterojs.js",
-	"src/_zoterowwwInit.js",
-	"src/State.js",
-	"src/Delay.js",
-	"src/libraryUi/eventful.js",
-	"src/libraryUi/format.js",
-	"src/libraryUi/init.js",
-	"src/libraryUi/misc.js",
-	"src/libraryUi/readstate.js",
-	"src/libraryUi/render.js",
-	"src/libraryUi/updatestate.js",
-	"src/libraryUi/bootstrapdialogs.js",
-	"src/libraryUi/widgets/citeitemdialog.js",
-	"src/libraryUi/widgets/feedlink.js",
-	"src/libraryUi/widgets/groups.js",
-	"src/libraryUi/widgets/groupsList.js",
-	"src/libraryUi/widgets/inviteToGroup.js",
-	"src/libraryUi/widgets/recentItems.js",
-	"src/libraryUi/widgets/itemContainer.js",
-	"src/libraryUi/widgets/librarysettingsdialog.js",
-	"src/libraryUi/widgets/libraryPreloader.js",
-	"src/libraryUi/widgets/progressModal.js",
-	"src/libraryUi/widgets/panelContainer.js",
-	"src/libraryUi/widgets/chooseSortingDialog.js",
-	"src/libraryUi/widgets/imagePreview.js",
-	"src/libraryUi/widgets/imageGrabber.js",
-	"src/libraryUi/widgets/localItems.js",
-	"src/libraryUi/widgets/siteSearch.js",
-	"src/libraryUi/widgets/publications.js",
-	"node_modules/floatthead/dist/jquery.floatThead.js",
-	"src/libraryUi/widgets/reactsrc/*",
-	"src/_zoteroLibraryUrl.js",
-	"src/zoteroPages.js",
-];
-
-function onError(err) {
-	console.warn(err);
-}
-
-gulp.task('build', function() {
-	return gulp.src(sources)
-		.pipe(plumber({errorHandler: onError}))
-		.pipe(sourcemaps.init({loadMaps: true}))
-		.pipe(babel({
-			presets:  ['es2015', 'react']
-		}))
-		.pipe(concat('zotero-web-library.js'))
-		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest('build'))
+function bbundle() {
+	return bb.bundle()
+		// log errors if they happen
+		.on('error', gutil.log.bind(gutil, 'Browserify Error'))
+		.pipe(source('zotero-web-library.js'))
+		// optional, remove if you don't need to buffer file contents
+		.pipe(buffer())
+		// optional, remove if you dont want sourcemaps
+		.pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
+		// Add transformation tasks to the pipeline here.
+		.pipe(sourcemaps.write('./')) // writes .map file
+		.pipe(gulp.dest('./build'))
+		// repeat for minified version
 		.pipe(filter('*.js'))
 		.pipe(uglify())
 		.pipe(rename({ extname: '.min.js' }))
 		// .pipe(sourcemaps.write('.'))
 		.pipe(gulp.dest('build'));
-});
+}
 
 gulp.task('less', function() {
 	gulp.src('./static/css/zotero-web-library.less')
@@ -150,5 +88,6 @@ gulp.task('less', function() {
 	.pipe(gulp.dest('./static/css/'));
 });
 
+gulp.task('build', bbundle);
+
 gulp.task('default', ['less', 'build']);
-*/
