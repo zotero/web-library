8aee22fff4ea7e9efc8b89b8388ac6e4488e7124
Faolan C-P <fcheslack@gmail.com>
2016-07-13 18:05:41 -0400

    update static library example

--- src/js/libraryUi/StaticComponents/LibraryContainer.js
+++ src/js/libraryUi/StaticComponents/LibraryContainer.js
@@ -3,11 +3,23 @@
 var log = require('libzotero/lib/Log').Logger('zotero-web-library:LibraryContainer');
 
 var React = require('react');
+var ReactDOM = require('react-dom');
+
 var TitleBar = require('./TitleBar.js');
 var CollectionTree = require('./CollectionTree.js');
 var TagSelector = require('./TagSelector.js');
 var ItemList = require('./ItemList.js');
 var ItemDetails = require('./ItemDetails.js');
+
+Zotero.ui.widgets.StaticLibrary = {};
+
+Zotero.ui.widgets.StaticLibrary.init = function(el){
+	log.debug('StaticLibrary.init');
+	ReactDOM.render(
+		<LibraryContainer />,
+		document.getElementById('staticlibrary')
+	);
+};
 
 var LibraryContainer = React.createClass({
 	getDefaultProps: function() {
