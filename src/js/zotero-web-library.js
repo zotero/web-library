c2530b35f721a68eb5ad90d812c2d4d20afcb91a
Faolan C-P <fcheslack@gmail.com>
2016-07-20 13:39:51 -0400

    make compatible enough with node for tests to run
    - Set up testing with mocha/chai (npm test)
    - Add react preset, and transform-object-rest-spread plugin (also used in libZotero) to .babelrc
    - Short circuit BrowserDetect with placeholder values in node
    - Stop using deepExtend. The version we had broke on arrays, and only jQuery seems to have a robust implementation. It wasn't actually necessary where we were using it, and we should rely on only the Object.assign compatible implementation of extend going forward.
    - Attach React and ReactDOM to globalScope rather than window
    - Don't load up browser dependent components when in node (floatthead, bootstrap-sass, $.ready)

--- src/js/zotero-web-library.js
+++ src/js/zotero-web-library.js
@@ -2,6 +2,18 @@
 
 var log = require('libzotero/lib/Log').Logger('zotero-web-library:zotero-web-library');
 
+if(typeof window === 'undefined') {
+	var globalScope = global;
+	if(!globalScope.XMLHttpRequest) {
+		globalScope.XMLHttpRequest = require('w3c-xmlhttprequest').XMLHttpRequest;
+	}
+} else {
+	var globalScope = window;
+	if(typeof Promise === 'undefined') {
+		require('es6-promise').polyfill();
+	}
+}
+
 var Zotero = require('libzotero');
 
 Zotero.State = require('./State.js');
@@ -29,26 +41,33 @@ if (typeof jQuery === 'undefined' && typeof $ === 'undefined') {
 	var jQuery = $;
 }
 
-window.$ = window.jQuery = window.J = jQuery || $;
-require('floatthead');
-require('bootstrap-sass');
+globalScope.$ = globalScope.jQuery = globalScope.J = jQuery || $;
+try{
+	require('floatthead');
+	require('bootstrap-sass');
+} catch(e){
+	log.error('error initializing floatthead and bootstrap-sass');
+	//log.debug(e);
+}
 
-window.React = require('react');
-window.ReactDOM = require('react-dom');
+globalScope.React = require('react');
+globalScope.ReactDOM = require('react-dom');
 
-jQuery(document).ready(function() {
-	/* The Zotero web library is built on top of libZotero as a group of
-	 * relatively independent widgets. They interact by listening to and
-	 * triggering events (with optional filters) on the Zotero object or
-	 * individual Zotero.Library objects. State is maintained by a
-	 * Zotero.State object that optionally stores variables in the url
-	 * using pushState as well. With pushState enabled back/forward
-	 * actions trigger events for the variables that have changed so
-	 * widgets listening know to update.
-	 */
-	log.debug('===== DOM READY =====', 3);
-	Zotero.state = new Zotero.State();
-	Zotero.init();
-});
+if(typeof window !== 'undefined') {
+	jQuery(document).ready(function() {
+		/* The Zotero web library is built on top of libZotero as a group of
+		 * relatively independent widgets. They interact by listening to and
+		 * triggering events (with optional filters) on the Zotero object or
+		 * individual Zotero.Library objects. State is maintained by a
+		 * Zotero.State object that optionally stores variables in the url
+		 * using pushState as well. With pushState enabled back/forward
+		 * actions trigger events for the variables that have changed so
+		 * widgets listening know to update.
+		 */
+		log.debug('===== DOM READY =====', 3);
+		Zotero.state = new Zotero.State();
+		Zotero.init();
+	});
+}
 
 module.exports = Zotero;
