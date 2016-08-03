8f1302ff11f575b350d5c4707ff17b641a19b8f7
Johannes Krtek <johannes.krtek@flachware.com>
2016-08-03 12:53:13 +0200

    Add collection tree markup.

--- src/js/libraryUi/StaticComponents/CollectionTree.js
+++ src/js/libraryUi/StaticComponents/CollectionTree.js
@@ -15,6 +15,23 @@ var CollectionTree = React.createClass({
 		return (
 			<nav id="collection-tree">
 				<h3 className="offscreen">Library navigation</h3>
+				<section>
+					<h4>My library</h4>
+					<ul>
+						<li className="active"><a href="#">All documents</a></li>
+						<li><a href="#">Collection 1</a></li>
+						<li><a href="#">Collection 2</a></li>
+						<li><a href="#">Collection 3</a></li>
+					</ul>
+				</section>
+				<section>
+					<h4>Group libraries</h4>
+					<ul>
+						<li><a href="#">Group library 1</a></li>
+						<li><a href="#">Group library 2</a></li>
+						<li><a href="#">Group library 3</a></li>
+					</ul>
+				</section>
 			</nav>
 		);
 	}
