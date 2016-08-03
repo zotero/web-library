8d7b595d967e52264ab6db64984e3f85b00c53b6
Johannes Krtek <johannes.krtek@flachware.com>
2016-08-03 15:35:39 +0200

    Add some collection tree levels.

--- src/js/libraryUi/StaticComponents/CollectionTree.js
+++ src/js/libraryUi/StaticComponents/CollectionTree.js
@@ -17,16 +17,34 @@ var CollectionTree = React.createClass({
 				<h3 className="offscreen">Library navigation</h3>
 				<section>
 					<h4>My library</h4>
-					<ul>
-						<li className="active"><a href="#">All documents</a></li>
-						<li><a href="#">Collection 1</a></li>
-						<li><a href="#">Collection 2</a></li>
-						<li><a href="#">Collection 3</a></li>
+					<ul className="nav">
+						<li><a href="#">All documents</a></li>
+						<li className="open"><a href="#">Level 1</a>
+							<section>
+								<h5 className="hidden-mouse-md-up">Level 2 title</h5>
+								<ul className="nav">
+									<li class="open"><a href="#">Level 2</a>
+										<section>
+											<h6 className="hidden-mouse-md-up">Level 3 title</h6>
+											<ul className="nav">
+												<li className="active"><a href="#">Level 3</a></li>
+												<li><a href="#">Level 3</a></li>
+												<li><a href="#">Level 3</a></li>
+											</ul>
+										</section>
+									</li>
+									<li><a href="#">Level 2</a></li>
+									<li><a href="#">Level 2</a></li>
+								</ul>
+							</section>
+						</li>
+						<li><a href="#">Level 1</a></li>
+						<li><a href="#">Level 1</a></li>
 					</ul>
 				</section>
 				<section>
 					<h4>Group libraries</h4>
-					<ul>
+					<ul className="nav">
 						<li><a href="#">Group library 1</a></li>
 						<li><a href="#">Group library 2</a></li>
 						<li><a href="#">Group library 3</a></li>
