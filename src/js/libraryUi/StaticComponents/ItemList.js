c36963d513f97eb660e85655a7f10ba63613aa90
Johannes Krtek <johannes.krtek@flachware.com>
2016-09-14 11:35:32 +0200

    Add note and attachment columns. Set default column widths.

--- src/js/libraryUi/StaticComponents/ItemList.js
+++ src/js/libraryUi/StaticComponents/ItemList.js
@@ -19,36 +19,48 @@ var ItemList = React.createClass({
 					<div className="metadata">Monteiro</div>
 					<div className="metadata">2012</div>
 					<div className="metadata">20/04/2016 12:54</div>
+					<div className="metadata"></div>
+					<div className="metadata"></div>
 				</li>
 				<li className="item">
 					<div className="metadata">HTML5 for Web Designers</div>
 					<div className="metadata">Keith and Andrew</div>
 					<div className="metadata">2016</div>
 					<div className="metadata">19/04/2016 10:36</div>
+					<div className="metadata"></div>
+					<div className="metadata"></div>
 				</li>
 				<li className="item">
 					<div className="metadata">Managing Complex Augmented Reality Models</div>
 					<div className="metadata">Schmalstieg et al.</div>
 					<div className="metadata">2007</div>
 					<div className="metadata">19/04/2016 10:38</div>
+					<div className="metadata"></div>
+					<div className="metadata"></div>
 				</li>
 				<li className="item">
 					<div className="metadata">Mobile First</div>
 					<div className="metadata">Wroblewski</div>
 					<div className="metadata">2012</div>
 					<div className="metadata">19/04/2016 10:44</div>
+					<div className="metadata"></div>
+					<div className="metadata"></div>
 				</li>
 				<li className="item">
 					<div className="metadata">Responsive Web Design</div>
 					<div className="metadata">Marcott</div>
 					<div className="metadata">2010</div>
 					<div className="metadata">20/04/2016 16:05</div>
+					<div className="metadata">2</div>
+					<div className="metadata">2</div>
 				</li>
 				<li className="item">
 					<div className="metadata">Sass for Web Designers</div>
 					<div className="metadata">Cederholm</div>
 					<div className="metadata">2013</div>
 					<div className="metadata">17/04/2016 19:20</div>
+					<div className="metadata"></div>
+					<div className="metadata"></div>
 				</li>
 			</ul>
 		);
