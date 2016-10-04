b0a17d3203c70a1c01dc1341086e4100df989584
Johannes Krtek <johannes.krtek@flachware.com>
2016-10-04 19:20:22 +0200

    Add horizontal description list.

--- src/js/item/item-details.jsx
+++ src/js/item/item-details.jsx
@@ -22,6 +22,62 @@ export default class ItemDetails extends React.Component {
 						</ul>
 					</nav>
 				</header>
+
+				{/* Description list */}
+				<dl className="dl-horizontal">
+					<dt>Item Type</dt>
+					<dd>Book</dd>
+					<dt>Title</dt>
+					<dd>Responsive Web Design</dd>
+					<dt>Author</dt>
+					<dd>Marcott, Ethan</dd>
+					<dt>Editor</dt>
+					<dd>Brown, Mandy</dd>
+					<dt>Series</dt>
+					<dd>A Book Apart</dd>
+					<dt>Series Number</dt>
+					<dd>4</dd>
+					<dt>Volume</dt>
+					<dd></dd>
+					<dt># of Volume</dt>
+					<dd></dd>
+					<dt>Edition</dt>
+					<dd></dd>
+					<dt>Place</dt>
+					<dd></dd>
+					<dt>Publisher</dt>
+					<dd></dd>
+					<dt>Date</dt>
+					<dd></dd>
+					<dt># of Pages</dt>
+					<dd></dd>
+					<dt>Language</dt>
+					<dd></dd>
+					<dt>ISBN</dt>
+					<dd>978-0-9844425-7-7</dd>
+					<dt>Short Title</dt>
+					<dd></dd>
+					<dt>URL</dt>
+					<dd></dd>
+					<dt>Accessed</dt>
+					<dd></dd>
+					<dt>Archive</dt>
+					<dd></dd>
+					<dt>Loc. in Archive</dt>
+					<dd></dd>
+					<dt>Library Catalog</dt>
+					<dd></dd>
+					<dt>Call Number:</dt>
+					<dd></dd>
+					<dt>Rights</dt>
+					<dd></dd>
+					<dt>Extra</dt>
+					<dd></dd>
+					<dt>Date Added</dt>
+					<dd>29/09/2016, 18:34:31</dd>
+					<dt>Modified</dt>
+					<dd>29/09/2016, 18:51:32</dd>
+				</dl>
 			</section>
 		);
 	}
