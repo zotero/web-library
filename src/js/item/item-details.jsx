c8dd458fffc1398b034f14a26ad041a65e8e9ef9
Johannes Krtek <johannes.krtek@flachware.com>
2016-10-04 16:35:39 +0200

    Add static tabs dummy and styles.

--- src/js/item/item-details.jsx
+++ src/js/item/item-details.jsx
@@ -5,10 +5,22 @@ import React from 'react';
 export default class ItemDetails extends React.Component {
 	render() {
 		return (
-			<section className="item-details">
+			<section className='item-details'>
 				<header>
-					<h4 className="offscreen">Item title</h4>
-					Item Details
+					<h4 className='offscreen'>Item title</h4>
+
+					{/* Future tabs component */}
+					<nav>
+
+						{/* Props: justified, compact */}
+						<ul className="tabs compact">
+							<li className='active'><a href="#">Info</a></li>
+							<li><a href="#">Notes</a></li>
+							<li><a href="#">Tags</a></li>
+							<li><a href="#">Attachments</a></li>
+							<li><a href="#">Related</a></li>
+						</ul>
+					</nav>
 				</header>
 			</section>
 		);
