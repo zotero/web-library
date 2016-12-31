8e39543476f980b2d41ed4b0d190ff672e1771e1
Tom Najdek <tom@doppnet.com>
2016-12-31 02:07:21 +0000

    Item details: basic tab switching support

--- src/js/item/item-details.jsx
+++ src/js/item/item-details.jsx
@@ -5,6 +5,19 @@ import React from 'react';
 import Item from './item.jsx';
 
 export default class ItemDetails extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			active: 'info'
+		};
+	}
+	setActiveTab(active, ev) {
+		ev.preventDefault();
+		this.setState({
+			active
+		});
+	}
+
 	render() {
 		var fieldsToSkip = [ 'abstractNote' ];
 		return (
@@ -16,26 +29,34 @@ export default class ItemDetails extends React.Component {
 
 					{/* Future tabs component */}
 					<nav>
-
 						{/* Props: justified, compact */}
 						<ul className="tabs compact">
-							<li className="active"><a href="#">Info</a></li>
-							<li><a href="#">Notes</a></li>
-							<li><a href="#">Tags</a></li>
-							<li><a href="#">Attachments</a></li>
-							<li><a href="#">Related</a></li>
+							<li className={ this.state.active == 'info' ? 'active' : '' }>
+								<a href="#" onClick={ ev => this.setActiveTab('info', ev) }>Info</a>
+							</li>
+							<li className={ this.state.active == 'notes' ? 'active' : '' }>
+								<a href="#" onClick={ ev => this.setActiveTab('notes', ev) }>Notes</a>
+							</li>
+							<li className={ this.state.active == 'tags' ? 'active' : '' }>
+								<a href="#" onClick={ ev => this.setActiveTab('tags', ev) }>Tags</a>
+							</li>
+							<li className={ this.state.active == 'attachments' ? 'active' : '' }>
+								<a href="#" onClick={ ev => this.setActiveTab('attachments', ev) }>Attachments</a>
+							</li>
+							<li className={ this.state.active == 'related' ? 'active' : '' }>
+								<a href="#" onClick={ ev => this.setActiveTab('related', ev) }>Related</a>
+							</li>
 						</ul>
 					</nav>
 				</header>
 				<div className="panel-body">
 					<div className="tab-content">
-						<div className="tab-pane active">
+						<div className={ `tab-pane ${this.state.active == 'info' ? 'active' : ''}` }>
 							<div className="toolbar hidden-sm-down hidden-lg-up">
 								<div className="toolbar-right"><button className="btn">Edit</button></div>
 							</div>
 							<div className="row">
 								<div className="col">
-
 									{/* Description list */}
 									<dl className="dl-horizontal">
 										{
@@ -85,6 +106,26 @@ export default class ItemDetails extends React.Component {
 									</section>
 								</div>
 							</div>
+						</div>
+					</div>
+					<div className="tab-content">
+						<div className={ `tab-pane ${this.state.active == 'notes' ? 'active' : ''}` }>
+							NOTES
+						</div>
+					</div>
+					<div className="tab-content">
+						<div className={ `tab-pane ${this.state.active == 'tags' ? 'active' : ''}` }>
+							TAGS
+						</div>
+					</div>
+					<div className="tab-content">
+						<div className={ `tab-pane ${this.state.active == 'attachments' ? 'active' : ''}` }>
+							ATTACHMENTS
+						</div>
+					</div>
+					<div className="tab-content">
+						<div className={ `tab-pane ${this.state.active == 'related' ? 'active' : ''}` }>
+							RELATED
 						</div>
 					</div>
 				</div>
