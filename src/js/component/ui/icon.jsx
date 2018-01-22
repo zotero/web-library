d02b02178337db93bce85bd8aa8a8be485004232
Tom Najdek <tom@doppnet.com>
2018-01-22 10:12:56 +0000

    Tweak the Icon component
    * Use attributes instead of inline css for dimensions
    * Make it a PureComponent
    * Do not set default viewBox as it's not required. Instead accept
      viewBox as an optional prop

--- src/js/component/ui/icon.jsx
+++ src/js/component/ui/icon.jsx
@@ -4,45 +4,13 @@ const React = require('react');
 const PropTypes = require('prop-types');
 const cx = require('classnames');
 
-const coerceDimensions = props => ({
-	width: parseInt(props.width),
-	height: parseInt(props.height)
-});
-
-class Icon extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = coerceDimensions(props);
-	}
-
-	componentWillReceiveProps(nextProps) {
-		this.setState(coerceDimensions(nextProps));
-	}
-
+class Icon extends React.PureComponent {
 	render() {
-		let style = {};
+		let style = {
+			color: this.props.color,
+			...this.props.style
+		};
 		let basename = this.props.type.split(/[\\/]/).pop();
-		if(this.props.width) {
-			style = Object.assign(style, {
-				width: `${this.props.width}px`
-			});
-		}
-
-		if(this.props.height) {
-			style = Object.assign(style, {
-				height: `${this.props.height}px`
-			});
-		}
-
-		if(this.props.color) {
-			style = Object.assign(style, {
-				color: this.props.color
-			});
-		}
-
-		if(this.props.style) {
-			style = Object.assign(style, this.props.style);
-		}
 
 		let svgAttr = {
 			className: cx(['icon', `icon-${basename}`, this.props.className]),
@@ -50,30 +18,41 @@ class Icon extends React.Component {
 			style
 		};
 
+		if(this.props.width) {
+			svgAttr['width'] = parseInt(this.props.width, 10);
+		}
+
+		if(this.props.height) {
+			svgAttr['height'] = parseInt(this.props.height, 10);
+		} 
+
+		if(this.props.viewBox) {
+			svgAttr['viewBox'] = this.props.viewBox;
+		}
+
 		if(this.props.label) {
 			svgAttr['aria-label'] = this.props.label;
 		}
 
 		return (
-			<svg { ...svgAttr }>
-				<use xlinkHref={ `/static/icons/${this.props.type}.svg#${basename}`} viewBox="0 0 512 512" />
+			<svg { ...svgAttr } viewBox={ this.props.viewBox}>
+				<use 
+					xlinkHref={ `/static/icons/${this.props.type}.svg#${basename}`}
+				/>
 			</svg>
 		);
 	}
+
+	static propTypes = {
+		className: PropTypes.string,
+		color: PropTypes.string,
+		height: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+		label: PropTypes.string,
+		style: PropTypes.object,
+		type: PropTypes.string.isRequired,
+		viewBox: PropTypes.string,
+		width: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+	}
 }
-
-Icon.propTypes = {
-	type: PropTypes.string.isRequired,
-	label: PropTypes.string,
-	width: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
-	height: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
-	color: PropTypes.string,
-	style: PropTypes.object,
-	className: PropTypes.string
-};
-
-Icon.defaultProps = {
-
-};
 
 module.exports = Icon;
