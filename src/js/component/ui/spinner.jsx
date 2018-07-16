62b5fa5c375b59035a8ce24b25639c2a8b245dce
Flachware <johannes.krtek@flachware.com>
2018-07-16 18:42:49 +0200

    Make spinner stroke-width styleable.

--- src/js/component/ui/spinner.jsx
+++ src/js/component/ui/spinner.jsx
@@ -5,7 +5,11 @@ const Icon = require('./icon');
 
 class Spinner extends React.Component {
 	render() {
-		return <Icon type="16/spin" width="16" height="16"/>;
+		return (
+			<svg className="icon icon-spin" width="16" height="16" viewBox="0 0 16 16">
+				<path strokeWidth="1" stroke="currentColor" fill="none" d="M8,15c-3.866,0-7-3.134-7-7s3.134-7,7-7s7,3.134,7,7"/>
+			</svg>
+		);
 	}
 }
 
