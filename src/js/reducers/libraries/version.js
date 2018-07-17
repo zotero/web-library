e02aa4fadd83c02b8cbce09a0f584e100f73e3a2
Tom Najdek <tom@doppnet.com>
2018-07-18 00:32:18 +0100

    Refactor + Fix in version reducer

--- src/js/reducers/libraries/version.js
+++ src/js/reducers/libraries/version.js
@@ -1,30 +1,26 @@
 'use strict';
 
 const {
-	RECEIVE_COLLECTIONS_IN_LIBRARY,
-	RECEIVE_ITEMS_IN_COLLECTION,
+	RECEIVE_ADD_ITEMS_TO_COLLECTION,
 	RECEIVE_CHILD_ITEMS,
+	RECEIVE_COLLECTIONS_IN_LIBRARY,
 	RECEIVE_FETCH_ITEMS,
+	RECEIVE_ITEMS_IN_COLLECTION,
+	RECEIVE_MOVE_ITEMS_TRASH,
 	RECEIVE_TOP_ITEMS,
 	RECEIVE_TRASH_ITEMS,
-	RECEIVE_MOVE_ITEMS_TRASH,
 } = require('../../constants/actions.js');
 
 const version = (state = 0, action) => {
 	switch(action.type) {
-		case RECEIVE_COLLECTIONS_IN_LIBRARY:
-			return parseInt(action.response.response.headers.get('Last-Modified-Version'), 10);
-		case RECEIVE_ITEMS_IN_COLLECTION:
-			return parseInt(action.response.response.headers.get('Last-Modified-Version'), 10);
+		case RECEIVE_ADD_ITEMS_TO_COLLECTION:
 		case RECEIVE_CHILD_ITEMS:
-			return parseInt(action.response.response.headers.get('Last-Modified-Version'), 10);
+		case RECEIVE_COLLECTIONS_IN_LIBRARY:
 		case RECEIVE_FETCH_ITEMS:
-			return parseInt(action.response.response.headers.get('Last-Modified-Version'), 10);
-		case RECEIVE_TOP_ITEMS:
-			return parseInt(action.response.response.headers.get('Last-Modified-Version'), 10);
-		case RECEIVE_TRASH_ITEMS:
-			return parseInt(action.response.response.headers.get('Last-Modified-Version'), 10);
+		case RECEIVE_ITEMS_IN_COLLECTION:
 		case RECEIVE_MOVE_ITEMS_TRASH:
+		case RECEIVE_TOP_ITEMS:
+		case RECEIVE_TRASH_ITEMS:
 			return parseInt(action.response.response.headers.get('Last-Modified-Version'), 10);
 		default:
 			return state;
