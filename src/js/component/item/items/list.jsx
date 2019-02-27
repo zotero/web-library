be2f13488bcbb8f307d9ef145b6602b080189e2f
Tom Najdek <tom@doppnet.com>
2019-02-27 14:52:28 +0000

    Add tag colors to the item list #150

--- src/js/component/item/items/list.jsx
+++ src/js/component/item/items/list.jsx
@@ -113,20 +113,21 @@ class ItemsList extends React.PureComponent {
 							{ item.year }
 						</div>
 						<div className="icons">
-							<Icon type="16/attachment" width="16" height="16" />
-							<Icon type="16/note-sm" width="16" height="16" />
-							<Icon
-								type="12/circle"
-								width="12"
-								height="12"
-								style={ { color: "#f66" } }
-							/>
-							<Icon
-								type="12/crescent-circle"
-								width="8"
-								height="12"
-								style={ { color: "#5fb236" } }
-							/>
+							{
+								// currently blocked #191
+								// <Icon type="16/attachment" width="16" height="16" />
+								// <Icon type="16/note-sm" width="16" height="16" />
+							}
+
+							{ item.coloredTags.map((tag, index) => (
+								<Icon
+									key={ tag.tag }
+									type={ index === 0 ? '12/circle' : '12/crescent-circle' }
+									width={ index === 0 ? 12 : 8 }
+									height="12"
+									style={ { color: tag.color } }
+								/>
+							))}
 						</div>
 					</div>
 					<Icon type={ '16/chevron-13' } width="16" height="16" />
