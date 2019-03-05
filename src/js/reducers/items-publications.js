a99e8d324be922a22fbb1d3e2e848a5228c446a3
Tom Najdek <tom@doppnet.com>
2019-03-05 11:47:30 +0000

    Fix items-publications reducer

--- src/js/reducers/items-publications.js
+++ src/js/reducers/items-publications.js
@@ -1,9 +1,14 @@
 'use strict';
 
-const { populateItemKeys, sortItemKeysOrClear } = require('../common/reducers');
+const { indexByKey } = require('../utils');
+const { filterItemKeys, injectExtraItemKeys, populateItemKeys,
+	sortItemKeysOrClear } = require('../common/reducers');
 
 const {
+	RECEIVE_RECOVER_ITEMS_TRASH,
 	RECEIVE_PUBLICATIONS_ITEMS,
+	RECEIVE_DELETE_ITEMS,
+	RECEIVE_MOVE_ITEMS_TRASH,
 	SORT_ITEMS,
 } = require('../constants/actions.js');
 
@@ -14,6 +19,15 @@ const itemsPublications = (state = {}, action) => {
 				state,
 				action.items.map(item => item.key),
 				action
+			);
+		case RECEIVE_DELETE_ITEMS:
+		case RECEIVE_MOVE_ITEMS_TRASH:
+			return filterItemKeys(state, action.itemKeys);
+		case RECEIVE_RECOVER_ITEMS_TRASH:
+			return injectExtraItemKeys(
+				state,
+				action.itemKeys,
+				{ ...action.otherItems, ...indexByKey(action.items) }
 			);
 		case SORT_ITEMS:
 			return sortItemKeysOrClear(state, action.items, action.sortBy, action.sortDirection);
