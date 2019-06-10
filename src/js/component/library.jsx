521032793f60038d6e702e7cd09e855ac494d093
Tom Najdek <tom@doppnet.com>
2019-06-10 15:32:47 +0100

    Adjust .search-init logic #230

--- src/js/component/library.jsx
+++ src/js/component/library.jsx
@@ -95,7 +95,7 @@ class Library extends React.PureComponent {
 					'no-transitions': !useTransitions || hasUserTypeChanged,
 					'search-active': isSearchMode && itemsSource !== 'query',
 					'search-results': isSearchMode && itemsSource === 'query',
-					'search-init': !isSearchModeTransitioning && isSearchMode && !searchState.hasViewedResult,
+					'search-init': isSearchModeTransitioning && isSearchMode,
 					'search-cancel': isSearchModeTransitioning && !isSearchMode
 				}) }>
 				{
