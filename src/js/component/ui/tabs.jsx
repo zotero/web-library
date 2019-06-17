c375ed05fd7416a32848a63423a9c7608590cce2
Tom Najdek <tom@doppnet.com>
2019-06-17 22:32:46 +0100

    Enable arrow-based navigation in tabs #218

--- src/js/component/ui/tabs.jsx
+++ src/js/component/ui/tabs.jsx
@@ -3,20 +3,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
+import withFocusManager from '../../enhancers/with-focus-manager';
 
-const Tab = ({ children, isActive, isDisabled, onActivate }) => (
-	<li
-		className={ cx({
-			tab: true,
-			active: isActive,
-			disabled: isDisabled
-		}) }
-		onClick={ isDisabled ? null : onActivate }>
-			<a href="" onClick={ ev => { ev.preventDefault(); isDisabled ? null : onActivate(); } }>
+const Tab = ({ children, isActive, isDisabled, onActivate, onFocusNext, onFocusPrev }) => {
+	const handleKeyDown = ev => {
+		if(ev.target !== ev.currentTarget) {
+			return;
+		}
+
+		if(ev.key === 'ArrowRight') {
+			onFocusNext(ev);
+		} else if(ev.key === 'ArrowLeft') {
+			onFocusPrev(ev);
+		}
+	};
+	return (
+		<li
+			className={ cx({
+				tab: true,
+				active: isActive,
+				disabled: isDisabled
+			}) }
+			onClick={ isDisabled ? null : onActivate }
+		>
+			<a href=""
+				onClick={ ev => { ev.preventDefault(); isDisabled ? null : onActivate(); } }
+				tabIndex={ -2 }
+				onKeyDown={ handleKeyDown }
+			>
 				{ children }
 			</a>
-	</li>
-);
+		</li>
+	);
+}
 
 Tab.propTypes = {
 	children: PropTypes.node,
@@ -26,13 +45,36 @@ Tab.propTypes = {
 };
 
 
-const Tabs = ({ children, justified, compact }) => (
+const mapChildren = (children, mapFunc, out = []) => {
+	React.Children.toArray(children).reduce((out, child) => {
+		if(child.type === React.Fragment) {
+			mapChildren(child.props.children, mapFunc, out);
+		} else {
+			out.push(mapFunc(child));
+		}
+		return out;
+	}, out);
+	return out;
+}
+
+const Tabs = withFocusManager(({ children, justified, compact, onFocus, onBlur,
+	registerFocusRoot, onFocusNext, onFocusPrev }) => (
 	<nav>
-		<ul className={ cx('nav', 'tabs', { justified, compact }) }>
-			{ children }
+		<ul
+			className={ cx('nav', 'tabs', { justified, compact }) }
+			onFocus={ onFocus }
+			onBlur={ onBlur }
+			tabIndex={ 0 }
+			ref={ ref => registerFocusRoot(ref) }
+		>
+			{
+				mapChildren(children, child =>
+					child && child.type === Tab ? React.cloneElement(child, { onFocusNext, onFocusPrev }) : child
+				)
+			}
 		</ul>
 	</nav>
-);
+));
 
 Tabs.propTypes = {
 	children: PropTypes.node,
