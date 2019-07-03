9dac70eda9482d68e7a70c29dee5f05a75e95396
Tom Najdek <tom@doppnet.com>
2019-07-03 13:25:28 +0100

    Add dropdown in the tag selector #64

--- src/js/component/tag-selector.jsx
+++ src/js/component/tag-selector.jsx
@@ -2,11 +2,13 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import TagList from './tag-selector/tag-list';
+import Icon from './ui/icon';
 import Input from './form/input';
+import TagList from './tag-selector/tag-list';
 import withFocusManager from '../enhancers/with-focus-manager';
-import { pick } from '../common/immutable';
 import { noop } from '../utils';
+import { pick } from '../common/immutable';
+import { UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap/lib';
 
 class TagSelector extends React.PureComponent {
 	handleKeyDown = ev => {
@@ -22,8 +24,14 @@ class TagSelector extends React.PureComponent {
 		}
 	};
 
+	handleDeselectClick = () => {
+		const { libraryKey: library, collectionKey: collection, search, qmode,
+			isTrash: trash, isMyPublications: publications, view, navigate } = this.props;
+		navigate({ library, collection, trash, publications, search, view, qmode, tags: [] });
+	}
+
 	render() {
-		const { isFetching, onFocus, onBlur, registerFocusRoot } = this.props;
+		const { isFetching, onFocus, onBlur, registerFocusRoot, selectedTags } = this.props;
 		return (
 			<div className="tag-selector">
 				<TagList
@@ -48,28 +56,49 @@ class TagSelector extends React.PureComponent {
 						value={ this.props.searchString }
 						isBusy={ isFetching }
 					/>
-					<button
-						className="tag-selector-actions"
-						onClick={ this.props.onSettings }
-						onKeyDown={ this.handleKeyDown }
-						tabIndex={ -2 }
-					/>
+					<UncontrolledDropdown className="dropdown dropdown-wrapper">
+							<DropdownToggle
+								href="#"
+								className="btn-link btn-icon dropdown-toggle tag-selector-actions"
+								onKeyDown={ this.handleKeyDown }
+								tabIndex={ -2 }
+							>
+								<Icon type="16/chevron-9" width="16" height="16" />
+							</DropdownToggle>
+							<DropdownMenu right>
+								<DropdownItem disabled>
+									{ selectedTags.length } tag selected
+								</DropdownItem>
+								<DropdownItem onClick={ this.handleDeselectClick } >
+									Deselect All
+								</DropdownItem>
+							</DropdownMenu>
+					</UncontrolledDropdown>
 				</div>
 			</div>
 		);
 	}
 
 	static propTypes = {
+		collectionKey: PropTypes.string,
+		fetchTags: PropTypes.func.isRequired,
 		isFetching: PropTypes.bool,
+		isMyPublications: PropTypes.bool,
+		isTrash: PropTypes.bool,
+		libraryKey: PropTypes.string,
+		navigate: PropTypes.func,
 		onBlur: PropTypes.func,
 		onFocus: PropTypes.func,
 		onFocusNext: PropTypes.func,
 		onFocusPrev: PropTypes.func,
-		fetchTags: PropTypes.func.isRequired,
 		onSearch: PropTypes.func,
 		onSettings: PropTypes.func,
+		qmode: PropTypes.string,
 		registerFocusRoot: PropTypes.func,
+		search: PropTypes.string,
 		searchString: PropTypes.string,
+		selectedTags: PropTypes.array,
+		view: PropTypes.string,
 	}
 
 	static defaultProps = {
