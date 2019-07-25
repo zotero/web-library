a3610738280b7b521603adcf88ca47542a622b82
Tom Najdek <tom@doppnet.com>
2019-07-25 20:15:10 +0100

    Add missing fields to standalone attachments #152

--- src/js/component/item/details/tab-panes/standalone-attachment.jsx
+++ src/js/component/item/details/tab-panes/standalone-attachment.jsx
@@ -3,6 +3,8 @@
 import React from 'react';
 import cx from 'classnames';
 
+import { dateLocalized } from '../../../../common/format';
+
 class StandaloneAttachmentTabPane extends React.PureComponent {
 	render() {
 		const { item, isActive } = this.props;
@@ -13,22 +15,43 @@ class StandaloneAttachmentTabPane extends React.PureComponent {
 				'active': isActive
 			}) }>
 				<div className="standalone-attachment">
-					<div className="download-link">
-						{
-							item[Symbol.for('attachmentUrl')] &&
-							<a href={ item[Symbol.for('attachmentUrl')] }>
-								{ item.filename }
-							</a>
-						}
-					</div>
-					<div className="download-link">
-						{
-							item.url &&
-							<a href={ item.url }>
-								{ item.url }
-							</a>
-						}
-					</div>
+					<dl>
+						{ item.url && (
+							<React.Fragment>
+								<dt>Original URL</dt>
+								<dd>
+									<a href={ item.url }>
+										{ item.url }
+									</a>
+								</dd>
+							</React.Fragment>
+						)}
+
+						{item.filename && (
+							<React.Fragment>
+								<dt>Filename</dt>
+								<dd>
+									{ item[Symbol.for('attachmentUrl')] ? (
+										<a href={ item[Symbol.for('attachmentUrl')] }>
+											{ item.filename }
+										</a>
+									) : item.filename }
+								</dd>
+							</React.Fragment>
+						)}
+						{item.accessDate && (
+							<React.Fragment>
+								<dt>Access Time</dt>
+								<dd>{ dateLocalized(new Date((item.accessDate))) }</dd>
+							</React.Fragment>
+						)}
+						{item.dateModified && (
+							<React.Fragment>
+								<dt>Modified Time</dt>
+								<dd>{ dateLocalized(new Date((item.dateModified))) }</dd>
+							</React.Fragment>
+						)}
+					</dl>
 				</div>
 			</div>
 		);
