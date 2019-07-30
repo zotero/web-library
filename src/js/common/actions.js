a4f2d2e6d0cd11a99fda5fbfbb1f2ffe1757320d
Tom Najdek <tom@doppnet.com>
2019-07-30 11:50:14 +0100

    Remove api key from attachment download url. #95

--- src/js/common/actions.js
+++ src/js/common/actions.js
@@ -6,9 +6,8 @@ const extractItems = (response, state) => {
 		tags: item.tags || [],  // tags are not present on items in my publications
 								// but most of the code expects tags key to be present
 		[Symbol.for('meta')]: response.getMeta()[index] || {},
-		// @TODO: url should not include the key
 		[Symbol.for('attachmentUrl')]: item.itemType === 'attachment' &&
-			`https://${state.config.apiConfig.apiAuthorityPart}/users/${state.config.userId}/items/${item.key}/file/view?key=${state.config.apiKey}`
+			`https://${state.config.apiConfig.apiAuthorityPart}/users/${state.config.userId}/items/${item.key}/file/view`
 	}));
 }
 
