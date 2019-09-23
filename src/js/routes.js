b7bfe80d1a84d4dd87ef647c30695b397b6d1420
Tom Najdek <tom@doppnet.com>
2019-09-23 20:47:51 +0200

    Add redirect for swapped params route #259
    Covers case where itemKey and collectionKey params are in swapped positions in
    the url, which is how it is redirected to server-side from a canonical url.

--- src/js/routes.js
+++ src/js/routes.js
@@ -29,59 +29,91 @@ export const routes = [
 ];
 
 export const redirects = [
-	{ 	from: '/:userslug/items/collectionKey/trash/itemKey/:items/:optional?/:mode?',
-		to: '/:userslug/trash/items/:items' },
-	{ 	from: '/:userslug/items/collectionKey/:collection/itemKey/:items/:optional?/:mode?',
-		to: '/:userslug/collections/:collection/items/:items' },
-	{	from: '/:userslug/items/collectionKey/trash/q/:search',
-		to: '/:userslug/trash/search/:search' },
-	{	from: '/:userslug/items/collectionKey/trash/:optional?/:mode?',
-		to: '/:userslug/trash' },
-	{	from: '/:userslug/items/collectionKey/:collection/q/:search/:optional?/:mode?',
-		to: '/:userslug/collections/:collection/search/:search' },
-	{	from: '/:userslug/items/collectionKey/:collection/:optional?/:mode?',
-		to: '/:userslug/collections/:collection' },
-	{ 	from: '/:userslug/items/action/newItem/collectionKey/trash/:optional?/:mode?',
-		to: '/:userslug/trash' },
-	{ 	from: '/:userslug/items/action/newItem/collectionKey/:collection/:optional?/:mode?',
-		to: '/:userslug/collections/:collection' },
-	{ 	from: '/:userslug/items/itemKey/:items/:optional?/:mode?',
-		to: '/:userslug/items/:items' },
-	{ 	from: '/:userslug/items/action/newItem/:optional?/:mode?',
-		to: '/:userslug/library' },
-	{ 	from: '/:userslug/items/q/:search',
-		to: '/:userslug/search/:search' },
-	{ 	from: '/:userslug/items',
-		to: '/:userslug/library' },
+	{ 	from: 	 '/:userslug/items/collectionKey/trash/itemKey/:items/:optional?/:mode?',
+		to: 	 '/:userslug/trash/items/:items'
+	},
+	{ 	from: 	 '/:userslug/items/collectionKey/:collection/itemKey/:items/:optional?/:mode?',
+		to: 	 '/:userslug/collections/:collection/items/:items'
+	},  //@NOTE: Canonical url redirect return collectionKey and itemKey swapped around:
+	{ 	from: 	 '/:userslug/items/itemKey/:items/collectionKey/:collection/:optional?/:mode?',
+		to: 	 '/:userslug/collections/:collection/items/:items'
+	},
+	{	from: 	 '/:userslug/items/collectionKey/trash/q/:search',
+		to: 	 '/:userslug/trash/search/:search'
+	},
+	{	from: 	 '/:userslug/items/collectionKey/trash/:optional?/:mode?',
+		to: 	 '/:userslug/trash'
+	},
+	{	from: 	 '/:userslug/items/collectionKey/:collection/q/:search/:optional?/:mode?',
+		to: 	 '/:userslug/collections/:collection/search/:search'
+	},
+	{	from: 	 '/:userslug/items/collectionKey/:collection/:optional?/:mode?',
+		to: 	 '/:userslug/collections/:collection'
+	},
+	{ 	from: 	 '/:userslug/items/action/newItem/collectionKey/trash/:optional?/:mode?',
+		to: 	 '/:userslug/trash'
+	},
+	{ 	from: 	 '/:userslug/items/action/newItem/collectionKey/:collection/:optional?/:mode?',
+		to: 	 '/:userslug/collections/:collection'
+	},
+	{ 	from: 	 '/:userslug/items/itemKey/:items/:optional?/:mode?',
+		to: 	 '/:userslug/items/:items'
+	},
+	{ 	from: 	 '/:userslug/items/action/newItem/:optional?/:mode?',
+		to: 	 '/:userslug/library'
+	},
+	{ 	from: 	 '/:userslug/items/q/:search',
+		to: 	 '/:userslug/search/:search'
+	},
+	{ 	from: 	 '/:userslug/items',
+		to: 	 '/:userslug/library'
+	},
 
-	{ 	from: '/groups/:groupid/:groupslug/items/collectionKey/trash/itemKey/:items/:optional?/:mode?',
-		to: '/groups/:groupid/:groupslug/trash/items/:items' },
-	{ 	from: '/groups/:groupid/:groupslug/items/collectionKey/:collection/itemKey/:items/:optional?/:mode?',
-		to: '/groups/:groupid/:groupslug/collections/:collection/items/:items' },
-	{	from: '/groups/:groupid/:groupslug/items/collectionKey/trash/q/:search',
-		to: '/groups/:groupid/:groupslug/trash/search/:search' },
-	{	from: '/groups/:groupid/:groupslug/items/collectionKey/trash/:optional?/:mode?',
-		to: '/groups/:groupid/:groupslug/trash' },
-	{	from: '/groups/:groupid/:groupslug/items/collectionKey/:collection/q/:search/:optional?/:mode?',
-		to: '/groups/:groupid/:groupslug/collections/:collection/search/:search' },
-	{	from: '/groups/:groupid/:groupslug/items/collectionKey/:collection/:optional?/:mode?',
-		to: '/groups/:groupid/:groupslug/collections/:collection' },
-	{ 	from: '/groups/:groupid/:groupslug/items/action/newItem/collectionKey/trash/:optional?/:mode?',
-		to: '/groups/:groupid/:groupslug/trash' },
-	{ 	from: '/groups/:groupid/:groupslug/items/action/newItem/collectionKey/:collection/:optional?/:mode?',
-		to: '/groups/:groupid/:groupslug/collections/:collection' },
-	{ 	from: '/groups/:groupid/:groupslug/items/itemKey/:items/:optional?/:mode?',
-		to: '/groups/:groupid/:groupslug/items/:items' },
-	{ 	from: '/groups/:groupid/:groupslug/items/action/newItem/:optional?/:mode?',
-		to: '/groups/:groupid/:groupslug/library' },
-	{ 	from: '/groups/:groupid/:groupslug/items/q/:search',
-		to: '/groups/:groupid/:groupslug/search/:search' },
-	{ 	from: '/groups/:groupid/:groupslug/items',
-		to: '/groups/:groupid/:groupslug/library' },
+	{ 	from: 	 '/groups/:groupid/:groupslug/items/collectionKey/trash/itemKey/:items/:optional?/:mode?',
+		to: 	 '/groups/:groupid/:groupslug/trash/items/:items'
+	},
+	{ 	from: 	 '/groups/:groupid/:groupslug/items/collectionKey/:collection/itemKey/:items/:optional?/:mode?',
+		to: 	 '/groups/:groupid/:groupslug/collections/:collection/items/:items'
+	},  //@NOTE: Canonical url redirect return collectionKey and itemKey swapped around:
+	{ 	from: 	 '/groups/:groupid/:groupslug/items/itemKey/:items/collectionKey/:collection/:optional?/:mode?',
+		to: 	 '/groups/:groupid/:groupslug/collections/:collection/items/:items'
+	},
+	{	from: 	 '/groups/:groupid/:groupslug/items/collectionKey/trash/q/:search',
+		to: 	 '/groups/:groupid/:groupslug/trash/search/:search'
+	},
+	{	from: 	 '/groups/:groupid/:groupslug/items/collectionKey/trash/:optional?/:mode?',
+		to: 	 '/groups/:groupid/:groupslug/trash'
+	},
+	{	from: 	 '/groups/:groupid/:groupslug/items/collectionKey/:collection/q/:search/:optional?/:mode?',
+		to: 	 '/groups/:groupid/:groupslug/collections/:collection/search/:search'
+	},
+	{	from: 	 '/groups/:groupid/:groupslug/items/collectionKey/:collection/:optional?/:mode?',
+		to: 	 '/groups/:groupid/:groupslug/collections/:collection'
+	},
+	{ 	from: 	 '/groups/:groupid/:groupslug/items/action/newItem/collectionKey/trash/:optional?/:mode?',
+		to: 	 '/groups/:groupid/:groupslug/trash'
+	},
+	{ 	from: 	 '/groups/:groupid/:groupslug/items/action/newItem/collectionKey/:collection/:optional?/:mode?',
+		to: 	 '/groups/:groupid/:groupslug/collections/:collection'
+	},
+	{ 	from: 	 '/groups/:groupid/:groupslug/items/itemKey/:items/:optional?/:mode?',
+		to: 	 '/groups/:groupid/:groupslug/items/:items'
+	},
+	{ 	from: 	 '/groups/:groupid/:groupslug/items/action/newItem/:optional?/:mode?',
+		to: 	 '/groups/:groupid/:groupslug/library'
+	},
+	{ 	from: 	 '/groups/:groupid/:groupslug/items/q/:search',
+		to: 	 '/groups/:groupid/:groupslug/search/:search'
+	},
+	{ 	from: 	 '/groups/:groupid/:groupslug/items',
+		to: 	 '/groups/:groupid/:groupslug/library'
+	},
 
 	{	from: '/groups/:groupid/:groupslug',
-		to: '/groups/:groupid/:groupslug/library' },
+		to: '/groups/:groupid/:groupslug/library'
+	},
 
 	{ 	from: '/',
-		to: '/libraries' },
+		to: '/libraries'
+	},
 ];
