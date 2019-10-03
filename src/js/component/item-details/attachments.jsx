35ef6736c3c78253d0bac04e910809e71600b975
Tom Najdek <tom@doppnet.com>
2019-10-03 17:18:08 +0200

    Check for enclosure instead of md5 #244
    Technically more correct way to verify if file exists online

--- src/js/component/item-details/attachments.jsx
+++ src/js/component/item-details/attachments.jsx
@@ -42,7 +42,7 @@ const Attachment = ({ attachment, deleteItem, isReadOnly, isUploading, getAttach
 		>
 			<Icon type={ getItemIcon(attachment) } width="16" height="16" />
 			{
-				attachment.linkMode.startsWith('imported') && attachment.md5 && !isUploading ? (
+				attachment.linkMode.startsWith('imported') && attachment[Symbol.for('links')].enclosure && !isUploading ? (
 					<a
 						onClick={ handleLinkInteraction }
 						onMouseDown={ handleLinkInteraction }
