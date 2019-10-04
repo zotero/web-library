2c3f6ae81d2570844223bf8d3038d00be2210bd8
Tom Najdek <tom@doppnet.com>
2019-10-04 21:08:40 +0200

    Rewrite TagSelector as functional component (WIP)

--- src/js/component/tag-selector.jsx
+++ src/js/component/tag-selector.jsx
@@ -1,20 +1,30 @@
 'use strict';
 
-import React from 'react';
-import PropTypes from 'prop-types';
 import cx from 'classnames';
+import PropTypes from 'prop-types';
+import React, { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap/lib';
+
 import Button from './ui/button';
 import Icon from './ui/icon';
 import Input from './form/input';
 import TagList from './tag-selector/tag-list';
 import withFocusManager from '../enhancers/with-focus-manager';
+import { getTagsData } from '../common/state';
+import { navigate, toggleTagSelector } from '../actions';
 import { noop } from '../utils';
 import { pick } from '../common/immutable';
-import { UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap/lib';
 
-class TagSelector extends React.PureComponent {
-	handleKeyDown = ev => {
-		const { onFocusNext, onFocusPrev } = this.props;
+const TagSelector = props => {
+	const { onFocus, onBlur, registerFocusRoot, searchString, onSearch } = props;
+	const { isFetching } = useSelector(state => getTagsData(state));
+	const { tags: selectedTags, isTagSelectorOpen } = useSelector(state => state.current);
+	const dispatch = useDispatch();
+
+
+	const handleKeyDown = useCallback(ev => {
+		const { onFocusNext, onFocusPrev } = props;
 		if(ev.target !== ev.currentTarget) {
 			return;
 		}
@@ -24,107 +34,100 @@ class TagSelector extends React.PureComponent {
 		} else if(ev.key === 'ArrowLeft') {
 			onFocusPrev(ev);
 		}
-	};
+	});
 
-	handleDeselectClick = () => {
-		const {  navigate } = this.props;
-		navigate({ tags: [] });
-	}
+	const handleDeselectClick = useCallback(() => {
+		dispatch(navigate({ tags: [] }));
+	});
 
-	handleCollapseClick = () => {
-		const { isTagSelectorOpen, toggleTagSelector } = this.props;
-		toggleTagSelector(!isTagSelectorOpen);
-	}
+	const handleCollapseClick = useCallback(() => {
+		dispatch(toggleTagSelector(!isTagSelectorOpen));
+	})
 
-	render() {
-		const { isFetching, onFocus, onBlur, registerFocusRoot, selectedTags } = this.props;
-		return (
-			<div
-				className={ cx('tag-selector', { 'collapsed': !this.props.isTagSelectorOpen }) }
+	return (
+		<div
+			className={ cx('tag-selector', { 'collapsed': !isTagSelectorOpen }) }
+			>
+			<div className="scroll-container">
+				<Button
+					className="tag-selector-toggle"
+					onClick={ handleCollapseClick }
 				>
-				<div className="scroll-container">
-					<Button
-						className="tag-selector-toggle"
-						onClick={ this.handleCollapseClick }
-					>
-						<Icon type="16/grip" width="16" height="2" />
-					</Button>
-					<TagList
-						{ ...pick(this.props, ['checkColoredTags', 'fetchTags',
-							'isFetching', 'onSelect', 'searchString', 'sourceTagsPointer',
-							'tags', 'totalTagCount'])
-						}
+					<Icon type="16/grip" width="16" height="2" />
+				</Button>
+				<TagList
+					{ ...pick(props, ['checkColoredTags', 'fetchTags',
+						'isFetching', 'onSelect', 'searchString', 'sourceTagsPointer',
+						'tags', 'totalTagCount'])
+					}
+				/>
+				<div
+					className="tag-selector-filter-container"
+					onBlur={ onBlur }
+					onFocus={ onFocus }
+					ref={ ref => registerFocusRoot(ref) }
+					tabIndex={ 0 }
+				>
+					<Input
+						className="tag-selector-filter form-control"
+						onChange={ onSearch }
+						onKeyDown={ handleKeyDown }
+						tabIndex={ -2 }
+						type="search"
+						value={ searchString }
+						isBusy={ isFetching }
+						placeholder="Filter Tags"
 					/>
-					<div
-						className="tag-selector-filter-container"
-						onBlur={ onBlur }
-						onFocus={ onFocus }
-						ref={ ref => registerFocusRoot(ref) }
-						tabIndex={ 0 }
-					>
-						<Input
-							className="tag-selector-filter form-control"
-							onChange={ this.props.onSearch }
-							onKeyDown={ this.handleKeyDown }
-							tabIndex={ -2 }
-							type="search"
-							value={ this.props.searchString }
-							isBusy={ isFetching }
-							placeholder="Filter Tags"
-						/>
-						<UncontrolledDropdown className="dropdown">
-								<DropdownToggle
-									className="btn-icon dropdown-toggle tag-selector-actions"
-									onKeyDown={ this.handleKeyDown }
-									tabIndex={ -2 }
-									color={ null }
-								>
-									<Icon type="16/options" width="16" height="16" />
-								</DropdownToggle>
-								<DropdownMenu right>
-									<DropdownItem disabled>
-										{ selectedTags.length } tag selected
-									</DropdownItem>
-									<DropdownItem onClick={ this.handleDeselectClick } >
-										Deselect All
-									</DropdownItem>
-								</DropdownMenu>
-						</UncontrolledDropdown>
-					</div>
+					<UncontrolledDropdown className="dropdown">
+							<DropdownToggle
+								className="btn-icon dropdown-toggle tag-selector-actions"
+								onKeyDown={ handleKeyDown }
+								tabIndex={ -2 }
+								color={ null }
+							>
+								<Icon type="16/options" width="16" height="16" />
+							</DropdownToggle>
+							<DropdownMenu right>
+								<DropdownItem disabled>
+									{ selectedTags.length } tag selected
+								</DropdownItem>
+								<DropdownItem onClick={ handleDeselectClick } >
+									Deselect All
+								</DropdownItem>
+							</DropdownMenu>
+					</UncontrolledDropdown>
 				</div>
 			</div>
-		);
-	}
+		</div>
+	);
+}
 
-	static propTypes = {
-		collectionKey: PropTypes.string,
-		fetchTags: PropTypes.func.isRequired,
-		isFetching: PropTypes.bool,
-		isMyPublications: PropTypes.bool,
-		isTagSelectorOpen: PropTypes.bool,
-		isTrash: PropTypes.bool,
-		libraryKey: PropTypes.string,
-		navigate: PropTypes.func,
-		onBlur: PropTypes.func,
-		onFocus: PropTypes.func,
-		onFocusNext: PropTypes.func,
-		onFocusPrev: PropTypes.func,
-		onSearch: PropTypes.func,
-		onSettings: PropTypes.func,
-		qmode: PropTypes.string,
-		registerFocusRoot: PropTypes.func,
-		search: PropTypes.string,
-		searchString: PropTypes.string,
-		selectedTags: PropTypes.array,
-		toggleTagSelector: PropTypes.func,
-		view: PropTypes.string,
-	}
+TagSelector.propTypes = {
+	collectionKey: PropTypes.string,
+	fetchTags: PropTypes.func.isRequired,
+	isFetching: PropTypes.bool,
+	isMyPublications: PropTypes.bool,
+	isTrash: PropTypes.bool,
+	libraryKey: PropTypes.string,
+	navigate: PropTypes.func,
+	onBlur: PropTypes.func,
+	onFocus: PropTypes.func,
+	onFocusNext: PropTypes.func,
+	onFocusPrev: PropTypes.func,
+	onSearch: PropTypes.func,
+	onSettings: PropTypes.func,
+	qmode: PropTypes.string,
+	registerFocusRoot: PropTypes.func,
+	search: PropTypes.string,
+	searchString: PropTypes.string,
+	toggleTagSelector: PropTypes.func,
+	view: PropTypes.string,
+}
 
-	static defaultProps = {
-		onSearch: noop,
-		onSettings: noop,
-		searchString: '',
-	}
+TagSelector.defaultProps = {
+	onSearch: noop,
+	onSettings: noop,
+	searchString: '',
 }
 
 export default withFocusManager(TagSelector);
