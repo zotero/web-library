zotero/web-library

commit 455fc656e19b1093bf541d30c3e2e89737db9745

Move device logic into redux #294 #322

src/js/component/edit-toggle-button.jsx
@@ -1,26 +1,35 @@
-'use strict';
+import PropTypes from 'prop-types';
+import React, { useCallback } from 'react';
+import { useSelector } from 'react-redux';
 
-import React from 'react';
-import withDevice from '../enhancers/with-device';
-import withEditMode from '../enhancers/with-edit-mode';
 import Button from './ui/button';
+import { useEditMode } from '../hooks';
 
+const EditToggleButton = props => {
+	const { className, isReadOnly } = props;
+	const isTouchUser = useSelector(state => state.device.isTouchUser);
+	const md = useSelector(state => state.viewport.md);
+	const [isEditing, dispatchTriggerEditingItem] = useEditMode();
+	const shouldShowEmptyLabel = md && (isReadOnly || !isTouchUser);
+	const toggleOnLabel = shouldShowEmptyLabel ? 'Show Empty Fields' : 'Edit';
+	const toggleOffLabel = shouldShowEmptyLabel ? 'Hide Empty Fields' : 'Done';
+	const label = isEditing ? toggleOffLabel : toggleOnLabel;
 
-class EditToggleButton extends React.PureComponent {
-	render() {
-		const { isEditing, device, onEditModeToggle, className,
-			isReadOnly } = this.props;
-		const isShowEmptyLabel = device.viewport.md && (isReadOnly || !device.isTouchUser);
-		const toggleOnLabel = isShowEmptyLabel ? 'Show Empty Fields' : 'Edit';
-		const toggleOffLabel = isShowEmptyLabel ? 'Hide Empty Fields' : 'Done';
-		const label = isEditing ? toggleOffLabel : toggleOnLabel;
+	const handleClick = useCallback(() => {
+		dispatchTriggerEditingItem(!isEditing);
+	});
 
-		return (
-			<Button className={ className} onClick={ () => onEditModeToggle(!isEditing) }>
-				{ label }
-			</Button>
-		);
-	}
+	return (
+		<Button className={ className} onClick={ handleClick }>
+			{ label }
+		</Button>
+	);
+
 }
 
-export default withEditMode(withDevice(EditToggleButton));
+EditToggleButton.propTypes = {
+	className: PropTypes.string,
+	isReadOnly: PropTypes.bool,
+}
+
+export default EditToggleButton;

src/js/reducers/device.js
@@ -1,0 +1,45 @@
+import { TRIGGER_USER_TYPE_CHANGE, TRIGGER_RESIZE_VIEWPORT } from '../constants/actions.js';
+import { getScrollbarWidth } from '../utils';
+import { pick } from '../common/immutable';
+
+const defaultState = {
+	isKeyboardUser: null,
+	isMouseUser: typeof(matchMedia) === 'function' ? matchMedia('(pointer:fine)').matches : null,
+	isSingleColumn: false,
+	isTouchOrSmall: false,
+	isTouchUser: typeof(matchMedia) === 'function' ? matchMedia('(pointer:coarse)').matches : null,
+	scrollbarWidth: getScrollbarWidth(),
+	shouldUseEditMode: false,
+	shouldUseModalCreatorField: false,
+	shouldUseSidebar: false,
+	shouldUseTabs: false,
+	userType: matchMedia('(pointer:coarse)').matches ? 'touch' : null,
+};
+
+const getDevice = ({ userType }, viewport) => {
+	const isSingleColumn = viewport.xxs || viewport.xs;
+	const isTouchOrSmall = userType === 'touch' || viewport.xxs || viewport.xs || viewport.sm;
+	const shouldUseEditMode = isTouchOrSmall;
+	const shouldUseModalCreatorField = isTouchOrSmall;
+	const shouldUseSidebar = !viewport.lg;
+	const shouldUseTabs = viewport.md || (viewport.lg && userType != 'touch');
+	return { isSingleColumn, isTouchOrSmall, shouldUseEditMode, shouldUseModalCreatorField,
+	shouldUseSidebar, shouldUseTabs }
+};
+
+const device = (state = defaultState, action, { viewport = {} } = {}) => {
+	switch(action.type) {
+		case TRIGGER_RESIZE_VIEWPORT:
+		case TRIGGER_USER_TYPE_CHANGE:
+			return {
+				...state,
+				...pick(action, ['isKeyboardUser', 'isMouseUser', 'isTouchUser', 'userType', 'scrollbarWidth']),
+				...getDevice(action, viewport),
+			}
+		default:
+			return state;
+	}
+};
+
+export default device;
+

src/js/reducers/index.js
@@ -6,6 +6,7 @@ import { connectRouter } from 'connected-react-router';
 import collectionCountByLibrary from './collection-count-by-library';
 import config from './config';
 import current from './current';
+import device from './device';
 import errors from './errors';
 import fetching from './fetching';
 import groups from './groups';
@@ -19,10 +20,9 @@ import preferences from './preferences';
 import query from './query';
 import sources from './sources'
 import styles from './styles';
-import userType from './user-type'
 import viewport from './viewport';
 
 
 export default history => combineSectionReducers({ collectionCountByLibrary, config, current,
-	errors, fetching, groups, identifier, itemsPublications, libraries, meta, modal, ongoing,
-	preferences, query, router: connectRouter(history), sources, styles, viewport, userType, });
+	device, errors, fetching, groups, identifier, itemsPublications, libraries, meta, modal,
+	ongoing, preferences, query, router: connectRouter(history), sources, styles, viewport, });

src/js/component/form/select.jsx
@@ -91,7 +91,7 @@ class SelectInput extends React.PureComponent {
 		};
 	}
 
-	renderInput(userType, viewport) {
+	renderInput(device) {
 		const {
 			options,
 			autoFocus,
@@ -111,7 +111,7 @@ class SelectInput extends React.PureComponent {
 			required: this.props.isRequired,
 		};
 
-		if(userType === 'touch' || viewport.xxs || viewport.xs || viewport.sm) {
+		if(device.isTouchOrSmall) {
 			const props = {
 				...commonProps,
 				onKeyDown: this.handleKeyDown.bind(this),
@@ -149,7 +149,6 @@ class SelectInput extends React.PureComponent {
 	}
 
 	render() {
-		const { userType, viewport } = this.props.device;
 		const className = cx({
 			'input-group': true,
 			'select': true,
@@ -157,7 +156,7 @@ class SelectInput extends React.PureComponent {
 		}, this.props.inputGroupClassName);
 		return (
 			<div className={ className }>
-				{ this.renderInput(userType, viewport) }
+				{ this.renderInput(this.props.device) }
 				{ this.renderSpinner() }
 			</div>
 		);

src/js/container/library.jsx
@@ -15,11 +15,11 @@ const mapStateToProps = state => {
 		useTransitions, userLibraryKey, view } = state.current;
 	const { itemTypes } = state.meta;
 	const { isSynced } = get(state, ['libraries', libraryKey, 'sync'], {});
-	const { config, fetching: { collectionsInLibrary }, viewport } = state;
+	const { config, fetching: { collectionsInLibrary } } = state;
 	const isFetchingCollections = collectionsInLibrary.some(key => key === userLibraryKey || key === libraryKey);
 	const isFetchingLibrarySettings = get(state, ['libraries', libraryKey, 'fetching', 'librarySettings']);
 
-	return { config, view, userLibraryKey, viewport, isSearchMode, isSelectMode, itemsSource,
+	return { config, view, userLibraryKey, isSearchMode, isSelectMode, itemsSource,
 		collectionKey, isFetchingCollections, isFetchingLibrarySettings, isNavBarOpen,
 		isMyPublications, isSynced, itemTypes, isTrash, useTransitions, libraryKey, noteKey, search,
 		searchState, tags, qmode, isLibraryReadOnly, };

src/js/enhancers/with-device.js
@@ -1,46 +1,13 @@
 'use strict';
 
 import React from 'react';
-import { useSelector } from 'react-redux';
-import deepEqual from 'deep-equal';
+import { useSelector, shallowEqual } from 'react-redux';
 import hoistNonReactStatic from 'hoist-non-react-statics';
 
-var device = {
-	isKeyboardUser: false,
-	isMouseUser: false,
-	isSingleColumn: false,
-	isTouchOrSmall: false,
-	isTouchUser: false,
-	scrollbarWidth: false,
-	shouldUseEditMode: false,
-	shouldUseModalCreatorField: false,
-	shouldUseSidebar: false,
-	shouldUseTabs: false,
-	userType: false,
-	viewport: false,
-};
-
+//@NOTE: legacy enhancer component, use selector instead. See #294
 const withDevice = Component => {
 	const C = React.forwardRef((props, ref) => {
-		const userTypeData = useSelector(state => state.userType);
-		const viewport = useSelector(state => state.viewport);
-
-		const { isKeyboardUser, isMouseUser, isTouchUser, scrollbarWidth, userType } = userTypeData;
-		const isTouchOrSmall = userType === 'touch' || viewport.xxs || viewport.xs || viewport.sm;
-		const shouldUseEditMode = isTouchOrSmall;
-		const shouldUseModalCreatorField = isTouchOrSmall;
-		const shouldUseTabs = viewport.md || (viewport.lg && userType != 'touch');
-		const isSingleColumn = viewport.xxs || viewport.xs;
-		const shouldUseSidebar = !viewport.lg;
-
-		const newDevice = { isKeyboardUser, isMouseUser, isSingleColumn, isTouchOrSmall,
-			isTouchUser, scrollbarWidth, shouldUseEditMode, shouldUseModalCreatorField,
-			shouldUseSidebar, shouldUseTabs, userType, viewport
-		};
-
-		if(!deepEqual(device, newDevice)) {
-			device = newDevice;
-		}
+		const device = useSelector(state => state.device, shallowEqual);
 
 		return <Component { ...props } device={ device } ref={ ref } />
 	});

src/js/reducers/user-type.js
@@ -1,29 +1,0 @@
-'use strict';
-
-import { TRIGGER_USER_TYPE_CHANGE } from '../constants/actions.js';
-import { getScrollbarWidth } from '../utils';
-import { pick } from '../common/immutable';
-
-const defaultState = {
-	isKeyboardUser: null,
-	isMouseUser: typeof(matchMedia) === 'function'
-		? matchMedia('(pointer:fine)').matches : null,
-	isTouchUser: typeof(matchMedia) === 'function'
-		? matchMedia('(pointer:coarse)').matches : null,
-	userType: matchMedia('(pointer:coarse)').matches ? 'touch' : null,
-	scrollbarWidth: getScrollbarWidth()
-};
-
-const userType = (state = defaultState, action) => {
-	switch(action.type) {
-		case TRIGGER_USER_TYPE_CHANGE:
-			return {
-				...state,
-				...pick(action, ['isKeyboardUser', 'isMouseUser', 'isTouchUser', 'userType', 'scrollbarWidth'])
-			}
-		default:
-			return state;
-	}
-};
-
-export default userType;

commit 2653ebe9066aaa8a871bf1bc752fb2bdc40f65b3

Merge device and viewport reducers #294 #322
Also fixes regression introduced in 455fc65.

src/js/component/edit-toggle-button.jsx
@@ -8,7 +8,7 @@ import { useEditMode } from '../hooks';
 const EditToggleButton = props => {
 	const { className, isReadOnly } = props;
 	const isTouchUser = useSelector(state => state.device.isTouchUser);
-	const md = useSelector(state => state.viewport.md);
+	const md = useSelector(state => state.device.md);
 	const [isEditing, dispatchTriggerEditingItem] = useEditMode();
 	const shouldShowEmptyLabel = md && (isReadOnly || !isTouchUser);
 	const toggleOnLabel = shouldShowEmptyLabel ? 'Show Empty Fields' : 'Edit';

src/js/reducers/current.js
@@ -54,7 +54,7 @@ const getLibraryKey = (params, config) => {
 }
 
 
-const current = (state = stateDefault, action, { config = {}, viewport = {} } = {}) => {
+const current = (state = stateDefault, action, { config = {}, device = {} } = {}) => {
 	switch(action.type) {
 		case CONFIGURE:
 			return {
@@ -178,8 +178,8 @@ const current = (state = stateDefault, action, { config = {}, viewport = {} } = 
 		case TRIGGER_USER_TYPE_CHANGE:
 			return {
 				...state,
-				editingItemKey: action.userType === 'mouse' && !viewport.xxs && !viewport.xs &&
-					!viewport.sm ? null : state.editingItemKey
+				editingItemKey: action.userType === 'mouse' && !device.xxs && !device.xs &&
+					!device.sm ? null : state.editingItemKey
 			}
 		default:
 			return state;

src/js/reducers/device.js
@@ -14,6 +14,11 @@ const defaultState = {
 	shouldUseSidebar: false,
 	shouldUseTabs: false,
 	userType: matchMedia('(pointer:coarse)').matches ? 'touch' : null,
+	xxs: false,
+	xs: false,
+	sm: false,
+	md: false,
+	lg: false,
 };
 
 const getDevice = ({ userType }, viewport) => {
@@ -22,19 +27,33 @@ const getDevice = ({ userType }, viewport) => {
 	const shouldUseEditMode = isTouchOrSmall;
 	const shouldUseModalCreatorField = isTouchOrSmall;
 	const shouldUseSidebar = !viewport.lg;
-	const shouldUseTabs = viewport.md || (viewport.lg && userType != 'touch');
+	const shouldUseTabs = viewport.md || (viewport.lg && userType !== 'touch');
+
 	return { isSingleColumn, isTouchOrSmall, shouldUseEditMode, shouldUseModalCreatorField,
-	shouldUseSidebar, shouldUseTabs }
+		shouldUseSidebar, shouldUseTabs };
 };
 
-const device = (state = defaultState, action, { viewport = {} } = {}) => {
+const getViewport = ({ width }) => {
+	return {
+		xxs: width < 480,
+		xs: width >= 480 && width < 768,
+		sm: width >= 768 && width < 992,
+		md: width >= 992 && width < 1200,
+		lg: width >= 1200
+	};
+}
+
+const device = (state = defaultState, action) => {
+	var viewport;
 	switch(action.type) {
 		case TRIGGER_RESIZE_VIEWPORT:
 		case TRIGGER_USER_TYPE_CHANGE:
+			viewport = action.type === TRIGGER_RESIZE_VIEWPORT ? getViewport(action) : pick(state, ['xxs', 'xs', 'sm', 'md', 'lg']);
 			return {
 				...state,
 				...pick(action, ['isKeyboardUser', 'isMouseUser', 'isTouchUser', 'userType', 'scrollbarWidth']),
 				...getDevice(action, viewport),
+				...viewport
 			}
 		default:
 			return state;

src/js/reducers/index.js
@@ -20,9 +20,8 @@ import preferences from './preferences';
 import query from './query';
 import sources from './sources'
 import styles from './styles';
-import viewport from './viewport';
 
 
 export default history => combineSectionReducers({ collectionCountByLibrary, config, current,
 	device, errors, fetching, groups, identifier, itemsPublications, libraries, meta, modal,
-	ongoing, preferences, query, router: connectRouter(history), sources, styles, viewport, });
+	ongoing, preferences, query, router: connectRouter(history), sources, styles, });

src/js/reducers/viewport.js
@@ -1,26 +1,0 @@
-'use strict';
-
-import { TRIGGER_RESIZE_VIEWPORT } from '../constants/actions.js';
-
-const defaultState = {
-	width: 0, height: 0, xs: false, sm: false, md: false, lg: false,
-};
-
-const viewport = (state = defaultState, action) => {
-	switch(action.type) {
-		case TRIGGER_RESIZE_VIEWPORT:
-			return {
-				width: action.width,
-				height: action.height,
-				xxs: action.width < 480,
-				xs: action.width >= 480 && action.width < 768,
-				sm: action.width >= 768 && action.width < 992,
-				md: action.width >= 992 && action.width < 1200,
-				lg: action.width >= 1200
-			};
-		default:
-			return state;
-	}
-};
-
-export default viewport;