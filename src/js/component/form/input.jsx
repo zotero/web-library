zotero/web-library

commit 7da4ca9c9a533eaa188b3e292b1d5ac33e118021

Fix <Input>'s default value for suggestions #309

src/js/component/form/input.jsx
@@ -190,7 +190,7 @@ class Input extends React.PureComponent {
 		tabIndex: -1,
 		type: 'text',
 		value: '',
-		suggestions: []
+		suggestions: null
 	};
 
 	static propTypes = {

commit a1c99bc75d6400daceb20cc482f919fcb7a64909

Fix a bug with confirming input value with a key
This is regression probably introduced in 7da4ca9c and only affecting
some inputs

src/js/component/form/input.jsx
@@ -76,7 +76,7 @@ class Input extends React.PureComponent {
 		const { hasCancelledSuggestions } = this.state;
 		switch (event.key) {
 			case 'Escape':
-				if(suggestions.length && !hasCancelledSuggestions) {
+				if(suggestions && suggestions.length && !hasCancelledSuggestions) {
 					this.setState({ hasCancelledSuggestions: true });
 					event.stopPropagation();
 				} else {
@@ -84,7 +84,7 @@ class Input extends React.PureComponent {
 				}
 			break;
 			case 'Enter':
-				if(suggestions.length && !hasCancelledSuggestions) {
+				if(suggestions && suggestions.length && !hasCancelledSuggestions) {
 					const value = this.suggestions.current.getSuggestion() || this.state.value;
 					event.persist();
 					this.setState({ value }, () => this.commit(event));
@@ -94,7 +94,7 @@ class Input extends React.PureComponent {
 			break;
 			case 'ArrowDown':
 			case 'ArrowUp':
-				if(suggestions.length && !hasCancelledSuggestions) {
+				if(suggestions && suggestions.length && !hasCancelledSuggestions) {
 					// stop cursor from jumping to the begining/end of the input when showing suggestions
 					event.preventDefault();
 				}