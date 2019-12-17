304d22bf813012cc9b9c5f959d9a6291026c3459
Tom Najdek <tom@doppnet.com>
2019-12-17 13:34:36 +0100

    Convert <Button /> to functional component

--- src/js/component/ui/button.jsx
+++ src/js/component/ui/button.jsx
@@ -1,42 +1,40 @@
-'use strict';
-
 import cx from 'classnames';
-import Icon from './icon';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { forwardRef, memo } from 'react';
+
 import { noop } from '../../utils';
 
-class Button extends React.PureComponent {
-	render() {
-		const { children, onClick, className, icon, ...props } = this.props;
+const Button = memo(forwardRef((props, ref) => {
+	const { children, className, icon, ...rest } = props;
 		const classNames = cx('btn', className, {
 			'btn-icon': icon
 		});
 		return (
 			<button
 				className={ classNames }
-				onFocus={ this.props.onFocus }
-				onBlur={ this.props.onBlur }
-				onClick={ onClick } { ...props }>
+				ref={ ref }
+				{ ...rest }
+			>
 				{ children }
 			</button>
 		);
-	}
+}));
 
-	static defaultProps = {
-		onBlur: noop,
-		onClick: noop,
-		onFocus: noop,
-	}
+Button.displayName = "Button";
 
-	static propTypes = {
-		children: PropTypes.node,
-		className: PropTypes.string,
-		icon: PropTypes.bool,
-		onBlur: PropTypes.func,
-		onClick: PropTypes.func,
-		onFocus: PropTypes.func,
-	}
-}
+Button.defaultProps = {
+	onBlur: noop,
+	onClick: noop,
+	onFocus: noop,
+};
+
+Button.propTypes = {
+	children: PropTypes.node,
+	className: PropTypes.string,
+	icon: PropTypes.bool,
+	onBlur: PropTypes.func,
+	onClick: PropTypes.func,
+	onFocus: PropTypes.func,
+};
 
 export default Button;
