zotero/web-library

commit 5aa53d3731719034cefd5f707fd3d914475e4583

Merge columns properties into single object #357

src/js/utils.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import baseMappings from 'zotero-base-mappings';
-import columnSortKeyLookup from './constants/column-sort-key-lookup';
+import columnProperties from './constants/column-properties';
 import { noteAsTitle } from './common/format';
 
 const splice = (array, at, count = 0, ...items) => {
@@ -110,7 +110,7 @@ const getSortKeyForItemType = (sortKey, itemType) => {
 }
 
 const getSortKeyValue = (item, sortBy) => {
-	sortBy = columnSortKeyLookup[sortBy];
+	sortBy = columnProperties[sortBy].sortKey;
 	if(item.itemType === 'note' && sortBy === 'title') {
 		return noteAsTitle(item.note || '');
 	}

src/js/actions/items-read.js
@@ -4,7 +4,7 @@ import { SORT_ITEMS, REQUEST_ATTACHMENT_URL, RECEIVE_ATTACHMENT_URL, ERROR_ATTAC
 import api from 'zotero-api-client';
 import { extractItems } from '../common/actions';
 import { mapRelationsToItemKeys } from '../utils';
-import columnSortKeyLookup from '../constants/column-sort-key-lookup';
+import columnProperties from '../constants/column-properties';
 
 const getApi = ({ config, libraryKey }, requestType, queryConfig) => {
 	switch(requestType) {
@@ -236,7 +236,7 @@ const fetchSource = (startIndex, stopIndex) => {
 			column => 'sort' in column) || { field: 'title', sort: 'asc' };
 
 		const direction = sortDirection.toLowerCase();
-		const sort = columnSortKeyLookup[sortBy] || 'title';
+		const sort = (sortBy in columnProperties && columnProperties[sortBy].sortKey) || 'title';
 		const sortAndDirection = { start, limit, sort, direction };
 
 		switch(itemsSource) {

src/js/component/item/actions.jsx
@@ -11,7 +11,7 @@ import NewItemSelector from './actions/new-item';
 import ExportActions from './actions/export';
 import { pick } from '../../common/immutable';
 import { noop } from '../../utils';
-import columnNames from '../../constants/column-names';
+import columnProperties from '../../constants/column-properties';
 import { ADD_BY_IDENTIFIER, SORT_ITEMS } from '../../constants/modals';
 import AddByIdentifier from './actions/add-by-identifier';
 
@@ -231,8 +231,8 @@ class ItemsActions extends React.PureComponent {
 		const { isOpen } = this.state;
 		const { isNewItemAllowed, isExportAllowed } = this;
 		const sortColumn = columns.find(c => c.sort) || columns.find(c => c.field === 'title');
-		const sortColumnLabel = sortColumn.field in columnNames ?
-			columnNames[sortColumn.field] : sortColumn.field;
+		const sortColumnLabel = sortColumn.field in columnProperties ?
+			columnProperties[sortColumn.field].name : sortColumn.field;
 		const sortColumnOrder = sortColumn.sort === 'desc' ? "Descending" : "Ascending"
 
 		return (

src/js/component/item/items/column-selector.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import React, { useCallback, useState } from 'react';
 import { useSelector, useDispatch, shallowEqual } from 'react-redux';
 
-import columnNames from '../../../constants/column-names';
+import columnProperties from '../../../constants/column-properties';
 import Dropdown from 'reactstrap/lib/Dropdown';
 import DropdownItem from 'reactstrap/lib/DropdownItem';
 import DropdownMenu from 'reactstrap/lib/DropdownMenu';
@@ -79,7 +79,7 @@ const ColumnSelector = props => {
 							onClick={ handleSelect }
 						>
 							<span className="tick">{ column.isVisible ? "✓" : "" }</span>
-							{ column.field in columnNames ? columnNames[column.field] : column.field }
+							{ column.field in columnProperties ? columnProperties[column.field].name : column.field }
 						</DropdownItem>
 					))
 				}

src/js/component/item/items/table-header-row.jsx
@@ -3,8 +3,7 @@ import React, { forwardRef, memo, useCallback, useEffect, useRef } from 'react';
 import { useDispatch } from 'react-redux';
 
 import Cell from './table-cell';
-import columnNames from '../../../constants/column-names';
-import columnSortKeyLookup from '../../../constants/column-sort-key-lookup';
+import columnProperties from '../../../constants/column-properties';
 import Icon from '../../ui/icon';
 import { isTriggerEvent } from '../../../common/event';
 import { updateItemsSorting } from '../../../actions';
@@ -21,7 +20,7 @@ const HeaderRow = memo(forwardRef((props, ref) => {
 	const handleCellClickAndKeyDown = ev => {
 		if(isTriggerEvent(ev)) {
 			const { columnName } = ev.currentTarget.dataset;
-			if(isResizing || isReordering || !columnSortKeyLookup[columnName]) {
+			if(isResizing || isReordering || !(columnName in columnProperties) || !columnProperties[columnName].sortKey) {
 				return;
 			}
 
@@ -125,7 +124,7 @@ const HeaderRow = memo(forwardRef((props, ref) => {
 							{
 								c.field === 'attachment' ?
 									<Icon type={ '16/attachment' } width="16" height="16" /> :
-									c.field in columnNames ? columnNames[c.field] : c.field
+									c.field in columnProperties ? columnProperties[c.field].name : c.field
 							}
 						</div>
 						{ sortBy === c.field &&

src/js/component/modal/items-sort-modal.jsx
@@ -5,8 +5,7 @@ import PropTypes from 'prop-types';
 import Modal from '../ui/modal';
 import Button from '../ui/button';
 import RadioSet from '../form/radio-set';
-import columnNames from '../../constants/column-names';
-import columnSortKeyLookup from '../../constants/column-sort-key-lookup';
+import columnProperties from '../../constants/column-properties';
 
 class ItemsSortModal extends React.PureComponent {
 	state = { sortColumn: null }
@@ -48,9 +47,9 @@ class ItemsSortModal extends React.PureComponent {
 
 	render() {
 		const { isOpen, toggleModal, device } = this.props;
-		const radioSetOptions = Object.entries(columnNames)
-			.map(([value, label]) => ({ value, label }))
-			.filter(({ value }) => columnSortKeyLookup[value]); // skip unsortable columns
+		const radioSetOptions = Object.entries(columnProperties)
+			.map(([value, properties]) => ({ value, label: properties.name }))
+			.filter(({ value }) => columnProperties[value].sortKey); // skip unsortable columns
 
 		return (
 			<Modal

src/js/constants/column-names.js
@@ -1,14 +1,0 @@
-export default {
-	'attachment': 'Attachment',
-	'createdByUser': 'Added By',
-	'creator': 'Creator',
-	'date': 'Date',
-	'dateAdded': 'Date Added',
-	'dateModified': 'Date Modified',
-	'extra': 'Extra',
-	'itemType': 'Item Type',
-	'publicationTitle': 'Publication Title',
-	'publisher': 'Publisher',
-	'title': 'Title',
-	'year': 'Year',
-};

src/js/constants/column-properties.js
@@ -1,0 +1,51 @@
+export default Object.freeze({
+	attachment: {
+		name: 'Attachment',
+		sortKey: false,
+	},
+	createdByUser: {
+		name: 'Added By',
+		sortKey: 'addedBy',
+		excludeInMyLibrary: true,
+	},
+	creator: {
+		name: 'Creator',
+		sortKey: 'creator',
+	},
+	date: {
+		name: 'Date',
+		sortKey: 'date',
+	},
+	dateAdded: {
+		name: 'Date Added',
+		sortKey: 'dateAdded',
+	},
+	dateModified: {
+		name: 'Date Modified',
+		sortKey: 'dateModified',
+	},
+	extra: {
+		name: 'Extra',
+		sortKey: false,
+	},
+	itemType: {
+		name: 'Item Type',
+		sortKey: 'itemType',
+	},
+	publicationTitle: {
+		name: 'Publication Title',
+		sortKey: 'publicationTitle',
+	},
+	publisher: {
+		name: 'Publisher',
+		sortKey: 'publisher',
+	},
+	title: {
+		name: 'Title',
+		sortKey: 'title',
+	},
+	year: {
+		name: 'Year',
+		sortKey: 'date',
+	},
+});

commit 9d384a2c2daa804e57b983fac3d1bc1b2b7cb837

Fix regression: unable to upload attachments
Introduced in 5aa53d3731719034cefd5f707fd3d914475e4583

src/js/utils.js
@@ -109,7 +109,7 @@ const getSortKeyForItemType = (sortKey, itemType) => {
 		baseMappings[itemType][sortKey] : sortKey;
 }
 
-const getSortKeyValue = (item, sortBy) => {
+const getSortKeyValue = (item, sortBy = 'title') => {
 	sortBy = columnProperties[sortBy].sortKey;
 	if(item.itemType === 'note' && sortBy === 'title') {
 		return noteAsTitle(item.note || '');