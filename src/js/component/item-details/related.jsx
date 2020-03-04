39cb7d8195d5bfaf3f4a03705b99538ef3a6317a
Tom Najdek <tom@doppnet.com>
2020-03-04 15:12:19 +0100

    Add key-based navigation in related tab #311 #330

--- src/js/component/item-details/related.jsx
+++ src/js/component/item-details/related.jsx
@@ -1,32 +1,37 @@
 'use strict';
 
 import PropTypes from 'prop-types';
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 
 import Button from '../ui/button';
 import Icon from '../ui/icon';
 import withDevice from '../../enhancers/with-device';
 import { getItemTitle } from '../../common/item';
+import { getUniqueId } from '../../utils';
+import { isTriggerEvent } from '../../common/event';
 import { pick } from '../../common/immutable';
 import { sortItemsByKey } from '../../utils';
 import { TabPane } from '../ui/tabs';
+import { useFocusManager } from '../../hooks';
 
 
 const RelatedItem = props => {
-	const { device, parentItemKey, relatedItem, libraryKey, removeRelatedItem, navigate } = props;
-	const handleSelect = ev => {
+	const { device, parentItemKey, relatedItem, libraryKey, removeRelatedItem, navigate, onKeyDown } = props;
+	const iconSize = device.isTouchOrSmall ? '28' : '16';
+	const id = useRef(getUniqueId());
+
+	const handleSelect = useCallback(ev => {
 		const relatedItemKey = ev.currentTarget.closest('[data-key]').dataset.key;
 		navigate({
 			library: libraryKey,
 			items: relatedItemKey
 		}, true);
-	}
-	const iconSize = device.isTouchOrSmall ? '28' : '16';
+	});
 
-	const handleDelete = ev => {
+	const handleDelete = useCallback(ev => {
 		const relatedItemKey = ev.currentTarget.closest('[data-key]').dataset.key;
 		removeRelatedItem(parentItemKey, relatedItemKey);
-	}
+	});
 
 	const getItemIcon = item => {
 		const { iconName } = item[Symbol.for('derived')];
@@ -37,19 +42,27 @@ const RelatedItem = props => {
 
 	return (
 			<li
+				aria-labelledby={ id.current }
 				className="related"
 				data-key={ relatedItem.key }
 				key={ relatedItem.key }
+				onKeyDown={ onKeyDown }
+				role="listitem button"
+				tabIndex={ -2 }
 			>
 				<Icon
 					type={ getItemIcon(relatedItem) }
 					width={ iconSize }
 					height={ iconSize }
 				/>
-				<a onClick={ handleSelect }>
+				<a id={ id.current } onClick={ handleSelect }>
 					{ getItemTitle(relatedItem) }
 				</a>
-				<Button icon onClick={ handleDelete }>
+				<Button icon
+					aria-label="remove related"
+					onClick={ handleDelete }
+					tabIndex={ -3 }
+				>
 					<Icon type={ '16/minus-circle' } width="16" height="16" />
 				</Button>
 			</li>
@@ -67,6 +80,9 @@ RelatedItem.propTypes = {
 
 const Related = ({ device, fetchRelatedItems, itemKey, isFetched, isFetching, relatedItems, ...props }) => {
 	const [sortedRelatedItems, setSortedRelatedItems] = useState([]);
+	const scrollContainerRef = useRef(null);
+	const { handleBlur, handleDrillDownPrev, handleDrillDownNext, handleFocus, handleNext,
+		handlePrevious } = useFocusManager(scrollContainerRef, { isCarousel: false });
 
 	useEffect(() => {
 		if(!isFetching && !isFetched) {
@@ -80,10 +96,31 @@ const Related = ({ device, fetchRelatedItems, itemKey, isFetched, isFetching, re
 		setSortedRelatedItems(sortedRelatedItems);
 	}, [relatedItems])
 
+	const handleKeyDown = useCallback(ev => {
+		if(ev.key === "ArrowLeft") {
+			handleDrillDownPrev(ev);
+		} else if(ev.key === "ArrowRight") {
+			handleDrillDownNext(ev);
+		} else if(ev.key === 'ArrowDown') {
+			ev.target === ev.currentTarget && handleNext(ev);
+		} else if(ev.key === 'ArrowUp') {
+			ev.target === ev.currentTarget && handlePrevious(ev);
+		} else if(isTriggerEvent(ev)) {
+			ev.target.querySelector('a').click();
+			ev.preventDefault();
+		}
+	});
+
 	return (
 		<TabPane { ...pick(props, ['isActive']) } isLoading={ device.shouldUseTabs && !isFetched }>
 			<h5 className="h2 tab-pane-heading hidden-mouse">Related</h5>
-			<div className="scroll-container-mouse">
+			<div
+				className="scroll-container-mouse"
+				onBlur={ handleBlur }
+				onFocus={ handleFocus }
+				ref={ scrollContainerRef }
+				tabIndex={ 0 }
+			>
 				{ sortedRelatedItems.length > 0 && (
 					<nav>
 						<ul className="details-list related-list">
@@ -94,6 +131,7 @@ const Related = ({ device, fetchRelatedItems, itemKey, isFetched, isFetching, re
 										key={ relatedItem.key }
 										parentItemKey={ itemKey }
 										relatedItem={ relatedItem }
+										onKeyDown={ handleKeyDown }
 										{ ...pick(props, ['libraryKey', 'navigate', 'removeRelatedItem']) }
 									/>
 								))
