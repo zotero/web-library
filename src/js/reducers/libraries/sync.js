zotero/web-library

commit 4daa08a217ae584441f197d6723d7f9be91e139e

Few fixes for multi-request operations #383
* Fixes an issue where copy operation is interruped if current library
  is changed
* Fixes an issue where if current library is changed during the copy
  operation, next operation sometimes triggers an out-of-sync library
  reset
* Fixes an issue where create attachment(s) operation is interruped if
  current library is changed

src/js/reducers/libraries/sync.js
@@ -13,8 +13,12 @@ const sync = (state = defaultState, action) => {
 		newState.requestsPending++;
 	} else if(action.type && action.type.startsWith('RECEIVE_')) {
 		newState.requestsPending--;
-		if(action.response && action.response.response) {
-			newState.version = action.response.getVersion() || state.version;
+		if('responses' in action && action.responses && action.responses.length) {
+			newState.version = (action.responses.reduce((curMax, response) =>
+				response.getVersion() > curMax.getVersion() ? response : curMax, action.responses[0])
+			).getVersion();
+		} else if(action.response && action.response.response) {
+			newState.version = Math.max(action.response.getVersion(), state.version);
 		}
 	} else if(action.type && action.type.startsWith('ERROR_')) {
 		newState.requestsPending--;

src/js/actions/items-read.js
@@ -59,12 +59,13 @@ const getApi = ({ config, libraryKey }, requestType, queryConfig) => {
 const fetchItems = (
 	type,
 	queryConfig,
-	queryOptions = {}
+	queryOptions = {},
+	overrides = {}
 ) => {
 	return async (dispatch, getState) => {
 		const state = getState();
-		const config = state.config;
-		const { libraryKey } = state.current;
+		const config = 'config' in overrides ? overrides.config : state.config;
+		const { libraryKey } = 'current' in overrides ? overrides.current : state.current;
 		const api = getApi({ config, libraryKey }, type, queryConfig);
 
 		dispatch({
@@ -95,42 +96,42 @@ const fetchItems = (
 	}
 }
 
-const fetchTopItems = queryOptions => {
-	return fetchItems('TOP_ITEMS', {}, queryOptions);
+const fetchTopItems = (queryOptions, overrides) => {
+	return fetchItems('TOP_ITEMS', {}, queryOptions, overrides);
 }
 
-const fetchTrashItems = queryOptions => {
-	return fetchItems('TRASH_ITEMS', {}, queryOptions);
+const fetchTrashItems = (queryOptions, overrides) => {
+	return fetchItems('TRASH_ITEMS', {}, queryOptions, overrides);
 }
 
-const fetchPublicationsItems = queryOptions => {
-	return fetchItems('PUBLICATIONS_ITEMS', {}, queryOptions);
+const fetchPublicationsItems = (queryOptions, overrides) => {
+	return fetchItems('PUBLICATIONS_ITEMS', {}, queryOptions, overrides);
 }
 
-const fetchItemsInCollection = (collectionKey, queryOptions) => {
-	return fetchItems('ITEMS_IN_COLLECTION', { collectionKey }, queryOptions);
+const fetchItemsInCollection = (collectionKey, queryOptions, overrides) => {
+	return fetchItems('ITEMS_IN_COLLECTION', { collectionKey }, queryOptions, overrides);
 }
 
-const fetchItemsQuery = (query, queryOptions) => {
+const fetchItemsQuery = (query, queryOptions, overrides) => {
 	const { collectionKey = null, tag = null, q = null, qmode = null, isTrash,
 		isMyPublications } = query;
 	const queryConfig = { collectionKey, isTrash, isMyPublications };
 	return fetchItems(
-		'ITEMS_BY_QUERY', queryConfig, { ...queryOptions, tag, q, qmode }
+		'ITEMS_BY_QUERY', queryConfig, { ...queryOptions, tag, q, qmode }, overrides
 	);
 }
 
-const fetchChildItems = (itemKey, queryOptions) => {
-	return fetchItems('CHILD_ITEMS', { itemKey }, queryOptions);
+const fetchChildItems = (itemKey, queryOptions, overrides) => {
+	return fetchItems('CHILD_ITEMS', { itemKey }, queryOptions, overrides);
 }
 
-const fetchItemsByKeys = (itemKeys, queryOptions) => {
+const fetchItemsByKeys = (itemKeys, queryOptions, overrides) => {
 	return fetchItems(
-		'FETCH_ITEMS', {}, { ...queryOptions, itemKey: itemKeys.join(','), }
+		'FETCH_ITEMS', {}, { ...queryOptions, itemKey: itemKeys.join(','), }, overrides
 	);
 }
 
-const fetchRelatedItems = (itemKey, queryOptions) => {
+const fetchRelatedItems = (itemKey, queryOptions, overrides) => {
 	return async (dispatch, getState) => {
 		const state = getState();
 		const { libraryKey } = state.current;
@@ -157,7 +158,7 @@ const fetchRelatedItems = (itemKey, queryOptions) => {
 		}
 
 		dispatch(fetchItems(
-			'RELATED_ITEMS', { itemKey }, { ...queryOptions, itemKey: relatedItemsKeys.join(',') }
+			'RELATED_ITEMS', { itemKey }, { ...queryOptions, itemKey: relatedItemsKeys.join(',') }, overrides
 		));
 	}
 }

src/js/actions/items-write.js
@@ -50,6 +50,9 @@ import {
 	REQUEST_STORE_RELATIONS_IN_SOURCE,
 	REQUEST_UPDATE_ITEM,
 	REQUEST_UPLOAD_ATTACHMENT,
+	REQUEST_REGISTER_FILE_ATTACHMENTS,
+	RECEIVE_REGISTER_FILE_ATTACHMENTS,
+	ERROR_REGISTER_FILE_ATTACHMENTS,
 } from '../constants/actions';
 
 const postItemsMultiPatch = async (state, multiPatch) => {
@@ -370,10 +373,9 @@ const queueUpdateItem = (itemKey, patch, libraryKey, queueId) => {
 	};
 }
 
-const uploadAttachment = (itemKey, fileData) => {
+const uploadAttachment = (itemKey, fileData, libraryKey) => {
 	return async (dispatch, getState) => {
 		const state = getState();
-		const { libraryKey } = state.current;
 		const config = state.config;
 		dispatch({
 			type: REQUEST_UPLOAD_ATTACHMENT,
@@ -658,7 +660,7 @@ const queueAddToCollection = (itemKeys, collectionKey, libraryKey, queueId) => {
 	};
 }
 
-const copyToLibrary = (itemKeys, targetLibraryKey, targetCollectionKeys = [], extraProperties = {}) => {
+const copyToLibrary = (itemKeys, sourceLibraryKey, targetLibraryKey, targetCollectionKeys = [], extraProperties = {}) => {
 	if(!Array.isArray(targetCollectionKeys)) {
 		if(targetCollectionKeys === null) {
 			targetCollectionKeys = [];
@@ -670,7 +672,6 @@ const copyToLibrary = (itemKeys, targetLibraryKey, targetCollectionKeys = [], ex
 	return async (dispatch, getState) => {
 		const state = getState();
 		const config = state.config;
-		const { libraryKey: sourceLibraryKey } = state.current;
 		const { libraries } = state.config;
 
 		const sourceLibrary = libraries.find(l => l.key === sourceLibraryKey);
@@ -767,9 +768,30 @@ const copyToLibrary = (itemKeys, targetLibraryKey, targetCollectionKeys = [], ex
 				.items(newItem.key)
 				.registerAttachment(filename, fileSize, mtime, md5)
 				.post();
+		}).filter(Boolean);
+
+		dispatch({
+			type: REQUEST_REGISTER_FILE_ATTACHMENTS,
+			libraryKey: targetLibraryKey,
 		});
 
-		await Promise.all(registerUploadsPromises);
+		try {
+			const responses = (await Promise.all(registerUploadsPromises)).filter(Boolean);
+			responses.sort((a, b) => a.getVersion() - b.getVersion());
+
+			dispatch({
+				type: RECEIVE_REGISTER_FILE_ATTACHMENTS,
+				libraryKey: targetLibraryKey,
+				responses
+			});
+		} catch(error) {
+			dispatch({
+				type: ERROR_REGISTER_FILE_ATTACHMENTS,
+				libraryKey: targetLibraryKey,
+				error
+			});
+			throw error;
+		}
 
 		const childItemsCopyPromises = itemKeys.map(async (ik, index) => {
 			const newItem = newItems[index];
@@ -777,11 +799,11 @@ const copyToLibrary = (itemKeys, targetLibraryKey, targetCollectionKeys = [], ex
 			if(!canHaveChildItems) {
 				return [];
 			}
-			const childItems = await dispatch(fetchChildItems(ik, { start: 0, limit: 100 }));
+			const childItems = await dispatch(fetchChildItems(ik, { start: 0, limit: 100 }, { current: { libraryKey: sourceLibraryKey }}));
 			const childItemsKeys = childItems.map(c => c.key);
 			const patch = { parentItem: newItem.key };
 			if(childItemsKeys && childItemsKeys.length) {
-				return await dispatch(chunkedAction(copyToLibrary, childItemsKeys, targetLibraryKey, [], patch));
+				return await dispatch(chunkedAction(copyToLibrary, childItemsKeys, sourceLibraryKey, targetLibraryKey, [], patch));
 			}
 			return [];
 		});
@@ -950,29 +972,27 @@ const chunkedTrashOrDelete = (itemKeys, ...args) => {
 	}
 }
 
-const chunkedCopyToLibrary = (itemKeys, ...args) => {
-	return async (dispatch, getState) => {
-		const state = getState();
+const chunkedCopyToLibrary = (itemKeys, sourceLibraryKey, ...args) => {
+	return async dispatch => {
 		const id = getUniqueId();
-		const { libraryKey } = state.current;
 
 		dispatch({
 			count: itemKeys.length,
 			id,
 			kind: 'cross-library-copy-items',
-			libraryKey,
+			libraryKey: sourceLibraryKey,
 			type: BEGIN_ONGOING,
 		});
 
 		try {
-			await dispatch(chunkedAction(copyToLibrary, itemKeys, ...args));
+			await dispatch(chunkedAction(copyToLibrary, itemKeys, sourceLibraryKey, ...args));
 		} catch(e) {
 			console.error(e);
 		} finally {
 			dispatch({
 				id,
 				kind: 'cross-library-copy-items',
-				libraryKey,
+				libraryKey: sourceLibraryKey,
 				type: COMPLETE_ONGOING,
 			});
 		}
@@ -1077,7 +1097,7 @@ const createAttachments = (filesData, { collection = null, parentItem = null } =
 
 			const uploadPromises = createdItems.map(async (item, index) => {
 				const fd = filesData[index];
-				await dispatch(uploadAttachment(item.key, fd));
+				await dispatch(uploadAttachment(item.key, fd, libraryKey));
 			});
 
 			await Promise.all(uploadPromises);
@@ -1085,7 +1105,7 @@ const createAttachments = (filesData, { collection = null, parentItem = null } =
 			const affectedParentItemKeys = createdItems.map(i => i.parentItem).filter(Boolean);
 
 			if(affectedParentItemKeys.length > 0) {
-				dispatch(fetchItemsByKeys(affectedParentItemKeys));
+				dispatch(fetchItemsByKeys(affectedParentItemKeys, {}, { current: { libraryKey }}));
 			}
 
 			return createdItems;

src/js/component/item-details/attachments.jsx
@@ -311,7 +311,7 @@ const Attachments = props => {
 			contentType: fileData.contentType
 		};
 		const item = await createItem(attachment, libraryKey);
-		await uploadAttachment(item.key, fileData);
+		await uploadAttachment(item.key, fileData, libraryKey);
 	});
 
 	const handleKeyDown = useCallback(ev => {

src/js/component/item/items/table-row.jsx
@@ -171,7 +171,7 @@ const TableRow = memo(props => {
 			if(dropResult) {
 				const { targetType, collectionKey: targetCollectionKey, libraryKey: targetLibraryKey } = dropResult;
 				if(targetLibraryKey && targetLibraryKey !== libraryKey) {
-					dispatch(chunkedCopyToLibrary(sourceItemKeys, targetLibraryKey, targetType === 'collection' ? targetCollectionKey : null));
+					dispatch(chunkedCopyToLibrary(sourceItemKeys, libraryKey, targetLibraryKey, targetType === 'collection' ? targetCollectionKey : null));
 				} else {
 					dispatch(chunkedAddToCollection(sourceItemKeys, targetCollectionKey));
 				}

src/js/component/modal/add-items-to-collections.jsx
@@ -45,7 +45,7 @@ class AddItemsToCollectionsModal extends React.PureComponent {
 		if(targetLibraryKey === libraryKey) {
 			await chunkedAddToCollection(sourceItemKeys, targetCollectionKey);
 		} else {
-			await chunkedCopyToLibrary(sourceItemKeys, targetLibraryKey, targetCollectionKey);
+			await chunkedCopyToLibrary(sourceItemKeys, libraryKey, targetLibraryKey, targetCollectionKey);
 		}
 		this.setState({ isBusy: false });
 		toggleModal(null, false);

src/js/constants/actions.js
@@ -4,6 +4,7 @@ export const CONFIGURE = 'CONFIGURE';
 export const DISMISS_ERROR = 'DISMISS_ERROR';
 export const ERROR_ADD_BY_IDENTIFIER = 'ERROR_ADD_BY_IDENTIFIER';
 export const ERROR_ADD_ITEMS_TO_COLLECTION = 'ERROR_ADD_ITEMS_TO_COLLECTION';
+export const ERROR_ALL_GROUPS = 'ERROR_ALL_GROUPS';
 export const ERROR_ATTACHMENT_URL = 'ERROR_ATTACHMENT_URL';
 export const ERROR_BIBLIOGRAPHY_COLLECTION = 'ERROR_BIBLIOGRAPHY_COLLECTION';
 export const ERROR_BIBLIOGRAPHY_ITEMS = 'ERROR_BIBLIOGRAPHY_ITEMS';
@@ -37,6 +38,7 @@ export const ERROR_META = 'ERROR_META';
 export const ERROR_MOVE_ITEMS_TRASH = 'ERROR_MOVE_ITEMS_TRASH';
 export const ERROR_PUBLICATIONS_ITEMS = 'ERROR_PUBLICATIONS_ITEMS';
 export const ERROR_RECOVER_ITEMS_TRASH = 'ERROR_RECOVER_ITEMS_TRASH';
+export const ERROR_REGISTER_FILE_ATTACHMENTS = 'ERROR_REGISTER_FILE_ATTACHMENTS';
 export const ERROR_RELATED_ITEMS = 'ERROR_RELATED_ITEMS';
 export const ERROR_REMOVE_ITEMS_FROM_COLLECTION = 'ERROR_REMOVE_ITEMS_FROM_COLLECTION';
 export const ERROR_SOURCE = 'ERROR_SOURCE';
@@ -65,6 +67,7 @@ export const PREFERENCE_CHANGE = 'PREFERENCE_CHANGE';
 export const PREFERENCES_LOAD = 'PREFERENCES_LOAD';
 export const RECEIVE_ADD_BY_IDENTIFIER = 'RECEIVE_ADD_BY_IDENTIFIER';
 export const RECEIVE_ADD_ITEMS_TO_COLLECTION = 'RECEIVE_ADD_ITEMS_TO_COLLECTION';
+export const RECEIVE_ALL_GROUPS = 'RECEIVE_ALL_GROUPS';
 export const RECEIVE_ATTACHMENT_URL = 'RECEIVE_ATTACHMENT_URL';
 export const RECEIVE_BIBLIOGRAPHY_COLLECTION = 'RECEIVE_BIBLIOGRAPHY_COLLECTION';
 export const RECEIVE_BIBLIOGRAPHY_ITEMS = 'RECEIVE_BIBLIOGRAPHY_ITEMS';
@@ -98,6 +101,7 @@ export const RECEIVE_META = 'RECEIVE_META';
 export const RECEIVE_MOVE_ITEMS_TRASH = 'RECEIVE_MOVE_ITEMS_TRASH';
 export const RECEIVE_PUBLICATIONS_ITEMS = 'RECEIVE_PUBLICATIONS_ITEMS';
 export const RECEIVE_RECOVER_ITEMS_TRASH = 'RECEIVE_RECOVER_ITEMS_TRASH';
+export const RECEIVE_REGISTER_FILE_ATTACHMENTS = 'RECEIVE_REGISTER_FILE_ATTACHMENTS';
 export const RECEIVE_RELATED_ITEMS = 'RECEIVE_RELATED_ITEMS';
 export const RECEIVE_REMOVE_ITEMS_FROM_COLLECTION = 'RECEIVE_REMOVE_ITEMS_FROM_COLLECTION';
 export const RECEIVE_SOURCE = 'RECEIVE_SOURCE';
@@ -116,6 +120,7 @@ export const RECEIVE_UPDATE_ITEM  = 'RECEIVE_UPDATE_ITEM';
 export const RECEIVE_UPLOAD_ATTACHMENT = 'RECEIVE_UPLOAD_ATTACHMENT';
 export const REQUEST_ADD_BY_IDENTIFIER = 'REQUEST_ADD_BY_IDENTIFIER';
 export const REQUEST_ADD_ITEMS_TO_COLLECTION = 'REQUEST_ADD_ITEMS_TO_COLLECTION';
+export const REQUEST_ALL_GROUPS = 'REQUEST_ALL_GROUPS';
 export const REQUEST_ATTACHMENT_URL = 'REQUEST_ATTACHMENT_URL';
 export const REQUEST_BIBLIOGRAPHY_COLLECTION = 'REQUEST_BIBLIOGRAPHY_COLLECTION';
 export const REQUEST_BIBLIOGRAPHY_ITEMS = 'REQUEST_BIBLIOGRAPHY_ITEMS';
@@ -149,6 +154,7 @@ export const REQUEST_META = 'REQUEST_META';
 export const REQUEST_MOVE_ITEMS_TRASH = 'REQUEST_MOVE_ITEMS_TRASH';
 export const REQUEST_PUBLICATIONS_ITEMS = 'REQUEST_PUBLICATIONS_ITEMS';
 export const REQUEST_RECOVER_ITEMS_TRASH = 'REQUEST_RECOVER_ITEMS_TRASH';
+export const REQUEST_REGISTER_FILE_ATTACHMENTS = 'REQUEST_REGISTER_FILE_ATTACHMENTS';
 export const REQUEST_RELATED_ITEMS = 'REQUEST_RELATED_ITEMS';
 export const REQUEST_REMOVE_ITEMS_FROM_COLLECTION = 'REQUEST_REMOVE_ITEMS_FROM_COLLECTION';
 export const REQUEST_SOURCE = 'REQUEST_SOURCE';
@@ -180,6 +186,3 @@ export const TRIGGER_RESIZE_VIEWPORT  = 'TRIGGER_RESIZE_VIEWPORT';
 export const TRIGGER_SEARCH_MODE = 'TRIGGER_SEARCH_MODE';
 export const TRIGGER_SELECT_MODE = 'TRIGGER_SELECT_MODE';
 export const TRIGGER_USER_TYPE_CHANGE = 'TRIGGER_USER_TYPE_CHANGE';
-export const REQUEST_ALL_GROUPS = 'REQUEST_ALL_GROUPS';
-export const RECEIVE_ALL_GROUPS = 'RECEIVE_ALL_GROUPS';
-export const ERROR_ALL_GROUPS = 'ERROR_ALL_GROUPS';

commit 78c5d425b46a7747940aa9253367d8e4c025ea87

Fix a regression where invalid version was stored
Introduced in 4daa08a217ae584441f197d6723d7f9be91e139e

src/js/reducers/libraries/sync.js
@@ -17,7 +17,7 @@ const sync = (state = defaultState, action) => {
 			newState.version = (action.responses.reduce((curMax, response) =>
 				response.getVersion() > curMax.getVersion() ? response : curMax, action.responses[0])
 			).getVersion();
-		} else if(action.response && action.response.response) {
+		} else if(action.response && action.response.getVersion && typeof(action.response.getVersion()) === 'number' && !Number.isNaN(action.response.getVersion())) {
 			newState.version = Math.max(action.response.getVersion(), state.version);
 		}
 	} else if(action.type && action.type.startsWith('ERROR_')) {