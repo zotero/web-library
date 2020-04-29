zotero/web-library

commit 6d328eda44756b833810bed04b77f56361d0cad5

Improve error type detection

src/js/reducers/errors.js
@@ -9,8 +9,8 @@ const isDuplicate = (error, prevError) =>
 	error.type === prevError.type && !prevError.isDismissed;
 
 const getErrorMessage = error => {
-	if(error instanceof TypeError) {
-		return `Unable to communicate with Zotero server. Please check your connection. ${error.message}.`;
+	if(error instanceof TypeError && (error.message === 'Failed to fetch' || error.nessage.startsWith('NetworkError'))) {
+		return `Unable to communicate with Zotero server. Please check your connection.`;
 	}
 	if(typeof error === 'object' && 'getResponseType' in error && error.getResponseType() === 'ErrorResponse') {
 		return error.reason ?

commit 5baadc8ef30c97b04d2ffaef90cd1c9d88e9e16e

Fix a typo causing an error to print in console
Introduced in 6d328eda44756b833810bed04b77f56361d0cad5

src/js/reducers/errors.js
@@ -9,7 +9,7 @@ const isDuplicate = (error, prevError) =>
 	error.type === prevError.type && !prevError.isDismissed;
 
 const getErrorMessage = error => {
-	if(error instanceof TypeError && (error.message === 'Failed to fetch' || error.nessage.startsWith('NetworkError'))) {
+	if(error instanceof TypeError && (error.message === 'Failed to fetch' || error.message.startsWith('NetworkError'))) {
 		return `Unable to communicate with Zotero server. Please check your connection.`;
 	}
 	if(typeof error === 'object' && 'getResponseType' in error && error.getResponseType() === 'ErrorResponse') {