00e868842cf5de101cc5db509dba73558841a4f7
Tom Najdek <tom@doppnet.com>
2020-05-20 17:11:21 +0200

    Fix handling urls that point at a child item. #391
    It will now correctly redirect to parent item and pre-select given
    note/attachment child item.

--- src/js/component/item/details.jsx
+++ src/js/component/item/details.jsx
@@ -7,7 +7,7 @@ import ItemDetailsInfoView from '../item-details/info-view';
 import ItemDetailsTabs from '../item-details/tabs';
 import TouchHeaderContainer from '../../container/touch-header';
 import withDevice from '../../enhancers/with-device';
-import { fetchItemsByKeys } from '../../actions';
+import { navigate, fetchItemsByKeys } from '../../actions';
 import { get } from '../../utils';
 
 const ItemDetails = props => {
@@ -17,12 +17,22 @@ const ItemDetails = props => {
 	const libraryKey = useSelector(state => state.current.libraryKey);
 	const item = useSelector(state => get(state, ['libraries', libraryKey, 'items', itemKey], null));
 	const isSelectMode = useSelector(state => state.current.isSelectMode);
+	const shouldRedirectToParentItem = itemKey && item && item.parentItem;
 
 	useEffect(() => {
 		if(itemKey && !item) {
 			dispatch(fetchItemsByKeys([itemKey]));
 		}
 	}, [item, itemKey]);
+
+	useEffect(() => {
+		if(shouldRedirectToParentItem) {
+			dispatch(navigate({
+				items: [item.parentItem],
+				[item.itemType === 'note' ? 'noteKey' : 'attachmentKey']: item.key,
+			}));
+		}
+	}, [shouldRedirectToParentItem]);
 
 	return (
 		<section className={ cx('item-details', { 'active': active }) }>
@@ -32,7 +42,7 @@ const ItemDetails = props => {
 			/>
 			{
 				(!device.isTouchOrSmall || (device.isTouchOrSmall && !isSelectMode))
-				&& item ? (
+				&& item && !shouldRedirectToParentItem ? (
 					<ItemDetailsTabs />
 				) : (
 					<ItemDetailsInfoView />
