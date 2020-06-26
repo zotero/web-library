zotero/web-library

commit 9aaea6ad104e068eb159caf2e3a9e4f768ec8bd0

Fix few focus management issues in attachments tab

src/js/component/item-details/attachments.jsx
@@ -22,7 +22,7 @@ import { isTriggerEvent } from '../../common/event';
 import { pluralize } from '../../common/format';
 import { TabPane } from '../ui/tabs';
 import { Toolbar, ToolGroup } from '../ui/toolbars';
-import { useFetchingState, useFocusManager } from '../../hooks';
+import { useFetchingState, useFocusManager, usePrevious } from '../../hooks';
 
 
 const AttachmentIcon = memo(({ isActive, item, size }) => {
@@ -130,7 +130,10 @@ const Attachment = memo(props => {
 	const handleDelete = useCallback(ev => {
 		ev.stopPropagation();
 		dispatch(moveToTrash([attachment.key]));
-	}, [dispatch, attachment]);
+		if(attachmentKey === attachment.key) {
+			dispatch(navigate({ attachmentKey: null }));
+		}
+	}, [dispatch, attachment, attachmentKey]);
 
 	const handleKeyDown = useCallback(ev => onKeyDown(ev), [onKeyDown]);
 
@@ -249,6 +252,8 @@ const Attachments = ({ isActive, isReadOnly }) => {
 	const { isFetching, isFetched, pointer, keys } = useFetchingState(
 		['libraries', libraryKey, 'itemsByParent', itemKey]
 	);
+	const attachmentKey = useSelector(state => state.current.attachmentKey);
+	const prevAttachmentKey = usePrevious(attachmentKey);
 
 	const allItems = useSelector(state => state.libraries[libraryKey].items);
 	const uploads = useSelector(state => get(state, ['libraries', libraryKey, 'updating', 'uploads'], []));
@@ -265,8 +270,8 @@ const Attachments = ({ isActive, isReadOnly }) => {
 
 	const scrollContainerRef = useRef(null);
 	const { focusNext, focusPrev, focusDrillDownNext, focusDrillDownPrev, receiveFocus,
-		receiveBlur, focusBySelector } = useFocusManager(
-			scrollContainerRef, null, false
+		receiveBlur, resetLastFocused, focusBySelector } = useFocusManager(
+			scrollContainerRef, '.attachment.selected', false
 		);
 
 	const fileInput = useRef(null);
@@ -383,6 +388,17 @@ const Attachments = ({ isActive, isReadOnly }) => {
 			dispatch(sourceFile('tinymce'));
 		}
 	}, [dispatch, isTinymceFetching, isTinymceFetched]);
+
+	useEffect(() => {
+		if(prevAttachmentKey !== attachmentKey && attachmentKey === null) {
+			resetLastFocused();
+			if(scrollContainerRef && scrollContainerRef.current) {
+				// When attachment has been deleted, keep focus within the the list by focusing on either
+				// first or second note (if first is being deleted) in the list
+				focusBySelector(`.attachment:first-child:not([data-key="${prevAttachmentKey}"]), .attachment:nth-child(2)`);
+			}
+		}
+	}, [focusBySelector, attachmentKey, resetLastFocused, prevAttachmentKey, receiveBlur]);
 
 	return (
 		<TabPane

commit 08513752b9a543c1dc319d09349b5cbd091669a0

Fix focus management logic breaks UI on tablets
Regression introduced in 9aaea6ad

src/js/component/item-details/attachments.jsx
@@ -16,7 +16,7 @@ import { ATTACHMENT } from '../../constants/dnd';
 import { createAttachmentsFromDropped, createItem, moveToTrash, uploadAttachment, fetchItemTemplate,
 fetchChildItems, navigate, sourceFile, openAttachment, toggleModal, updateItem } from
 '../../actions';
-import { get, getScrollContainerPageCount, getUniqueId, stopPropagation, sortByKey } from '../../utils';
+import { get, getScrollContainerPageCount, getUniqueId, stopPropagation, sortByKey, noop } from '../../utils';
 import { getFileData } from '../../common/event';
 import { isTriggerEvent } from '../../common/event';
 import { pluralize } from '../../common/format';
@@ -146,14 +146,20 @@ const Attachment = memo(props => {
 		if(ev.target !== ev.currentTarget) {
 			return;
 		}
+		if(isTouchOrSmall) {
+			return;
+		}
 
 		dispatch(navigate({ attachmentKey: attachment.key }));
 		setIsFocused(true);
-	}, [dispatch, attachment]);
+	}, [dispatch, attachment, isTouchOrSmall]);
 
 	const handleBlur = useCallback(() => {
+		if(isTouchOrSmall) {
+			return;
+		}
 		setIsFocused(false);
-	}, []);
+	}, [isTouchOrSmall]);
 
 	return drag(
 		<li
@@ -390,7 +396,7 @@ const Attachments = ({ isActive, isReadOnly }) => {
 	}, [dispatch, isTinymceFetching, isTinymceFetched]);
 
 	useEffect(() => {
-		if(prevAttachmentKey !== attachmentKey && attachmentKey === null) {
+		if(!isTouchOrSmall && prevAttachmentKey !== attachmentKey && attachmentKey === null) {
 			resetLastFocused();
 			if(scrollContainerRef && scrollContainerRef.current) {
 				// When attachment has been deleted, keep focus within the the list by focusing on either
@@ -398,7 +404,7 @@ const Attachments = ({ isActive, isReadOnly }) => {
 				focusBySelector(`.attachment:first-child:not([data-key="${prevAttachmentKey}"]), .attachment:nth-child(2)`);
 			}
 		}
-	}, [focusBySelector, attachmentKey, resetLastFocused, prevAttachmentKey, receiveBlur]);
+	}, [focusBySelector, attachmentKey, resetLastFocused, prevAttachmentKey, receiveBlur, isTouchOrSmall]);
 
 	return (
 		<TabPane
@@ -463,8 +469,8 @@ const Attachments = ({ isActive, isReadOnly }) => {
 			<div
 				aria-label="attachments list"
 				className="scroll-container-mouse"
-				onBlur={ receiveBlur }
-				onFocus={ receiveFocus }
+				onBlur={ isTouchOrSmall ? noop : receiveBlur }
+				onFocus={ isTouchOrSmall ? noop : receiveFocus }
 				ref={ scrollContainerRef }
 				role="list"
 				tabIndex={ 0 }

src/js/component/item-details/notes.jsx
@@ -14,7 +14,7 @@ import RichEditor from '../../component/rich-editor';
 import { get, scrollIntoViewIfNeeded } from '../../utils';
 import { isTriggerEvent } from '../../common/event';
 import { noteAsTitle, pluralize } from '../../common/format';
-import { getScrollContainerPageCount, sortByKey, stopPropagation } from '../../utils';
+import { getScrollContainerPageCount, noop, sortByKey, stopPropagation } from '../../utils';
 import { TabPane } from '../ui/tabs';
 import { Toolbar, ToolGroup } from '../ui/toolbars';
 import { useFetchingState, useFocusManager, usePrevious } from '../../hooks';
@@ -23,7 +23,6 @@ navigate, sourceFile } from '../../actions';
 
 const PAGE_SIZE = 100;
 
-//@TODO: convert to useDispatch hook
 const Note = memo(forwardRef((props, ref) => {
 	const dispatch = useDispatch();
 	const isTouchOrSmall = useSelector(state => state.device.isTouchOrSmall);
@@ -62,9 +61,12 @@ const Note = memo(forwardRef((props, ref) => {
 		if(ev.target !== ev.currentTarget) {
 			return;
 		}
+		if(isTouchOrSmall) {
+			return;
+		}
 
 		dispatch(navigate({ noteKey: note.key }));
-	}, [dispatch, note]);
+	}, [dispatch, isTouchOrSmall, note]);
 
 	const handleKeyDown = useCallback(ev => {
 		if(ev.key === 'Escape' && isDropdownOpen) {
@@ -273,15 +275,15 @@ const Notes = ({ isActive, isReadOnly }) => {
 	}, [dispatch, itemKey, isActive, isFetching, isFetched, pointer]);
 
 	useEffect(() => {
-		if(isActive && noteKey && editorRef.current && addedNoteKey.current === noteKey) {
+		if(!isTouchOrSmall && isActive && noteKey && editorRef.current && addedNoteKey.current === noteKey) {
 			editorRef.current.focus();
 			focusBySelector(`[data-key="${noteKey}"]`);
 			addedNoteKey.current = null;
 		}
-	}, [focusBySelector, isActive, noteKey, notes]);
+	}, [focusBySelector, isActive, isTouchOrSmall, noteKey, notes]);
 
 	useEffect(() => {
-		if(prevNoteKey !== noteKey && noteKey === null) {
+		if(!isTouchOrSmall && prevNoteKey !== noteKey && noteKey === null) {
 			resetLastFocused();
 			if(notesEl && notesEl.current) {
 				// When note has been deleted, keep focus within the the list by focusing on either
@@ -289,7 +291,7 @@ const Notes = ({ isActive, isReadOnly }) => {
 				focusBySelector(`.note:first-child:not([data-key="${prevNoteKey}"]), .note:nth-child(2)`);
 			}
 		}
-	}, [focusBySelector, noteKey, resetLastFocused, prevNoteKey, receiveBlur]);
+	}, [focusBySelector, noteKey, isTouchOrSmall, resetLastFocused, prevNoteKey, receiveBlur]);
 
 	// Scroll selected note into view when it's first ready.
 	useEffect(() => {
@@ -339,8 +341,8 @@ const Notes = ({ isActive, isReadOnly }) => {
 			)}
 			<div
 				className="scroll-container-mouse"
-				onBlur={ receiveBlur }
-				onFocus={ receiveFocus }
+				onBlur={ isTouchOrSmall ? noop : receiveBlur }
+				onFocus={ isTouchOrSmall ? noop : receiveFocus }
 				ref={ notesEl }
 				tabIndex={ 0 }
 			>

src/js/component/item-details/related.jsx
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import Button from '../ui/button';
 import Icon from '../ui/icon';
-import { fetchRelatedItems, navigate, removeRelatedItem } from '../../actions';
+import { fetchRelatedItems, navigate, noop, removeRelatedItem } from '../../actions';
 import { getItemTitle } from '../../common/item';
 import { getScrollContainerPageCount, sortItemsByKey } from '../../utils';
 import { getUniqueId, get, mapRelationsToItemKeys } from '../../utils';
@@ -89,6 +89,7 @@ const Related = ({ isActive }) => {
 	const isFetching = useSelector(state => get(state, ['libraries', libraryKey, 'itemsRelated', itemKey, 'isFetching'], false));
 	const isFetched = useSelector(state => get(state, ['libraries', libraryKey, 'itemsRelated', itemKey, 'isFetched'], false));
 	const allItems = useSelector(state => state.libraries[libraryKey].items);
+	const isTouchOrSmall = useSelector(state => state.device.isTouchOrSmall);
 	const relatedItems = (relatedKeys || [])
 		.map(relatedKey => allItems[relatedKey])
 		.filter(Boolean);
@@ -142,8 +143,8 @@ const Related = ({ isActive }) => {
 			<h5 className="h2 tab-pane-heading hidden-mouse">Related</h5>
 			<div
 				className="scroll-container-mouse"
-				onBlur={ receiveBlur }
-				onFocus={ receiveFocus }
+				onBlur={ isTouchOrSmall ? noop : receiveBlur }
+				onFocus={ isTouchOrSmall ? noop : receiveFocus }
 				ref={ scrollContainerRef }
 				tabIndex={ 0 }
 			>

src/js/component/item-details/tags.jsx
@@ -5,7 +5,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import Button from '../ui/button';
 import Editable from '../editable';
 import Icon from '../ui/icon';
-import { deduplicateByKey, get, getScrollContainerPageCount, sortByKey } from '../../utils';
+import { deduplicateByKey, get, getScrollContainerPageCount, noop, sortByKey } from '../../utils';
 import { fetchTagSuggestions, updateItem } from '../../actions';
 import { pick } from '../../common/immutable';
 import { TabPane } from '../ui/tabs';
@@ -164,11 +164,11 @@ const Tags = props => {
 	}, [focusBySelector, focusDrillDownNext, focusDrillDownPrev, focusNext, focusPrev, resetLastFocused]);
 
 	const handleFocusOnContainer = useCallback(ev => {
-		if(ev.target.nodeName === 'INPUT') {
+		if(ev.target.nodeName === 'INPUT' || isTouchOrSmall) {
 			return;
 		}
 		receiveFocus(ev);
-	}, [receiveFocus]);
+	}, [receiveFocus, isTouchOrSmall]);
 
 	return (
 		<TabPane
@@ -201,7 +201,7 @@ const Tags = props => {
 				) }
 			<div
 				className="scroll-container-mouse"
-				onBlur={ receiveBlur }
+				onBlur={ isTouchOrSmall ? noop : receiveBlur }
 				onFocus={ handleFocusOnContainer }
 				ref={ scrollContainerRef }
 				tabIndex={ 0 }