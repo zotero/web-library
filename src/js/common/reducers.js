zotero/web-library

commit fc2338e7dc109cfadecd129772ea3008aac61511

Fix a bug when sorting is set to "Added By" column
With sorting set to "Added By" column, certain actions would trigger an
error because field for "Added By" is called "createdByUser" but sortKey
for that column is "addedBy". Field-to-sortKey lookup was done
correctly, however reverse lookup when receiving data from the API was
not handled thus causing an issue.

src/js/common/reducers.js
@@ -1,7 +1,7 @@
 import { sortItemsByKey, compareItem } from '../utils';
 import { omit } from '../common/immutable';
 import { shallowEqual } from 'react-redux';
-import { get } from '../utils';
+import { get, getFieldNameFromSortKey } from '../utils';
 
 const replaceDuplicates = (entries, comparer = null, useSplice = false) => {
 	const seen = [];
@@ -134,7 +134,7 @@ const populate = (state, newItems, action, keyName, comparer = null) => {
 		...state,
 		[keyName]: replaceDuplicates(items, comparer),
 		totalResults,
-		sortBy: sort,
+		sortBy: getFieldNameFromSortKey(sort),
 		sortDirection: direction,
 		isFetching: false,
 		isError: false,

src/js/component/library.jsx
@@ -44,6 +44,7 @@ const Library = props => {
 	const wasSearchMode = useRef(isSearchMode);
 	const prevItemsSource = useRef(itemsSource);
 	const wasSynced = useRef(isSynced);
+	const isSearchQuery = search && search.length > 0;
 
 	useEffect(() => {
 		if(device.userType !== prevUserType.current) {
@@ -109,10 +110,10 @@ const Library = props => {
 			<div className={ cx('library-container', {
 					'navbar-nav-opened': isNavBarOpen,
 					'no-transitions': !useTransitions || hasUserTypeChanged,
-					'search-active': (isSearchMode || (!isSearchMode && isSearchModeTransitioning)) && itemsSource !== 'query',
+					'search-active': (isSearchMode || (!isSearchMode && isSearchModeTransitioning)),
 					'search-cancel': isSearchModeTransitioning && !isSearchMode,
 					'search-init': (isSearchMode && !searchState.hasViewedResult) || (isSearchModeTransitioning && !isSearchMode),
-					'search-results': (isSearchMode || (!isSearchMode && isSearchModeTransitioning)) && (itemsSource === 'query' || prevItemsSource.current === 'query'),
+					'search-results': (isSearchMode || (!isSearchMode && isSearchModeTransitioning)) && itemsSource === 'query' && isSearchQuery,
 					'touch-tag-selector-active': tags.length > 0,
 					'view-note-active': noteKey,
 					'view-attachment-active': attachmentKey,

src/js/utils.js
@@ -375,6 +375,18 @@ const getLibraryKeyFromTopic = topic => {
 	return null;
 }
 
+const getFieldNameFromSortKey = sortKey => {
+	const matchingKeyValuePair = Object.entries(columnProperties)
+		.find(([_, properties]) => properties.sortKey === sortKey);
+
+	if(matchingKeyValuePair) {
+		return matchingKeyValuePair[0];
+	} else {
+		console.warn(`Unrecognized sort key ${sortKey}. Using title instead.`);
+		return 'title';
+	}
+}
+
 export {
 	applyChangesToVisibleColumns,
 	clamp,
@@ -388,6 +400,7 @@ export {
 	enumerateObjects,
 	get,
 	getDOIURL,
+	getFieldNameFromSortKey,
 	getItemCanonicalUrl,
 	getLibraryKeyFromTopic,
 	getScrollbarWidth,

commit 7bb637b4980340155e17e8540628e3d966a7c678

Fix a bogus error message when adding attachments
Regression caused by fc2338e7.

src/js/common/reducers.js
@@ -134,7 +134,7 @@ const populate = (state, newItems, action, keyName, comparer = null) => {
 		...state,
 		[keyName]: replaceDuplicates(items, comparer),
 		totalResults,
-		sortBy: sort ? getFieldNameFromSortKey(sort) : null,
+		sortBy: sort ? getFieldNameFromSortKey(sort) : undefined,
 		sortDirection: direction,
 		isFetching: false,
 		isError: false,