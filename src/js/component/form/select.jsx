6a3d778709394a04a10b010c0149d652c8fbe4ee
Tom Najdek <tom@doppnet.com>
2020-11-16 20:48:02 +0100

    Refactor SelectInput component

--- src/js/component/form/select.jsx
+++ src/js/component/form/select.jsx
@@ -1,124 +1,92 @@
-/* eslint-disable react/no-deprecated */
-'use strict';
-
-import React from 'react';
-import PropTypes from 'prop-types';
 import cx from 'classnames';
-import { noop } from '../../utils';
-import Spinner from '../ui/spinner';
+import PropTypes from 'prop-types';
+import React, { forwardRef, memo, useCallback, useEffect, useImperativeHandle, useRef, useState } from 'react';
+import { useSelector } from 'react-redux';
+
 import Select from '../ui/select';
-import withDevice from '../../enhancers/with-device';
+import Spinner from '../ui/spinner';
+import { usePrevious } from '../../hooks';
 
-class SelectInput extends React.PureComponent {
-	constructor(props) {
-		super(props);
-		this.state = {
-			value: props.value
-		};
-	}
+const SelectInput = forwardRef((props, ref) => {
+	const { autoFocus, className, clearable, isDisabled, isReadOnly, isRequired, id, placeholder, tabIndex,
+		inputGroupClassName, isBusy, value: initialValue, options, onBlur, onCancel, onCommit, onChange,
+		onFocus } = props;
+	const isTouchOrSmall = useSelector(state => state.device.isTouchOrSmall);
+	const [value, setValue]  = useState(initialValue);
+	const prevInitialValue = usePrevious(initialValue);
+	const input = useRef(null);
 
-	cancel(event = null) {
-		this.props.onCancel(this.hasChanged, event);
-	}
+	const groupClassName = cx({
+		'busy': isBusy,
+		'input-group': true,
+		'select': true,
+		[inputGroupClassName]: true
+	});
 
-	commit(event = null, value = null, force = false) {
-		this.props.onCommit(value || this.state.value, force ? true : this.hasChanged, event);
-	}
-
-	focus() {
-		if(this.input != null) {
-			this.input.focus();
+	useImperativeHandle(ref, () => ({
+		focus: () => {
+			input.current.focus();
 		}
-	}
+	}));
 
-	UNSAFE_componentWillReceiveProps({ value }) {
-		if (value !== this.props.value) {
-			this.setState({ value });
+	const handleBlur = useCallback(ev => {
+		if(onBlur) {
+			onBlur(ev);
 		}
-	}
+		if(onCancel) {
+			onCancel(value !== initialValue, ev);
+		}
+	}, [initialValue, onCancel, onBlur, value]);
 
-	handleChange(value, ev) {
-		value = value !== null || (value === null && this.props.clearable) ?
-			value : this.props.value;
+	// const handleFocus = useCallback(() => {}, []);
+	const handleKeyDown = useCallback(ev => {
+		if(ev.key === 'Escape' && onCancel) {
+			onCancel();
+		}
+	}, [onCancel]);
 
-		this.setState({ value });
+	const handleChange = useCallback((newValue, ev) => {
+		newValue = newValue !== null || (newValue === null && clearable) ?
+			newValue : initialValue;
 
-		if(this.props.onChange(value) || this.forceCommitOnNextChange) {
+		setValue(newValue);
+
+		if(onChange(newValue)) {
 			if(!ev) {
-				//@NOTE: this is using undocumeneted feature of react-selct v1, but see #131
-				const source = typeof this.input.getElement === 'function' ?
-					this.input.getElement() : this.input.input;
+				const source = input.current && typeof input.current.getElement === 'function' ?
+					input.current.getElement() : input.current.input;
 				ev = {
 					type: 'change',
 					currentTarget: source,
 					target: source
 				}
 			}
-			this.commit(ev, value, value !== this.props.value);
+			onCommit(newValue, newValue !== initialValue, ev);
 		}
-		this.forceCommitOnNextChange = false;
-	}
+	}, [clearable, initialValue, onChange, onCommit]);
 
-	handleBlur(event) {
-		this.props.onBlur(event);
-		this.cancel(event);
-		if(this.props.autoBlur) {
-			this.forceCommitOnNextChange = true;
+	const handleNativeChange = useCallback(ev => handleChange(ev.target.value, ev), [handleChange]);
+
+	const commonProps = { disabled: isDisabled, onBlur: handleBlur, onFocus, readOnly:
+	isReadOnly, ref, required: isRequired, id, value, tabIndex };
+
+	useEffect(() => {
+		if(initialValue !== prevInitialValue && initialValue !== value) {
+			setValue(initialValue);
 		}
-	}
+	}, [initialValue, value, prevInitialValue]);
 
-	handleFocus(event) {
-		this.props.onFocus(event);
-	}
-
-	handleKeyDown(event) {
-		switch (event.key) {
-			case 'Escape':
-				this.cancel(event);
-			break;
-		default:
-			return;
-		}
-	}
-
-	get hasChanged() {
-		return this.state.value !== this.props.value;
-	}
-
-	get defaultSelectProps() {
-		return { searchable: false };
-	}
-
-	renderInput(device) {
-		const {
-			options,
-			autoFocus,
-			className,
-			id,
-			placeholder,
-			tabIndex,
-			value,
-		} = this.props;
-
-		const commonProps = {
-			disabled: this.props.isDisabled,
-			onBlur: this.handleBlur.bind(this),
-			onFocus: this.handleFocus.bind(this),
-			readOnly: this.props.isReadOnly,
-			ref: input => this.input = input,
-			required: this.props.isRequired,
-		};
-
-		if(device.isTouchOrSmall) {
-			const props = {
-				...commonProps,
-				onKeyDown: this.handleKeyDown.bind(this),
-				onChange: ev => this.handleChange(ev.target.value, ev),
-				autoFocus, id, placeholder, tabIndex, value
-			};
-			return (
+	return (
+		<div className={ groupClassName }>
+			{ isTouchOrSmall ? (
 				<div className="native-select-wrap" >
-					<select { ...props }>
+					<select
+						{ ...commonProps }
+						{ ...({ autoFocus, placeholder })}
+						onKeyDown={ handleKeyDown }
+						onChange={ handleNativeChange }
+						ref={ input }
+					>
 						{ options.map(({ value, label }) => (
 							<option key={ value } value={ value }>{ label }</option>)
 						)}
@@ -127,69 +95,40 @@ class SelectInput extends React.PureComponent {
 						{ (options.find(o => o.value === value) || options[0] || {}).label }
 					</div>
 				</div>
+			) : (
+				<Select
+					{ ...props }
+					{ ...commonProps }
+					onInputKeyDown={ handleKeyDown }
+					onChange={ handleChange }
+					ref={ input }
+				/>
+			) }
+			{ isBusy ? <Spinner className="small" /> : null }
+		</div>
+	);
+});
 
-			);
-		} else {
-			const props = {
-				...this.defaultSelectProps,
-				...this.props,
-				...commonProps,
-				onInputKeyDown: this.handleKeyDown.bind(this),
-				onChange: this.handleChange.bind(this),
-			};
+SelectInput.propTypes = {
+	autoFocus: PropTypes.bool,
+	className: PropTypes.string,
+	clearable: PropTypes.bool,
+	id: PropTypes.string,
+	inputGroupClassName: PropTypes.string,
+	isBusy: PropTypes.bool,
+	isDisabled: PropTypes.bool,
+	isReadOnly: PropTypes.bool,
+	isRequired: PropTypes.bool,
+	onBlur: PropTypes.func,
+	onCancel: PropTypes.func,
+	onChange: PropTypes.func.isRequired,
+	onCommit: PropTypes.func.isRequired,
+	onFocus: PropTypes.func,
+	options: PropTypes.array.isRequired,
+	placeholder: PropTypes.string,
+	searchable: PropTypes.bool,
+	tabIndex: PropTypes.number,
+	value: PropTypes.string.isRequired,
+};
 
-			return <Select { ...props } />;
-		}
-	}
-
-	renderSpinner() {
-		return this.props.isBusy ? <Spinner className="small" /> : null;
-	}
-
-	render() {
-		const className = cx({
-			'input-group': true,
-			'select': true,
-			'busy': this.props.isBusy
-		}, this.props.inputGroupClassName);
-		return (
-			<div className={ className }>
-				{ this.renderInput(this.props.device) }
-				{ this.renderSpinner() }
-			</div>
-		);
-	}
-
-	static defaultProps = {
-		className: 'form-control',
-		onBlur: noop,
-		onCancel: noop,
-		onChange: noop,
-		onCommit: noop,
-		onFocus: noop,
-		options: [],
-		value: '',
-	};
-
-	static propTypes = {
-		autoFocus: PropTypes.bool,
-		className: PropTypes.string,
-		id: PropTypes.string,
-		inputGroupClassName: PropTypes.string,
-		isBusy: PropTypes.bool,
-		isDisabled: PropTypes.bool,
-		isReadOnly: PropTypes.bool,
-		isRequired: PropTypes.bool,
-		onBlur: PropTypes.func.isRequired,
-		onCancel: PropTypes.func.isRequired,
-		onChange: PropTypes.func.isRequired,
-		onCommit: PropTypes.func.isRequired,
-		onFocus: PropTypes.func.isRequired,
-		options: PropTypes.array.isRequired,
-		placeholder: PropTypes.string,
-		tabIndex: PropTypes.number,
-		value: PropTypes.string.isRequired,
-	};
-}
-
-export default withDevice(SelectInput);
+export default memo(SelectInput);
