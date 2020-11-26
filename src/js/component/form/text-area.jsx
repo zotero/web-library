765a69eb32300ee76fb25ce7b72abb1c0513273c
Tom Najdek <tom@doppnet.com>
2020-11-26 14:44:42 +0100

    Refactor & optimise TextAreaInput component
    Remove legacy UNSAFE_componentWillReceiveProps method.

--- src/js/component/form/text-area.jsx
+++ src/js/component/form/text-area.jsx
@@ -1,187 +1,143 @@
-/* eslint-disable react/no-deprecated */
-'use strict';
-
-import React from 'react';
-import PropTypes from 'prop-types';
 import cx from 'classnames';
-import { noop } from '../../utils';
+import PropTypes from 'prop-types';
+import React, { forwardRef, memo, useCallback, useEffect, useImperativeHandle, useRef, useState } from 'react';
+
 import AutoResizer from './auto-resizer';
 import Spinner from '../ui/spinner';
+import { noop } from '../../utils';
+import { pick } from '../../common/immutable';
+import { usePrevious } from '../../hooks';
 
-class TextAreaInput extends React.PureComponent {
-	constructor(props) {
-		super(props);
-		this.state = {
-			value: props.value
-		};
-	}
+const NATIVE_INPUT_PROPS = ['autoComplete', 'autoFocus', 'cols', 'disabled', 'form', 'id',
+'maxLength', 'minLength', 'name', 'placeholder', 'readOnly', 'required', 'rows', 'spellCheck',
+'tabIndex', 'wrap'];
 
-	cancel(event = null) {
-		this.hasBeenCancelled = true;
-		this.props.onCancel(this.hasChanged, event);
-	}
+const TextAreaInput = memo(forwardRef((props, ref) => {
+	const { className, inputGroupClassName, isBusy, value: initialValue, isSingleLine, onBlur = noop, onFocus =
+	noop, onCancel = noop, onChange = noop, onCommit = noop, selectOnFocus, resize, ...rest } = props;
+	const [value, setValue] = useState(initialValue);
+	const prevInitialValue = usePrevious(initialValue);
 
-	commit(event = null) {
-		this.props.onCommit(this.state.value, this.hasChanged, event);
-		this.hasBeenCommitted = true;
-	}
+	const input = useRef(null);
+	const hasBeenCancelled = useRef(false);
+	const hasBeenCommitted = useRef(false);
 
-	focus() {
-		if(this.input != null) {
-			this.input.focus();
-			this.props.selectOnFocus && this.input.select();
+	const groupClassName = cx({
+		'input-group': true,
+		'textarea': true,
+		'busy': isBusy
+	}, inputGroupClassName);
+
+	//reset on every render
+	hasBeenCancelled.current = false;
+	hasBeenCommitted.current = false;
+
+	useImperativeHandle(ref, () => ({
+		focus: () => {
+			input.current.focus();
+			if(selectOnFocus) {
+				input.current.select();
+			}
 		}
-	}
+	}));
 
-	UNSAFE_componentWillReceiveProps({ value }) {
-		if (value !== this.props.value) {
-			this.setState({ value });
+	const handleChange = useCallback(ev => {
+		setValue(ev.currentTarget.value);
+		onChange(ev.currentTarget.value);
+	}, [onChange]);
+
+	const handleBlur = useCallback(ev => {
+		const shouldCancel = onBlur(ev);
+		if (hasBeenCancelled.current || hasBeenCommitted.current) {
+			return;
 		}
-	}
+		if(shouldCancel) {
+			onCancel(value !== initialValue, ev);
+			hasBeenCancelled.current = true;
+		} else {
+			onCommit(value, value !== initialValue, ev);
+			hasBeenCommitted.current = true;
+		}
+	}, [onBlur, initialValue, onCancel, onCommit, value]);
 
-	handleChange({ target }) {
-		this.setState({ value: target.value });
-		this.props.onChange(target.value);
-	}
+	const handleFocus = useCallback(ev => {
+		if(selectOnFocus) {
+			ev.currentTarget.select();
+		} else {
+			onFocus(ev);
+		}
+	}, [onFocus, selectOnFocus]);
 
-	handleBlur(event) {
-		const shouldCancel = this.props.onBlur(event);
-		if (this.hasBeenCancelled || this.hasBeenCommitted) { return; }
-		shouldCancel ? this.cancel(event) : this.commit(event);
-	}
-
-	handleFocus(event) {
-		this.props.selectOnFocus && event.target.select();
-		this.props.onFocus(event);
-	}
-
-	handleKeyDown(event) {
-		const { isSingleLine } = this.props;
+	const handleKeyDown = useCallback(ev => {
 		switch (event.key) {
 			case 'Escape':
-				this.cancel(event);
+				onCancel(value !== initialValue, ev);
+				hasBeenCancelled.current = true;
 			break;
 			case 'Enter':
 				if(event.shiftKey || isSingleLine) {
-					event.preventDefault();
-					this.commit(event);
+					onCommit(value, value !== initialValue, ev);
+					hasBeenCommitted.current = true;
 				}
 			break;
 		default:
 			return;
 		}
-	}
+	}, [initialValue, isSingleLine, onCancel, onCommit, value]);
 
-	get hasChanged() {
-		return this.state.value !== this.props.value;
-	}
+	useEffect(() => {
+		if(initialValue !== prevInitialValue && initialValue !== value) {
+			setValue(initialValue);
+		}
+	}, [initialValue, value, prevInitialValue]);
 
-	renderInput() {
-		this.hasBeenCancelled = false;
-		this.hasBeenCommitted = false;
+	const textarea = (
+		<textarea
+			className={ className }
+			onBlur={ handleBlur }
+			onChange={ handleChange }
+			onFocus={ handleFocus }
+			onKeyDown={ handleKeyDown }
+			ref={ input }
+			value={ value }
+			{ ...pick(rest, NATIVE_INPUT_PROPS) }
+			{ ...pick(rest, key => key.match(/^(aria-|data-).*/)) }
+		/>
+	);
 
-		const extraProps = Object.keys(this.props).reduce((aggr, key) => {
-			if(key.match(/^(aria-|data-).*/)) {
-				aggr[key] = this.props[key];
-			}
-			return aggr;
-		}, {});
-		const input = <textarea
-			autoComplete={ this.props.autoComplete }
-			autoFocus={ this.props.autoFocus }
-			className={ this.props.className }
-			cols={ this.props.cols }
-			disabled={ this.props.isDisabled }
-			form={ this.props.form }
-			id={ this.props.id }
-			maxLength={ this.props.maxLength }
-			minLength={ this.props.minLength }
-			name={ this.props.name }
-			onBlur={ this.handleBlur.bind(this) }
-			onChange={ this.handleChange.bind(this) }
-			onFocus={ this.handleFocus.bind(this) }
-			onKeyDown={ this.handleKeyDown.bind(this) }
-			placeholder={ this.props.placeholder }
-			readOnly={ this.props.isReadOnly }
-			ref={ input => this.input = input }
-			required={ this.props.isRequired }
-			rows={ this.props.rows }
-			spellCheck={ this.props.spellCheck }
-			tabIndex={ this.props.tabIndex }
-			value={ this.state.value }
-			wrap={ this.props.wrap }
-			{ ...extraProps }
-		/>;
+	return (
+		<div className={ groupClassName }>
+			{ resize ? (
+				<AutoResizer
+					content={ value }
+					vertical={ resize === 'vertical' }
+				>
+					{ textarea }
+				</AutoResizer>
+			) : textarea };
+			{ isBusy && <Spinner /> }
+		</div>
+	);
+}));
 
-		return this.props.resize ?
-			<AutoResizer
-				content={ this.state.value }
-				vertical={ this.props.resize === 'vertical' }
-			>
-				{ input }
-			</AutoResizer> :
-			input;
-	}
-
-	renderSpinner() {
-		return this.props.isBusy ? <Spinner /> : null;
-	}
-
-	render() {
-		const className = cx({
-			'input-group': true,
-			'textarea': true,
-			'busy': this.props.isBusy
-		}, this.props.inputGroupClassName);
-		return (
-			<div className={ cx(className) }>
-				{ this.renderInput() }
-				{ this.renderSpinner() }
-			</div>
-		);
-	}
-
-	static defaultProps = {
-		className: 'form-control',
-		onBlur: noop,
-		onCancel: noop,
-		onChange: noop,
-		onCommit: noop,
-		onFocus: noop,
-		tabIndex: -1,
-		value: '',
-	};
-
-	static propTypes = {
-		autoComplete: PropTypes.bool,
-		autoFocus: PropTypes.bool,
+TextAreaInput.displayName = 'TextAreaInput';
+TextAreaInput.propTypes = {
 		className: PropTypes.string,
-		cols: PropTypes.number,
 		form: PropTypes.string,
-		id: PropTypes.string,
 		inputGroupClassName: PropTypes.string,
 		isBusy: PropTypes.bool,
 		isDisabled: PropTypes.bool,
 		isReadOnly: PropTypes.bool,
 		isRequired: PropTypes.bool,
 		isSingleLine: PropTypes.bool,
-		maxLength: PropTypes.number,
-		minLength: PropTypes.number,
-		name: PropTypes.string,
-		onBlur: PropTypes.func.isRequired,
-		onCancel: PropTypes.func.isRequired,
-		onChange: PropTypes.func.isRequired,
-		onCommit: PropTypes.func.isRequired,
-		onFocus: PropTypes.func.isRequired,
-		placeholder: PropTypes.string,
+		onBlur: PropTypes.func,
+		onCancel: PropTypes.func,
+		onChange: PropTypes.func,
+		onCommit: PropTypes.func,
+		onFocus: PropTypes.func,
 		resize: PropTypes.oneOfType([PropTypes.bool, PropTypes.string]),
-		rows: PropTypes.number,
 		selectOnFocus: PropTypes.bool,
-		spellCheck: PropTypes.bool,
-		tabIndex: PropTypes.number,
-		value: PropTypes.string.isRequired,
-		wrap: PropTypes.bool,
-	};
+		value: PropTypes.string,
 }
 
 export default TextAreaInput;
