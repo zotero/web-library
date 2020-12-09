zotero/web-library

commit 39648cd8180bb04ffabad16283ad52ac3474dc5b

Avoid needless rerenders in TouchHeaderWrap

src/js/reducers/current.js
@@ -81,10 +81,10 @@ const current = (state = stateDefault, action, { config = {}, device = {} } = {}
 			var isTrash = params.source === 'trash';
 			var isMyPublications = params.source === 'publications';
 			var collectionKey = params.collection || null;
-			var itemKeys = params.items && isValidItemsKey ? params.items.split(',') : [];
+			var itemKeys = params.items && isValidItemsKey ? params.items.split(',') : stateDefault.itemKeys;
 			var noteKey = params.note || null;
 			var attachmentKey = params.attachment || null;
-			var tags = tagsFromUrlPart(params.tags);
+			var tags = tagsFromUrlPart(params.tags) || stateDefault.tags;
 			var isSelectMode = itemKeys.length > 1 ? true : state.isSelectMode;
 			var view = params.view;
 			var libraryKey = getLibraryKey(params, config);

src/js/common/navigation.js
@@ -1,11 +1,9 @@
-'use strict';
-
 const getQueryFromParams = params => {
 	const { collection, tags, search = '', qmode = 'titleCreatorYear' } = params;
-	return { collection, tag: tagsFromUrlPart(tags), q: decodeURIComponent(search), qmode };
+	return { collection, tag: tagsFromUrlPart(tags) || [], q: decodeURIComponent(search), qmode };
 }
 
-const tagsFromUrlPart = tags => tags ? tags.split(',').map(t => decodeURIComponent(t)) : [];
+const tagsFromUrlPart = tags => tags ? tags.split(',').map(t => decodeURIComponent(t)) : null;
 
 const tagsToUrlPart = tags => tags.map(t => encodeURIComponent(t)).join(',');
 

src/js/common/state.js
@@ -3,17 +3,15 @@
 import memoize from 'memoize-one';
 import deepEqual from 'deep-equal';
 import { createMatchSelector } from 'connected-react-router';
-import { get } from '../utils';
 import { routes, redirects } from '../routes';
 
-const getCollectionsPath = state => {
-	const { libraryKey, collectionKey } = state.current;
+const getCollectionsPath = ({ libraryKey, collectionKey, collectionsData = {} }) => {
 	const path = [];
 	if(libraryKey) {
 		var nextKey = collectionKey;
 
 		while(nextKey) {
-			const collection = get(state, ['libraries', libraryKey, 'collections', 'data', nextKey]);
+			const collection = collectionsData[nextKey];
 			if(collection) {
 				path.push(collection.key);
 				nextKey = collection.parentCollection;

src/js/component/touch-header-wrap.jsx
@@ -20,11 +20,11 @@ const ROOT_NODE = {
 const TouchHeaderWrap = memo(({ className, variant }) => {
 	const dispatch = useDispatch();
 	const collections = useSelector(state => get(
-		state, ['libraries', state.current.libraryKey, 'collections', 'data'], {}
+		state, ['libraries', state.current.libraryKey, 'collections', 'data'],
 	));
 	const item = useSelector(state => get(
-		state, ['libraries', state.current.libraryKey, 'items', state.current.itemKey]), {}
-	);
+		state, ['libraries', state.current.libraryKey, 'items', state.current.itemKey]
+	));
 	const attachmentKey = useSelector(state => state.current.attachmentKey);
 	const collectionKey = useSelector(state => state.current.collectionKey);
 	const isMyPublications = useSelector(state => state.current.isMyPublications);
@@ -40,12 +40,12 @@ const TouchHeaderWrap = memo(({ className, variant }) => {
 	const libraryConfig = useSelector(state => state.config.libraries.find(l => l.key === libraryKey) || {});
 	const itemKeys = useSelector(state => state.current.itemKeys);
 	const { isReadOnly } = libraryConfig;
-	const childMap = useMemo(() => makeChildMap(collections.length ? Object.values(collections) : []), [collections]);
+	const childMap = useMemo(() => makeChildMap(Object.values(collections || {})), [collections]);
 	const [isEditing, ] = useEditMode();
 
-	const path = useSelector(state => getCollectionsPath(state).map(
+	const collectionsPath = useMemo(() => getCollectionsPath(libraryKey, collectionKey, collections).map(
 		key => {
-			const { name } = collections[key]
+			const { name } = collections[key];
 			return {
 				key,
 				type: 'collection',
@@ -53,7 +53,8 @@ const TouchHeaderWrap = memo(({ className, variant }) => {
 				path: { library: libraryKey, collection: key },
 			};
 		}
-	));
+	), [collectionKey, collections, libraryKey]);
+	const path = [...collectionsPath];
 
 	if(libraryKey && view !== 'libraries') {
 		path.unshift({

commit 5d81f32a51d26027099f2440113bcda5debfb55f

Fix a regression in 39648cd8

src/js/reducers/current.js
@@ -81,10 +81,10 @@ const current = (state = stateDefault, action, { config = {}, device = {} } = {}
 			var isTrash = params.source === 'trash';
 			var isMyPublications = params.source === 'publications';
 			var collectionKey = params.collection || null;
-			var itemKeys = params.items && isValidItemsKey ? params.items.split(',') : stateDefault.itemKeys;
+			var itemKeys = params.items && isValidItemsKey ? params.items.split(',') : [];
 			var noteKey = params.note || null;
 			var attachmentKey = params.attachment || null;
-			var tags = tagsFromUrlPart(params.tags) || stateDefault.tags;
+			var tags = tagsFromUrlPart(params.tags) || [];
 			var isSelectMode = itemKeys.length > 1 ? true : state.isSelectMode;
 			var view = params.view;
 			var libraryKey = getLibraryKey(params, config);