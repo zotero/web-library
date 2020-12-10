ab6dc7ecac7491f21440358459c3fbd1e1cb92b0
Sean Takats <sean@takats.org>
2020-12-10 14:56:31 +0100

    Some tweaks to the crash handler message

--- src/js/component/crash-handler.jsx
+++ src/js/component/crash-handler.jsx
@@ -5,9 +5,9 @@ const oldWebLibraryUrl = 'https://www.zotero.org/mylibrary?usenewlibrary=0';
 
 const CrashHandler = ({ error = {}, info = {} }) => (
 	<div className="crash-handler">
-		<h1>Web Library has encountered an error</h1>
+		<h1>The Zotero web interface has encountered an error</h1>
 		<p>
-		Please try reloading this page. If the error persist, you can try using a <a href={ oldWebLibraryUrl }>legacy version</a> of Web Library. Bugs can be reported on <a href={ forumsUrl }>Zotero Forums</a>, please mention <em>Web Library</em> in the title and include details printed below.
+		Please try reloading this page. If the error persists, you can try temporarily using an <a href={ oldWebLibraryUrl }>older version</a> of the web viewer. Bugs should be reported on <a href={ forumsUrl }>Zotero Forums</a>; please mention <em>web library</em> in the title and include the details printed below.
 		</p>
 		<textarea
 			onClick={ ev => ev.currentTarget.select() }
