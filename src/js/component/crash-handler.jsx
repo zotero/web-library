dee9e55cb9fadc96775a6a1295c8aa51b45a1fd6
Tom Najdek <tom@doppnet.com>
2020-12-10 13:46:10 +0100

    Tweak wording and styling in CrashHandler

--- src/js/component/crash-handler.jsx
+++ src/js/component/crash-handler.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 
+const forumsUrl = 'https://forums.zotero.org/';
+const oldWebLibraryUrl = 'https://www.zotero.org/mylibrary?usenewlibrary=0';
+
 const CrashHandler = ({ error = {}, info = {} }) => (
 	<div className="crash-handler">
 		<h1>Web Library has encountered an error</h1>
 		<p>
-			Please try reloading this page. If the error persist, you can try using a <a href="https://www.zotero.org/mylibrary?usenewlibrary=0">legacy version</a> of Web Library. Bugs can be reported on <a href="https://forums.zotero.org/">Zotero Forums</a>, please include details printed below.
+		Please try reloading this page. If the error persist, you can try using a <a href={ oldWebLibraryUrl }>legacy version</a> of Web Library. Bugs can be reported on <a href={ forumsUrl }>Zotero Forums</a>, please mention <em>Web Library</em> in the title and include details printed below.
 		</p>
 		<textarea
 			onClick={ ev => ev.currentTarget.select() }
 			rows={ 10 }
-			cols={ 80 }
+			cols={ 120 }
 			value={ error.stack + "\n\n" + info.componentStack + "\n\n" + (navigator || {}).userAgent }
 			readOnly={ true }
 		/>
