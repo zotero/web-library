dcc07dfd78539de58e90587352db38b088d4272d
Tom Najdek <tom@doppnet.com>
2021-02-23 14:20:56 +0100

    Backoff meta requests (item type creators/fields)

--- src/js/actions/meta.js
+++ src/js/actions/meta.js
@@ -1,70 +1,61 @@
 import {
     REQUEST_ITEM_TYPE_CREATOR_TYPES,
-    RECEIVE_ITEM_TYPE_CREATOR_TYPES,
-    ERROR_ITEM_TYPE_CREATOR_TYPES,
     REQUEST_ITEM_TYPE_FIELDS,
-    RECEIVE_ITEM_TYPE_FIELDS,
-    ERROR_ITEM_TYPE_FIELDS,
     REQUEST_ITEM_TEMPLATE,
     RECEIVE_ITEM_TEMPLATE,
     ERROR_ITEM_TEMPLATE,
 } from '../constants/actions';
+import { requestTracker, requestWithBackoff } from '.';
 import apiBase from 'zotero-api-client';
 const api = apiBase().api;
 
 const fetchItemTypeCreatorTypes = itemType => {
 	return async (dispatch, getState) => {
+		const id = requestTracker.id++;
+		const config = getState().config;
+		const type = 'ITEM_TYPE_CREATOR_TYPES';
+
 		dispatch({
 			type: REQUEST_ITEM_TYPE_CREATOR_TYPES,
 			itemType
 		});
-		let config = getState().config;
-		try {
-			let creatorTypes = (await api(config.apiKey, config.apiConfig)
+
+		const makeRequest = async () => {
+			const creatorTypes = (await api(config.apiKey, config.apiConfig)
 				.itemTypeCreatorTypes(itemType)
 				.get())
-			.getData();
+				.getData();
 
-			dispatch({
-				type: RECEIVE_ITEM_TYPE_CREATOR_TYPES,
-				itemType,
-				creatorTypes
-			});
-			return creatorTypes;
-		} catch(error) {
-			dispatch({
-				type: ERROR_ITEM_TYPE_CREATOR_TYPES,
-				error,
-				itemType
-			});
-			throw error;
+			return { creatorTypes }
 		}
+
+		const payload = { itemType };
+		return dispatch(requestWithBackoff(makeRequest, { id, type, payload }));
 	};
 };
 
 const fetchItemTypeFields = itemType => {
 	return async (dispatch, getState) => {
+		const id = requestTracker.id++;
+		const config = getState().config;
+		const type = 'ITEM_TYPE_FIELDS';
+
 		dispatch({
 			type: REQUEST_ITEM_TYPE_FIELDS,
 			itemType
 		});
-		let config = getState().config;
-		try {
-			let fields = (await api(config.apiKey, config.apiConfig).itemTypeFields(itemType).get()).getData();
-			dispatch({
-				type: RECEIVE_ITEM_TYPE_FIELDS,
-				itemType,
-				fields
-			});
-			return fields;
-		} catch(error) {
-			dispatch({
-				type: ERROR_ITEM_TYPE_FIELDS,
-				itemType,
-				error
-			});
-			throw error;
+
+		const makeRequest = async () => {
+			const fields = (await api(config.apiKey, config.apiConfig)
+				.itemTypeFields(itemType)
+				.get())
+				.getData();
+
+			return { fields }
 		}
+
+		const payload = { itemType };
+		return dispatch(requestWithBackoff(makeRequest, { id, type, payload }));
 	};
 };
 
