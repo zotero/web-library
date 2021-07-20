zotero/web-library

commit f67c1b1f5723843d19d2e58f209fe6b7316aa5a7

Reduce the bundle size by approx 25%
* Switched from Browserify to Rollup
* Removed some development-only code from the bundle
* Tweaked Reactstrap imports to avoid importing unused components
  (treeshaking doesn't seem to work correctly with this library)
* Updated zotero-api-client and tweaked build process to avoid
  processing this dependency with babel and reduce bundle size by few kb
* Tweaked react-transition-group imports to shave off another few kb
* Update & remove unused dependencies.

Results in reduction in gunzipped bundle size from 382KB down to 277KB.

src/js/component/ui/menu-entry-mobile.jsx
@@ -3,7 +3,9 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import { NavItem, NavLink } from 'reactstrap/lib';
+import { default as NavLink } from 'reactstrap/lib/NavItem';
+import { default as NavItem } from 'reactstrap/lib/NavLink';
+
 import { pick } from '../../common/immutable';
 
 export default class MobileMenuEntry extends React.PureComponent {

.eslintrc
@@ -1,6 +1,6 @@
 {
   "globals": {},
-  "parser": "babel-eslint",
+  "parser": "@babel/eslint-parser",
   "env": {
     "browser": true,
     "node": true,

babel.config.cjs
@@ -1,17 +1,24 @@
 const presets = [
 	["@babel/preset-env", {
 		"debug": !!process.env.DEBUG || false,
-		"corejs": { version: 3, proposals: true },
-		"useBuiltIns": "usage",
-		"ignoreBrowserslistConfig": true,
-		"targets": "firefox >= 68, chrome >=67, edge >= 15, safari >= 10, last 2 versions, not dead, not ie 11, not ie 10"
+		"useBuiltIns": false,
+		// "corejs": { version: 3, proposals: true },
+		// "useBuiltIns": "usage",
+		// "ignoreBrowserslistConfig": true,
+		// "targets": "firefox >= 68, chrome >=67, edge >= 15, safari >= 10, last 2 versions, not dead, not ie 11, not ie 10"
 	}],
 	"@babel/preset-react"
 ];
 
 const plugins = [
 	["@babel/plugin-proposal-class-properties", { "loose" : true }],
-	["@babel/plugin-proposal-private-methods", { "loose": true }]
+	["@babel/plugin-proposal-private-methods", { "loose": true }],
+	["@babel/plugin-transform-runtime", {
+		"absoluteRuntime": false,
+		"corejs": 3,
+		"helpers": true,
+		"regenerator": false,
+	}]
 ];
 
 module.exports = { presets, plugins };

rollup.config.js
@@ -1,0 +1,61 @@
+import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import filesize from 'rollup-plugin-filesize';
+import json from '@rollup/plugin-json';
+import replace from '@rollup/plugin-replace';
+import resolve from '@rollup/plugin-node-resolve';
+import sizes from 'rollup-plugin-sizes';
+import webWorkerLoader from 'rollup-plugin-web-worker-loader';
+import { terser } from 'rollup-plugin-terser';
+
+const isProduction = process.env.NODE_ENV?.startsWith('prod');
+
+const config = {
+	input: './src/js/main.js',
+	external: [
+		'cross-fetch/polyfill'
+	],
+	output: {
+		file: './build/static/zotero-web-library.js',
+		format: 'iife',
+		sourcemap: !isProduction,
+		compact: isProduction
+	},
+	treeshake: {
+		moduleSideEffects: 'no-external',
+	},
+	plugins: [
+		resolve({
+			preferBuiltins: false,
+			mainFields: ['browser', 'main'],
+			extensions: ['.js', '.jsx'],
+		}),
+		json(),
+		webWorkerLoader({
+			targetPlatform: 'browser',
+			skipPlugins: ['resolve', 'json', 'commonjs', 'replace', 'babel', 'sizes', 'filesize']
+		}),
+		commonjs(),
+		replace({
+			preventAssignment: true,
+			'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV ?? 'development'),
+		}),
+		babel({
+			include: ['src/js/**'],
+			extensions: ['.js', '.jsx'],
+			babelHelpers: 'runtime'
+		}),
+		filesize({ showMinifiedSize: false, showGzippedSize: !!process.env.DEBUG }),
+	]
+};
+
+if(process.env.DEBUG) {
+	config.plugins.splice(-1, 0, sizes());
+}
+
+if(isProduction) {
+	config.plugins.push(terser());
+	config.external.push('./wdyr'); //exclude why-did-you-render from production
+}
+
+export default config;

src/js/component/form/creators.jsx
@@ -2,7 +2,7 @@ import cx from 'classnames';
 import deepEqual from 'deep-equal';
 import PropTypes from 'prop-types';
 import React, { memo, useCallback, useEffect, useMemo, useRef, useState } from 'react';
-import { CSSTransition } from 'react-transition-group';
+import CSSTransition from 'react-transition-group/cjs/CSSTransition';
 import { useSelector } from 'react-redux';
 
 import Button from '../ui/button';

src/js/component/form/suggestions.jsx
@@ -1,7 +1,10 @@
 import cx from 'classnames';
 import PropTypes from 'prop-types';
 import React, { memo, forwardRef, useCallback, useState, useImperativeHandle } from 'react';
-import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap/lib';
+import { default as Dropdown } from 'reactstrap/lib/Dropdown';
+import { default as DropdownToggle } from 'reactstrap/lib/DropdownToggle';
+import { default as DropdownMenu } from 'reactstrap/lib/DropdownMenu';
+import { default as DropdownItem } from 'reactstrap/lib/DropdownItem';
 
 import { omit } from '../../common/immutable';
 

src/js/component/item-details/attachments.jsx
@@ -1,7 +1,7 @@
 import cx from 'classnames';
 import PropTypes from 'prop-types';
 import React, { memo, useCallback, useEffect, useRef, useState } from 'react';
-import { CSSTransition } from 'react-transition-group';
+import CSSTransition from 'react-transition-group/cjs/CSSTransition';
 import { NativeTypes } from 'react-dnd-html5-backend';
 import { useDispatch, useSelector } from 'react-redux';
 import { useDrag, useDrop } from 'react-dnd';

src/js/component/item/actions.jsx
@@ -1,6 +1,9 @@
 import React, { memo, useCallback, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap/lib';
+import { default as Dropdown } from 'reactstrap/lib/Dropdown';
+import { default as DropdownToggle } from 'reactstrap/lib/DropdownToggle';
+import { default as DropdownMenu } from 'reactstrap/lib/DropdownMenu';
+import { default as DropdownItem } from 'reactstrap/lib/DropdownItem';
 import { ToolGroup } from '../ui/toolbars';
 import Button from '../ui/button';
 import Icon from '../ui/icon';

commit a312960e58ce40577791ebc2c933f8e546690c3b

Fix regression: Side nav broken on mobile
Introduced in f67c1b1f5723

src/js/component/ui/menu-entry-mobile.jsx
@@ -3,8 +3,8 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import { default as NavLink } from 'reactstrap/lib/NavItem';
-import { default as NavItem } from 'reactstrap/lib/NavLink';
+import { default as NavItem } from 'reactstrap/lib/NavItem';
+import { default as NavLink } from 'reactstrap/lib/NavLink';
 
 import { pick } from '../../common/immutable';
 