6100e0c54615e870f32fc9a394249fdc6cbffa28
Tom Najdek <tom@doppnet.com>
2021-08-18 23:07:10 +0200

    Update dependencies

--- babel.config.cjs
+++ babel.config.cjs
@@ -13,6 +13,7 @@ const presets = [
 const plugins = [
 	["@babel/plugin-proposal-class-properties", { "loose" : true }],
 	["@babel/plugin-proposal-private-methods", { "loose": true }],
+	["@babel/plugin-proposal-private-property-in-object", { "loose": true }],
 	["@babel/plugin-transform-runtime", {
 		"absoluteRuntime": false,
 		"corejs": 3,
