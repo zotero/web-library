zotero/web-library

commit 7acb53f040a26ba3b93bfab34a8af30c9b6f1d7b

Redirect if collection key doesn't exist

src/js/component/libraries/collection-tree.jsx
@@ -877,6 +877,10 @@ const CollectionTree = props => {
 				targetEl.scrollIntoView();
 			}
 		}
+		// detect if URL contains non-existing collection key and redirect if it does
+		if(!(stateSelectedCollectionKey in collections)) {
+			dispatch(navigate({ items: null, collection: null, view: 'library' }));
+		}
 	}, []); // eslint-disable-line react-hooks/exhaustive-deps
 
 	if(isFetchingAllCollections) {

commit 854457c2f5e7a8f0ed255127c7fb693ec5951e2d

Fix a recent regression in URL handling
Fixes an issue introduced in 7acb53f0 where certain parts of the URL
were ignored if collection key was not present in the URL.

src/js/component/libraries/collection-tree.jsx
@@ -878,8 +878,8 @@ const CollectionTree = props => {
 			}
 		}
 		// detect if URL contains non-existing collection key and redirect if it does
-		if(!(stateSelectedCollectionKey in collections)) {
-			dispatch(navigate({ items: null, collection: null, view: 'library' }));
+		if(!isPickerMode && stateSelectedCollectionKey && !(stateSelectedCollectionKey in collections)) {
+			dispatch(navigate({ library: stateSelectedLibraryKey, view: 'library' }, true));
 		}
 	}, []); // eslint-disable-line react-hooks/exhaustive-deps
 