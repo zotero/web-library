zotero/web-library

commit 1aecc7bdf07926ad2f656c9b48379858eab996e3

Skip fetching nonessential data for Reader

src/js/component/loader.jsx
@@ -47,6 +47,7 @@ const Loader = () => {
 
 	const isEmbedded = useSelector(state => state.config.isEmbedded);
 	const libraryKey = useSelector(state => state.current.libraryKey);
+	const view = useSelector(state => state.current.view);
 	const userLibraryKey = useSelector(state => state.current.userLibraryKey);
 	const config = useSelector(state => state.config);
 	const itemTypes = useSelector(state => state.meta.itemTypes);
@@ -61,15 +62,18 @@ const Loader = () => {
 	const hasCheckedCollections = useSelector(
 		state => get(state, ['libraries', libraryKey, 'collections', 'totalResults'], null) !== null
 	);
-	const isWaitingForCollections = !hasCheckedCollections || isFetchingAllCollections;
+	const isReader = view === 'reader';
+	const isWaitingForCollections = !isReader && (!hasCheckedCollections || isFetchingAllCollections);
 
 	const [isReady, setIsReady] = useState(false);
 
 	useEffect(() => {
-		if(itemTypes && itemFields && creatorFields && tagColors && !isWaitingForCollections && !isFetchingGroups) {
+		if (isReader && tagColors) {
+			setIsReady(true);
+		} else if(itemTypes && itemFields && creatorFields && tagColors && !isWaitingForCollections && !isFetchingGroups) {
 			setIsReady(true);
 		}
-	}, [itemTypes, itemFields, creatorFields, tagColors, isWaitingForCollections, isFetchingGroups]);
+	}, [itemTypes, itemFields, creatorFields, tagColors, isWaitingForCollections, isFetchingGroups, isReader]);
 
 	useEffect(() => {
 		dispatch(preferencesLoad());
@@ -77,12 +81,15 @@ const Loader = () => {
 		dispatch(triggerResizeViewport(window.innerWidth, window.innerHeight));
 		dispatch(fetchLibrarySettings(libraryKey));
 		dispatch(toggleTransitions(true));
-		dispatch(fetchAllCollections(libraryKey));
 
-		if(config.includeUserGroups) {
-			dispatch(fetchAllGroups(userLibraryKey));
+		if(!isReader) {
+			dispatch(fetchAllCollections(libraryKey));
+
+			if(config.includeUserGroups) {
+				dispatch(fetchAllGroups(userLibraryKey));
+			}
 		}
-	}, []);
+	}, []); // eslint-disable-line react-hooks/exhaustive-deps
 
 	return isReady ? isEmbedded ? <EmbedLibrary /> : <Library /> : <LoadingCover />;
 }

commit 0da019879cfec3243fd2baaec160db434ee984f4

Fix reader crash when opened in group lib
Regression caused by 1aecc7bd

src/js/component/loader.jsx
@@ -68,7 +68,7 @@ const Loader = () => {
 	const [isReady, setIsReady] = useState(false);
 
 	useEffect(() => {
-		if (isReader && tagColors) {
+		if (isReader && tagColors && !isFetchingGroups) {
 			setIsReady(true);
 		} else if(itemTypes && itemFields && creatorFields && tagColors && !isWaitingForCollections && !isFetchingGroups) {
 			setIsReady(true);
@@ -81,6 +81,11 @@ const Loader = () => {
 		dispatch(triggerResizeViewport(window.innerWidth, window.innerHeight));
 		dispatch(fetchLibrarySettings(libraryKey));
 		dispatch(toggleTransitions(true));
+
+		if(isReader && config.includeUserGroups && libraryKey !== userLibraryKey) {
+			// we're in reader mode, but in a group so we still need to fetch groups data
+			dispatch(fetchAllGroups(userLibraryKey));
+		}
 
 		if(!isReader) {
 			dispatch(fetchAllCollections(libraryKey));