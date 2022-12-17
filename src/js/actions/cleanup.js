9c4cd8e4e6296c90eff0e33af39681d3ff8e6e9f
Tom Najdek <tom@doppnet.com>
2022-12-17 11:14:17 +0100

    Add additional checks when cleaning unused images

--- src/js/actions/cleanup.js
+++ src/js/actions/cleanup.js
@@ -9,19 +9,28 @@ const deleteUnusedEmbeddedImages = itemKey => {
 		const state = getState();
 		const item = state.libraries[state.current.libraryKey]?.items[itemKey];
 
-		if (!item) {
+		if (!item || !item.note) {
 			return null;
 		}
 
 		try {
 			const parser = new DOMParser();
 			await dispatch(fetchAllChildItems(item.key));
-			const childItemsKeys = getState().libraries[state.current.libraryKey]?.itemsByParent[item.key]?.keys ?? [];
+			const allChildItemsKeys = getState().libraries[state.current.libraryKey]?.itemsByParent[item.key]?.keys ?? [];
+
+			const embeddedImages = allChildItemsKeys
+				.map(cik => getState().libraries[state.current.libraryKey]?.items[cik])
+				.filter(ci => ci && ci.linkMode === 'embedded_image')
+
 			const doc = parser.parseFromString(item.note, 'text/html');
-			const keys = Array.from(doc.querySelectorAll('img[data-attachment-key]'))
+			const presentKeys = Array.from(doc.querySelectorAll('img[data-attachment-key]'))
 				.map(node => node.getAttribute('data-attachment-key'));
-			const redundantChildItemsKeys = childItemsKeys.filter(cik => !keys.includes(cik));
-			dispatch(chunkedDeleteItems(redundantChildItemsKeys));
+
+			const redundantEmbeddedImageKeys = embeddedImages
+				.filter(ci => !presentKeys.includes(ci.key))
+				.map(ci => ci.key);
+
+			dispatch(chunkedDeleteItems(redundantEmbeddedImageKeys));
 		} catch {
 			console.warn(`Failed to cleanup embededed images on item ${item.key}`);
 		}
