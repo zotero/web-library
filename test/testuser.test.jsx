dc61a2cd8942e5049106248952c2f1ba36170079
Tom Najdek <tom@doppnet.com>
2023-01-14 00:41:43 +0100

    Add a test for removing redundant images

---
+++ test/testuser.test.jsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import { screen } from '@testing-library/react';
+
+import { renderWithProviders } from './utils/render';
+import { JSONtoState } from './utils/state';
+import { MainZotero } from '../src/js/component/main';
+import { applyAdditionalJestTweaks, waitForPosition, actWithFakeTimers, resizeWindow } from './utils/common';
+import stateRaw from './fixtures/state/test-user-item-view.json';
+import userPicturesRedundant from './fixtures/response/test-user-pictures-redundant.json';
+import itemFields from './fixtures/response/item-fields';
+import itemTypeFieldsBook from './fixtures/response/item-type-fields-film.json';
+import itemTypeCreatorTypesBook from './fixtures/response/item-type-creator-types-film.json';
+
+const state = JSONtoState(stateRaw);
+applyAdditionalJestTweaks();
+
+describe('Test User\'s library', () => {
+	const handlers = [];
+	const server = setupServer(...handlers)
+
+	beforeAll(() => {
+		server.listen({
+			onUnhandledRequest: 'error',
+		});
+		resizeWindow(1280, 720);
+	});
+
+	beforeEach(() => {
+		delete window.location;
+		window.location = new URL('http://localhost/testuser/collections/WTTJ2J56/items/VR82JUX8/collection');
+	});
+
+	afterEach(() => server.resetHandlers());
+	afterAll(() => server.close());
+
+	test('Selecting standalone note, then another item, should run redundant image cleanup', async () => {
+		renderWithProviders(<MainZotero />, { preloadedState: state });
+		await waitForPosition();
+
+		const noteItem = screen.getByRole('row', { name: 'Puppies!' });
+		await actWithFakeTimers(user => user.click(noteItem));
+		await waitForPosition();
+
+		expect(screen.getByRole('tab', { name: 'Note', selected: true })).toBeInTheDocument();
+
+		server.use(
+			rest.get('https://api.zotero.org/users/1/items/Z6HA62VJ/children', (req, res) => {
+				return res(res => {
+					res.headers.set('Total-Results', 4);
+					res.body = JSON.stringify(userPicturesRedundant);
+					return res;
+				});
+			}),
+			rest.delete('https://api.zotero.org/users/1/items', (req, res) => {
+				const itemKey = req.url.searchParams.get('itemKey');
+				expect(itemKey).toBe('ZZZZZZZZ');
+				return res(res => {
+					res.status = 204;
+					return res;
+				});
+			}),
+			rest.get('https://api.zotero.org/creatorFields', (req, res) => {
+				return res(res => {
+					res.body = JSON.stringify(itemFields);
+					return res;
+				});
+			}),
+			rest.get('https://api.zotero.org/itemTypeCreatorTypes', (req, res) => {
+				return res(res => {
+					res.body = JSON.stringify(itemTypeCreatorTypesBook);
+					return res;
+				});
+			}),
+			rest.get('https://api.zotero.org/itemTypeFields', (req, res) => {
+				return res(res => {
+					res.body = JSON.stringify(itemTypeFieldsBook);
+					return res;
+				});
+			}),
+		);
+
+		const nextItem = screen.getByRole('row', { name: 'Understanding dogs' });
+		await actWithFakeTimers(user => user.click(nextItem));
+		await waitForPosition();
+	});
+});
