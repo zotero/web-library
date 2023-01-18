zotero/web-library

commit 894028fd39148dc51538386c0bbef257d62b6d59

Embedded: Avoid polluting global css

src/scss/components/modal/_react-modal.scss
@@ -73,7 +73,7 @@
 		overflow: hidden;
 	}
 
-	.modal-scrollable & {
+	@include scopedVariant(".modal-scrollable") {
 		@include bp-up(sm) {
 			display: flex;
 			flex-direction: column;
@@ -116,7 +116,7 @@
 			background-color: $modal-content-bg;
 		}
 
-		.modal-form & {
+		@include scopedVariant(".modal-form") {
 			@include touch-or-bp-down(sm) {
 				background-color: $modal-form-header-bg;
 			}
@@ -161,19 +161,19 @@
 		-webkit-overflow-scrolling: touch; // Momentum scrolling for iOS
 	}
 
-	.modal-scrollable & {
+	@include scopedVariant(".modal-scrollable") {
 		overflow-y: auto;
 		-webkit-overflow-scrolling: touch; // Momentum scrolling for iOS 10–
 
 		&:focus {
-			.keyboard & {
+			@include scopedVariant(".keyboard") {
 				outline: none;
 				border: $outline-width solid $focus-color;
 			}
 		}
 	}
 
-	.modal-form & {
+	@include scopedVariant(".modal-form") {
 		@include touch-or-bp-down(sm) {
 			background-color: $form-bg-touch;
 		}

src/js/component/form/auto-resizer.jsx
@@ -9,7 +9,7 @@ class AutoResizer extends React.PureComponent {
 		const { vertical, children, content } = this.props;
 		return (
 			<div className={ cx('auto-resizer', { vertical, horizontal: !vertical }) } >
-			<div className="content">{ content }</div>
+				<div className="content">{ content }</div>
 				{ children }
 			</div>
 		)

src/js/embedded.jsx
@@ -80,6 +80,6 @@ const init = (element, config = {}) => {
 }
 
 if(targetDom) {
-	targetDom.classList.add('zotero-web-library-embedded');
+	targetDom.classList.add('zotero-wle');
 	init(targetDom, config);
 }

src/js/main.js
@@ -8,5 +8,6 @@ const config = configDom ? JSON.parse(configDom.textContent) : {};
 config.menus = menuConfigDom ? JSON.parse(menuConfigDom.textContent) : null;
 
 if(targetDom) {
+	targetDom.classList.add('zotero-wl');
 	init(targetDom, config);
 }

src/scss/abstracts/_functions.scss
@@ -6,3 +6,4 @@
 @import "functions/asset-url";
 @import "functions/strings";
 @import "functions/color";
+@import "functions/lists";

src/scss/abstracts/functions/_lists.scss
@@ -1,0 +1,16 @@
+@use "sass:list";
+
+@function nth-delete($list, $n) {
+	$result: ();
+	$n: if($n < 0, list.length($list) + $n + 1, $n);
+	$isBracketed: list.is-bracketed($list);
+	$separator: list.separator($list);
+
+	@for $i from 1 through list.length($list) {
+		@if $i != $n {
+			$result: list.append($result, list.nth($list, $i));
+		}
+	}
+
+	@return list.join((), $result, $separator, $isBracketed);
+}

src/scss/abstracts/mixins/_selectors.scss
@@ -1,6 +1,11 @@
 //
 // Selectors
 // --------------------------------------------------
+//
+
+@use "sass:selector";
+@use "sass:list";
+@use "sass:string";
 
 // State mixin
 @mixin state($states...) {
@@ -29,5 +34,65 @@
 @mixin variant($args...) {
 	@include state($args...) {
 		@content;
+	}
+}
+
+// Whenever parent selector would be used to style "in context" (e.g. `.variant &` but not
+// `&.variant` ), use `scopedVariant(".variant")` instead. This is required because embedded version
+// of web library prefixes every single style with a scope class to avoid polluting global css (e.g.
+// if web library included styling for ".details" and someone had such a class on their page, web
+// library would pollute that style)
+@mixin scopedVariant($selectors...) {
+	@each $selector in $selectors {
+		@if $scope {
+			$parent: list.nth(selector.parse(&), 1);
+			$parentIndex: list.index($parent, $scope);
+			$selectorIndex: list.index($parent, $selector);
+			$shouldScope: true;
+
+
+			@if $parentIndex == null {
+				$shouldScope: false;
+			}
+
+			@if string.index(#{list.nth(selector.parse($selector), 1)}, "html") == 1 {
+				$shouldScope: false
+			}
+
+			@if string.index(#{list.nth(selector.parse($selector), 1)}, "body") == 1 {
+				$shouldScope: false
+			}
+
+			@if #{list.nth(selector.parse($selector), 1)} == ".keyboard" {
+				$shouldScope: false
+			}
+
+			@if #{list.nth(selector.parse($selector), 1)} == ".touch" {
+				$shouldScope: false
+			}
+
+			@if #{list.nth(selector.parse($selector), 1)} == ".mouse" {
+				$shouldScope: false
+			}
+
+			@if #{list.nth(selector.parse($selector), 1)} == ".scrollbar-style-permanent" {
+				$shouldScope: false
+			}
+
+			@if $shouldScope {
+				$parent: nth-delete($parent, $parentIndex);
+				@at-root #{$scope} #{$selector} #{$parent} {
+					@content;
+				}
+			} @else {
+				@at-root #{$selector} #{&} {
+					@content;
+				}
+			}
+		} @else {
+			@at-root #{$selector} #{&} {
+				@content;
+			}
+		}
 	}
 }

src/scss/abstracts/utilities/_flex.scss
@@ -2,31 +2,33 @@
 // Flex
 //
 
-.flex-row {
-	flex-grow: 1; // Do not set flex-basis as it conflicts with grid classes
-	display: flex;
-	min-width: 0; // Fix truncation
-}
+#{$scope} {
+	.flex-row {
+		flex-grow: 1; // Do not set flex-basis as it conflicts with grid classes
+		display: flex;
+		min-width: 0; // Fix truncation
+	}
 
-.flex-column {
-	display: flex;
-	flex-direction: column;
-	flex-grow: 1;
-	min-width: 0; // Fix truncation
-}
+	.flex-column {
+		display: flex;
+		flex-direction: column;
+		flex-grow: 1;
+		min-width: 0; // Fix truncation
+	}
 
-.justify-content-end {
-	justify-content: flex-end;
-}
+	.justify-content-end {
+		justify-content: flex-end;
+	}
 
-.justify-content-between {
-	justify-content: space-between;
-}
+	.justify-content-between {
+		justify-content: space-between;
+	}
 
-.justify-center {
-	justify-content: center;
-}
+	.justify-center {
+		justify-content: center;
+	}
 
-.align-items-center {
-	align-items: center;
+	.align-items-center {
+		align-items: center;
+	}
 }

commit 7c4ad33d66c065e01466deed079b08baf30e33e2

Fix misaligned colored tags. Resolve #499
This only appeared on desktop browsers with a fixed scrollbar.

Regression introduced in 894028fd

src/js/utils.js
@@ -297,10 +297,11 @@ const getScrollbarWidth = () => {
 		return 0;
 	}
 	const scrollDiv = document.createElement('div');
-	scrollDiv.className = 'modal-scrollbar-measure';
+	scrollDiv.style = 'position: absolute; top: -9999px; width: 50px; height: 50px; overflow: scroll;';
 	document.body.appendChild(scrollDiv);
 	const scrollbarWidth = scrollDiv.getBoundingClientRect().width - scrollDiv.clientWidth;
 	document.body.removeChild(scrollDiv);
+
 	return scrollbarWidth;
 }
 

src/scss/components/modal/_react-modal.scss
@@ -215,14 +215,6 @@
 	}
 }
 
-.modal-scrollbar-measure {
-	position: absolute;
-	top: -9999px;
-	width: 50px;
-	height: 50px;
-	overflow: scroll;
-}
-
 .modal-slide {
 	&.ReactModal {
 		&__Overlay {
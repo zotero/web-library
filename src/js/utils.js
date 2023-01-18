7c4ad33d66c065e01466deed079b08baf30e33e2
Tom Najdek <tom@doppnet.com>
2023-01-18 17:11:56 +0100

    Fix misaligned colored tags. Resolve #499
    This only appeared on desktop browsers with a fixed scrollbar.
    
    Regression introduced in 894028fd

--- src/js/utils.js
+++ src/js/utils.js
@@ -297,10 +297,11 @@ const getScrollbarWidth = () => {
 		return 0;
 	}
 	const scrollDiv = document.createElement('div');
-	scrollDiv.className = 'modal-scrollbar-measure';
+	scrollDiv.style = 'position: absolute; top: -9999px; width: 50px; height: 50px; overflow: scroll;';
 	document.body.appendChild(scrollDiv);
 	const scrollbarWidth = scrollDiv.getBoundingClientRect().width - scrollDiv.clientWidth;
 	document.body.removeChild(scrollDiv);
+
 	return scrollbarWidth;
 }
 
