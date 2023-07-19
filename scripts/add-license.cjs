a8daa1c547fcb8cbad89369c5c28e974c90feb77
Tom Najdek <tom@doppnet.com>
2023-07-19 23:57:13 +0200

    Add icon for dataset item type
    Also update add-license script to work with latest glob.

--- scripts/add-license.cjs
+++ scripts/add-license.cjs
@@ -1,7 +1,7 @@
 const licenseString ='<!-- This file is part of Zotero. It is distributed under the GNU Affero General Public License version 3. -->';
 
 const fs = require('fs-extra');
-const glob = require('glob');
+const { glob } = require('glob');
 const path = require('path');
 
 var filesChanged = 0;
@@ -17,11 +17,8 @@ const addLicense = async file => {
 }
 
 (async () => {
-	var promises;
-
-	glob(`${path.join(__dirname, '..', 'src', 'static', 'icons')}/**/*.svg`, async (_, files) => {
-		promises = files.map(f => addLicense(f));
-		await Promise.all(promises);
-		console.log(`Injected license into ${filesChanged}/${files.length} files.`);
-	});
+	const files = await glob(`${path.join(__dirname, '..', 'src', 'static', 'icons')}/**/*.svg`);
+	const promises = files.map(f => addLicense(f));
+	await Promise.all(promises);
+	console.log(`Injected license into ${filesChanged}/${files.length} files.`);
 })();
