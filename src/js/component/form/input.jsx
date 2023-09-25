zotero/web-library

commit b825df4a97c0a04113a17b68a550c6e5afa24057

Reader 2: Improve tags popover positioning
* Limit popover growth to 12 items
* Flip popup if not enough space below anchor
* Use portal for tag suggestions to avoid issue with overflow
* Allow keyboard-navigation between tags popover and reader iframe
* Create dropdown mixins so suggestions and dropdowns can re-use styles

src/js/component/form/input.jsx
@@ -1,12 +1,14 @@
 import cx from 'classnames';
 import PropTypes from 'prop-types';
-import { forwardRef, memo, useCallback, useEffect, useId, useImperativeHandle, useLayoutEffect, useRef, useState, } from 'react';
+import { forwardRef, memo, useCallback, useContext, useEffect, useId, useImperativeHandle, useLayoutEffect, useRef, useState, } from 'react';
 import { usePrevious } from 'web-common/hooks';
 import { mod, noop, omit, pick } from 'web-common/utils';
 import { Spinner } from 'web-common/components';
+import { createPortal } from 'react-dom';
 
 import AutoResizer from './auto-resizer';
-import { useFloating, shift } from '@floating-ui/react-dom';
+import { PortalContext } from '../portal';
+import { useFloating, shift, flip, size } from '@floating-ui/react-dom';
 
 const NATIVE_INPUT_PROPS = ['autoFocus', 'form', 'id', 'inputMode', 'max', 'maxLength',
 'min', 'minLength', 'name', 'placeholder', 'required', 'type', 'spellCheck', 'step', 'tabIndex'];
@@ -28,11 +30,54 @@ AutoResizerInput.propTypes = {
 	value: PropTypes.string
 };
 
+const Suggestions = memo(props => {
+	const { highlighted, id, onMousedown, refs, show, strategy, suggestions, suggestionsRef, x, y } = props;
+	return (
+		<div
+			aria-label='Suggestions'
+			role="listbox"
+			id={`${id}-suggestions`}
+			style={{ position: strategy, transform: `translate3d(${x}px, ${y}px, 0px)` }}
+			ref={r => { refs.setFloating(r); suggestionsRef.current = r; }}
+			className={cx("suggestions", {
+				'show': show,
+			})}>
+			{suggestions.map((s, index) =>
+				<div
+					aria-label={s}
+					role="listitem"
+					className={cx("suggestion", {
+						'active': index === highlighted,
+					})}
+					data-suggestion={s}
+					key={s}
+					onMouseDown={onMousedown}
+				>
+					{s}
+				</div>
+			)}
+		</div>
+	);
+});
+
+Suggestions.displayName = 'Suggestions';
+Suggestions.propTypes = {
+	highlighted: PropTypes.number.isRequired,
+	id: PropTypes.string.isRequired,
+	onMousedown: PropTypes.func.isRequired,
+	refs: PropTypes.object.isRequired,
+	show: PropTypes.bool.isRequired,
+	strategy: PropTypes.string.isRequired,
+	suggestions: PropTypes.arrayOf(PropTypes.string).isRequired,
+	suggestionsRef: PropTypes.object.isRequired,
+	x: PropTypes.number.isRequired,
+	y: PropTypes.number.isRequired,
+};
 
 const Input = memo(forwardRef((props, ref) => {
-	const { className = 'form-control', inputGroupClassName, isBusy, isDisabled, isReadOnly, isRequired, onBlur = noop, onCancel
-	= noop, onCommit = noop, onChange = noop, onFocus = noop, onKeyDown = noop, selectOnFocus,
-	suggestions, validationError, value: initialValue, resize, ...rest } = props;
+	const { className = 'form-control', inputGroupClassName, isBusy, isDisabled, isReadOnly, isRequired, onBlur = noop,
+		onCancel = noop, onCommit = noop, onChange = noop, onFocus = noop, onKeyDown = noop, selectOnFocus, suggestions,
+		validationError, value: initialValue, resize, ...rest } = props;
 	const [hasCancelledSuggestions, setHasCancelledSuggestions] = useState(false);
 	const [value, setValue] = useState(initialValue);
 	const [highlighted, setHighlighted] = useState(0);
@@ -43,11 +88,18 @@ const Input = memo(forwardRef((props, ref) => {
 	const id = useId();
 	const hasBeenCancelled = useRef(false);
 	const hasBeenCommitted = useRef(false);
-	const show = suggestions && suggestions.length && !hasCancelledSuggestions;
+	const show = !!(suggestions && suggestions.length && !hasCancelledSuggestions);
 	const prevShow = usePrevious(show);
+	const prevSuggestions = usePrevious(suggestions);
+	const { ref: portalRef } = useContext(PortalContext);
 
 	const { x, y, refs, strategy, update } = useFloating({
-		placement: 'bottom', middleware: [shift()]
+		placement: 'bottom', middleware: [shift(), flip(), size({
+			apply({ rects }) {
+				Object.assign(refs.floating.current.style, {
+					width: `${rects.reference.width}px`,
+				});
+			}})]
 	});
 
 	//reset on every render
@@ -163,10 +215,18 @@ const Input = memo(forwardRef((props, ref) => {
 	}, [validationError, prevValidationError]);
 
 	useLayoutEffect(() => {
+		if (show && prevSuggestions?.length && suggestions.length !== prevSuggestions?.length) {
+			update();
+		}
+	}, [show, prevSuggestions, update, suggestions]);
+
+	useLayoutEffect(() => {
 		if (show && !prevShow) {
 			update();
 		}
 	}, [show, prevShow, update]);
+
+	const suggestionsEl = suggestions ? <Suggestions {...{ x, y, strategy, suggestions, highlighted, onMousedown: handleSuggestionMouseDown, id, refs, show, suggestionsRef }} /> : null;
 
 	return (
 		<div className={ groupClassName }>
@@ -187,32 +247,7 @@ const Input = memo(forwardRef((props, ref) => {
 				aria-autocomplete={suggestions ? 'list' : null}
 				aria-controls={ `${id}-suggestions}` }
 			/>
-			{ suggestions && (
-				<div
-					aria-label='Suggestions'
-					role="listbox"
-					id={ `${id}-suggestions` }
-					style={{ position: strategy, transform: `translate3d(${x}px, ${y}px, 0px)` }}
-					ref={r => { refs.setFloating(r); suggestionsRef.current = r; } }
-					className={ cx("dropdown-menu suggestions", {
-						'show': show,
-				})}>
-					{ suggestions.map((s, index) =>
-						<div
-							aria-label={s}
-							role="listitem"
-							className={ cx("dropdown-item", {
-								'active': index === highlighted,
-							})}
-							data-suggestion={s}
-							key={s}
-							onMouseDown={ handleSuggestionMouseDown }
-						>
-							{s}
-						</div>
-					) }
-				</div>
-			)}
+			{suggestions && (portalRef?.current ? createPortal(suggestionsEl, portalRef.current) : suggestionsEl) }
 			{ isBusy ? <Spinner /> : null }
 		</div>
 	);

src/js/component/item-details/tag-picker.jsx
@@ -219,7 +219,6 @@ const TagPicker = ({ itemKey, libraryKey, isReadOnly }) => {
 				onFocus={handleFocusOnContainer}
 				ref={scrollContainerRef}
 				tabIndex={0}
-				style={{ overflowY: suggestions && suggestions.length ? 'visible' : null }}
 			>
 				{tags.length > 0 && (
 					<nav>

src/js/component/portal.jsx
@@ -1,0 +1,36 @@
+import { createContext, useRef, useCallback, memo } from 'react';
+import PropTypes from 'prop-types';
+import { noop } from 'web-common/utils';
+
+export const PortalContext = createContext({});
+
+const Portal = memo(({ onClose = noop, children }) => {
+	const ref = useRef(null);
+
+	const handleClick = useCallback(ev => {
+		if (ev.target === ref.current) {
+			onClose();
+		}
+	}, [onClose]);
+
+	return (
+		<PortalContext.Provider value={{ ref }}>
+			<div
+				ref={ref}
+				className="portal"
+				onClick={handleClick}
+			>
+				{children}
+			</div>
+		</PortalContext.Provider>
+	);
+});
+
+Portal.displayName = 'Portal';
+
+Portal.propTypes = {
+	onClose: PropTypes.func,
+	children: PropTypes.node.isRequired,
+};
+
+export default Portal;

src/js/component/reader.jsx
@@ -4,9 +4,9 @@ import { memo, useCallback, useEffect, useLayoutEffect, useMemo, useReducer, use
 import { usePrevious } from 'web-common/hooks';
 import { pick } from 'web-common/utils';
 import { getLastPageIndexSettingKey } from '../common/item';
-import { DropdownContext, DropdownMenu, DropdownItem, Icon, Spinner } from 'web-common/components';
-import { useFloating, offset, flip, shift, autoUpdate } from '@floating-ui/react-dom';
-import { useDebouncedCallback } from 'use-debounce';
+import { Spinner } from 'web-common/components';
+import { useFloating, flip, shift } from '@floating-ui/react-dom';
+import PropTypes from 'prop-types';
 
 import { annotationItemToJSON } from '../common/annotations.js';
 import { ERROR_PROCESSING_ANNOTATIONS } from '../constants/actions';
@@ -20,6 +20,7 @@ import { useFetchingState } from '../hooks';
 import { strings } from '../constants/strings.js';
 import TagPicker from './item-details/tag-picker.jsx';
 import { READER_CONTENT_TYPES } from '../constants/reader.js';
+import Portal from './portal';
 
 const PAGE_SIZE = 100;
 
@@ -62,60 +63,11 @@ const computeDiffUsingWorker = (oldFile, newFile) => {
 };
 
 
-const Portal = ({ onClose, children }) => {
-	const ref = useRef(null);
-	const handleClick = useCallback(ev => {
-		if (ev.target === ref.current) {
-			onClose();
-		}
-	}, [onClose]);
-
-	return <div
-		ref={ref}
-		className="portal"
-		onClick={handleClick}
-	>
-		{children}
-	</div>;
-};
-
-const Overlay = ({ children }) => {
-	return <div className="overlay">{children}</div>;
-};
-
-
-const PopupPortal = ({ anchor, children, onClose }) => {
+const PopupPortal = memo(({ anchor, children, onClose }) => {
 	const { x, y, refs, strategy, update } = useFloating({
-		placement: 'bottom-start', middleware: [shift()]
+		placement: 'bottom-start', middleware: [shift(), flip()]
 	});
 	const isOpen = children !== null;
-
-	// const options = useMemo(() =>
-	// 	(context?.itemGroups ?? [])
-	// 		.map((group, i) => ([
-	// 			...group.map(item => (
-	// 				<DropdownItem
-	// 					onClick={item.onCommand}
-	// 					key={item.label}
-	// 				>
-	// 					{item.color && (
-	// 						<Icon
-	// 							aria-role="presentation"
-	// 							type="12/square"
-	// 							symbol="square"
-	// 							width={10}
-	// 							height={10}
-	// 							style={{ color: item.color }}
-	// 						/>
-	// 					)}
-	// 					{item.label}
-	// 				</DropdownItem>
-	// 			)),
-	// 			...(i < context.itemGroups.length - 1 ? [<DropdownItem key={`group-${i}`} divider />] : [])
-	// 		]))
-	// 		.flat()
-	// 	, [context]);
-
 
 	useLayoutEffect(() => {
 		if (children !== null) {
@@ -126,17 +78,26 @@ const PopupPortal = ({ anchor, children, onClose }) => {
 	return (
 		<Portal onClose={ onClose }>
 			{isOpen && (
-				<Overlay>
+				<>
 					<div className="anchor" ref={refs.setReference} style={{ position: 'absolute', left: anchor.x, top: anchor.y }} />
 					<div className="popup" ref={refs.setFloating} style={{ position: strategy, transform: `translate3d(${x}px, ${y}px, 0px)` }}>
 						{ children }
 					</div>
-				</Overlay>
+				</>
 			)}
 		</Portal>
 	);
-}
+});
 
+PopupPortal.displayName = 'PopupPortal';
+PopupPortal.propTypes = {
+	anchor: PropTypes.shape({
+		x: PropTypes.number.isRequired,
+		y: PropTypes.number.isRequired,
+	}),
+	children: PropTypes.node,
+	onClose: PropTypes.func.isRequired,
+};
 
 const readerReducer = (state, action) => {
 	console.log(action);
@@ -239,6 +200,7 @@ const Reader = () => {
 
 	const handleClose = useCallback(() => {
 		setTagPicker(null);
+		iframeRef.current.focus();
 	}, []);
 
 	const getProcessedAnnotations = useCallback((annotations) => {
@@ -279,6 +241,13 @@ const Reader = () => {
 		}
 	}, [attachmentItem, dispatch, url]);
 
+	const handleKeyDown = useCallback((ev) => {
+		if (ev.key === 'Escape') {
+			setTagPicker(null);
+			iframeRef.current.focus();
+		}
+	}, []);
+
 	const handleIframeLoaded = useCallback(() => {
 		const processedAnnotations = getProcessedAnnotations(annotations);
 		const pageIndexKey = PAGE_INDEX_KEY_LOOKUP[attachmentItem.contentType];
@@ -294,16 +263,16 @@ const Reader = () => {
 				baseURI: new URL('/', window.location).toString()
 			},
 			annotations: [...processedAnnotations, ...state.importedAnnotations],
-			primaryViewState: readerState,  // Do we want to save PDF reader view state?
+			primaryViewState: readerState,
 			secondaryViewState: null,
-			location: null, // Navigate to specific PDF part when opening it
+			location: null,
 			readOnly: isReadOnly,
 			authorName: isGroup ? currentUserSlug : '',
-			showItemPaneToggle: false, //  ???
+			showItemPaneToggle: false,
 			sidebarWidth: readerSidebarWidth,
-			sidebarOpen: isReaderSidebarOpen ?? true, // Save sidebar open/close state?
-			bottomPlaceholderHeight: 0, /// ???
-			rtl: false, // TODO: ?
+			sidebarOpen: isReaderSidebarOpen ?? true,
+			bottomPlaceholderHeight: 0,
+			rtl: false,
 			localizedStrings: strings,
 			showAnnotations: true,
 			onSaveAnnotations: (annotations) => {
@@ -320,6 +289,9 @@ const Reader = () => {
 			},
 			onOpenTagsPopup: (key, x, y) => {
 				setTagPicker({ key, x, y});
+				setTimeout(() => {
+					document.querySelector('.add-tag').focus();
+				}, 0);
 				console.log('onOpenTagsPopup', { key, x, y });
 			},
 			onClosePopup: (...args) => {
@@ -454,7 +426,7 @@ const Reader = () => {
 	}, [rotatePages, state.action, state.data, state.isReady]);
 
 	return (
-		<section className="reader-wrapper">
+		<section className="reader-wrapper" onKeyDown={handleKeyDown} tabIndex="0">
 			{state.isReady ? (
 				<>
 					<iframe onLoad={handleIframeLoaded} ref={iframeRef} src={pdfReaderURL} />

src/scss/abstracts/_mixins.scss
@@ -3,6 +3,7 @@
 // --------------------------------------------------
 
 @import "mixins/breakpoints";
+@import "mixins/dropdown";
 @import "mixins/same";
 @import "mixins/utilities";
 @import "mixins/selectors";

src/scss/abstracts/mixins/_dropdown.scss
@@ -1,0 +1,95 @@
+@mixin dropdown-menu {
+	background-color: $menu-bg;
+	box-shadow: $menu-box-shadow;
+	border-radius: $menu-border-radius;
+	z-index: $z-index-menu;
+	margin: $space-min 0;
+	display: none;
+	will-change: transform;
+	transform: translate3d(0, 0, 0);
+	top: 0;
+	left: 0; // required for correct popper js rendering
+
+	&:focus {
+		outline: none;
+	}
+
+	@include touch-or-bp-down(sm) {
+		padding: $space-xs 0;
+	}
+
+	@include mouse-and-bp-up(md) {
+		padding: $space-min 0;
+	}
+}
+
+
+@mixin dropdown-item {
+	display: block;
+	width: 100%;
+	border: none;
+	text-align: left;
+	background-color: transparent;
+	margin: 0;
+	white-space: nowrap; // Prevent line break due to positioned dropdown-wrapper
+	color: $menu-color;
+	cursor: pointer;
+
+	@include touch-or-bp-down(sm) {
+		line-height: $line-height-base-mobile; // Prevent inheritance
+		padding: $dropdown-item-padding-y-touch $dropdown-item-padding-x-touch;
+	}
+
+	@include mouse-and-bp-up(md) {
+		line-height: $line-height-base; // Prevent inheritance
+		padding: ($input-btn-padding-y + $input-border-width) $input-btn-padding-x;
+	}
+
+	&.disabled {
+		color: $menu-color-disabled;
+
+		&:hover,
+		&:focus {
+			background-color: transparent;
+		}
+	}
+
+	&:focus {
+		outline: none;
+	}
+
+	&:hover,
+	&.active {
+		background-color: $menu-item-hover-bg;
+	}
+
+	&.selected {
+		background-color: $menu-item-selected-bg;
+
+		&:hover,
+		&:focus,
+		&.active {
+			background-color: $menu-item-selected-hover-bg;
+		}
+	}
+
+	.icon {
+		vertical-align: top;
+
+		@include touch-or-bp-down(sm) {
+			margin-right: $space-xs;
+		}
+	}
+}
+
+@mixin dropdown-divider {
+	border-top: $input-border-width solid $menu-devider;
+
+	@include touch-or-bp-down(sm) {
+		margin: $space-xs 0;
+	}
+
+	@include mouse-and-bp-up(md) {
+		margin: $space-min 0;
+	}
+}

src/scss/components/_dropdown.scss
@@ -26,28 +26,7 @@
 	}
 
 	.dropdown-menu {
-		background-color: $menu-bg;
-		box-shadow: $menu-box-shadow;
-		border-radius: $menu-border-radius;
-		z-index: $z-index-menu;
-		margin: $space-min 0;
-		display: none;
-		will-change: transform;
-		transform: translate3d(0, 0, 0);
-		top: 0;
-		left: 0; // required for correct popper js rendering
-
-		&:focus {
-			outline: none;
-		}
-
-		@include touch-or-bp-down(sm) {
-			padding: $space-xs 0;
-		}
-
-		@include mouse-and-bp-up(md) {
-			padding: $space-min 0;
-		}
+		@include dropdown-menu;
 
 		@include state(".dropdown.show") {
 			display: block;
@@ -55,78 +34,16 @@
 	}
 
 	.dropdown-item {
-		display: block;
-		width: 100%;
-		border: none;
-		text-align: left;
-		background-color: transparent;
-		margin: 0;
-		white-space: nowrap; // Prevent line break due to positioned dropdown-wrapper
-		color: $menu-color;
-		cursor: pointer;
-
-		@include touch-or-bp-down(sm) {
-			line-height: $line-height-base-mobile; // Prevent inheritance
-			padding: $dropdown-item-padding-y-touch $dropdown-item-padding-x-touch;
-		}
-
-		@include mouse-and-bp-up(md) {
-			line-height: $line-height-base; // Prevent inheritance
-			padding: ($input-btn-padding-y + $input-border-width) $input-btn-padding-x;
-		}
-
-		&.disabled {
-			color: $menu-color-disabled;
-
-			&:hover,
-			&:focus {
-				background-color: transparent;
-			}
-		}
-
-		&:focus {
-			outline: none;
-		}
-
-		&:hover,
-		&.active {
-			background-color: $menu-item-hover-bg;
-		}
+		@include dropdown-item;
 
 		@include scopedVariant(".keyboard") {
 			&:focus {
 				background-color: $menu-item-hover-bg;
 			}
 		}
-
-		&.selected {
-			background-color: $menu-item-selected-bg;
-
-			&:hover,
-			&:focus,
-			&.active {
-				background-color: $menu-item-selected-hover-bg;
-			}
-		}
-
-		.icon {
-			vertical-align: top;
-
-			@include touch-or-bp-down(sm) {
-				margin-right: $space-xs;
-			}
-		}
 	}
 
 	.dropdown-divider {
-		border-top: $input-border-width solid $menu-devider;
-
-		@include touch-or-bp-down(sm) {
-			margin: $space-xs 0;
-		}
-
-		@include mouse-and-bp-up(md) {
-			margin: $space-min 0;
-		}
+		@include dropdown-divider;
 	}
 }

src/scss/components/_reader.scss
@@ -13,6 +13,7 @@
 	.portal {
 		position: absolute;
 		inset: 0;
+		overflow: hidden;
 	}
 
 	.popup {
@@ -27,6 +28,12 @@
 			.counter {
 				margin-right: $space-xs;
 			}
+		}
+
+		.scroll-container-mouse {
+			// limit tags popup height to 12 items (then it will scroll)
+			overflow-y: auto;
+			max-height: 12 * 26px;
 		}
 	}
 

commit ae5b846ba27d17bb29c00655418631ebcf11e1cb

Fix regression where tag suggestions wouldn't close
Regression in b825df4a

src/js/component/form/input.jsx
@@ -146,7 +146,7 @@ const Input = memo(forwardRef((props, ref) => {
 				inputRef.current.blur();
 			}
 		} else {
-			onCommit(value, value !== initialValue, ev);
+			onCommit(value, value !== initialValue, ev, inputRef.current);
 		}
 	}, [initialValue, onBlur, onCancel, onCommit, value]);
 
@@ -173,10 +173,10 @@ const Input = memo(forwardRef((props, ref) => {
 				if(suggestions && suggestions.length && !hasCancelledSuggestions) {
 					const newValue = suggestions[highlighted] || value;
 					setValue(newValue);
-					onCommit(newValue, newValue !== initialValue, ev);
+					onCommit(newValue, newValue !== initialValue, ev, inputRef.current);
 				} else {
 					hasBeenCommitted.current = true;
-					onCommit(value, value !== initialValue, ev);
+					onCommit(value, value !== initialValue, ev, inputRef.current);
 				}
 			break;
 			case 'ArrowDown':
@@ -198,7 +198,7 @@ const Input = memo(forwardRef((props, ref) => {
 	const handleSuggestionMouseDown = useCallback(ev => {
 		const newValue = ev.currentTarget.dataset.suggestion;
 		setValue(newValue);
-		onCommit(newValue, newValue !== initialValue, ev);
+		onCommit(newValue, newValue !== initialValue, ev, inputRef.current);
 		ev.preventDefault();
 	}, [initialValue, onCommit]);
 

src/js/component/item-details/tag-picker.jsx
@@ -41,8 +41,8 @@ const TagPicker = ({ itemKey, libraryKey, isReadOnly }) => {
 	const realTagCount = tags.filter(t => t.tag !== '').length;
 
 
-	const handleCommit = useCallback(async (newTagValue, hasChanged, ev) => {
-		const tag = (ev.currentTarget || ev.target).closest('[data-tag]').dataset.tag;
+	const handleCommit = useCallback(async (newTagValue, hasChanged, _ev, input) => {
+		const tag = input.closest('[data-tag]').dataset.tag;
 		setTagRedacted(null);
 		setSuggestions([]);
 		requestId.current += 1;