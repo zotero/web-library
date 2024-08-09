zotero/web-library

commit 18bfe04c62fc2fa34177616f5908fbe11dd9a0e9

Fix compatiblity with sass >=1.77.7. Resolve #553

src/scss/components/_searchbar.scss
@@ -3,8 +3,6 @@
 // --------------------------------------------------
 
 .searchbar {
-	@include hairline(top, "color-shade-4");
-	@include hairline(bottom, "color-shade-4");
 	position: absolute;
 	top: 0;
 	right: 0;
@@ -15,6 +13,8 @@
 	padding: 0 $default-padding-x-touch;
 	background: var(--color-toolbar);
 	z-index: $z-index-search-bar;
+	@include hairline(top, "color-shade-4");
+	@include hairline(bottom, "color-shade-4");
 
 	.input-group.search {
 		margin: 0 $default-padding-x-touch 0 (-$default-padding-x-touch + $space-min);

src/scss/abstracts/mixins/_hairline.scss
@@ -69,7 +69,9 @@ $-colors-bg-lookup: (
 	$height: map-get(map-get($specs, $side), height);
 	$pseudo-element: if($side == top or $side == left, "&::before", "&::after");
 
-	position: relative;
+	& {
+		position: relative;
+	}
 
 	#{$pseudo-element} {
 		content: "";

src/scss/components/_button.scss
@@ -18,9 +18,9 @@
 		padding: $input-btn-padding-y-touch $default-padding-x-touch;
 
 		&.hairline-top {
-			@include hairline(top, "color-shade-4");
 			border-top: none;
 			padding-top: $input-btn-padding-y-touch + 2 * $input-border-width;
+			@include hairline(top, "color-shade-4");
 		}
 
 		&.hairline-start-padding {

src/scss/components/_collection-tree.scss
@@ -32,9 +32,9 @@
 
 
 		@include touch-or-bp-down(sm) {
-			@include hairline(top, "color-shade-4");
 			padding: $border-width $collection-tree-padding-x-touch 0;
 			background: var(--material-toolbar);
+			@include hairline(top, "color-shade-4");
 		}
 
 		@include mouse-and-bp-up(md) {

src/scss/components/_library.scss
@@ -151,6 +151,7 @@
 
 				.icon-previous + ul .current { // Hack: restrict animation to searches started in item details view on tablets
 					.center-axis {
+						animation: fade-in-search-results $fade-transition-duration ease-in-out forwards;
 						@keyframes fade-in-search-results {
 							0% {
 								opacity: 0;
@@ -160,8 +161,6 @@
 								opacity: 1;
 							}
 						}
-
-						animation: fade-in-search-results $fade-transition-duration ease-in-out forwards;
 					}
 				}
 	  	}

src/scss/components/_touch-footer.scss
@@ -4,11 +4,11 @@
 
 .touch-footer {
 	flex: 0 0 auto;
-	@include hairline(top, "color-shade-4");
 	padding: $border-width 0;
 	height: $touch-footer-height;
 	background-color: $touch-footer-bg;
 	color: $touch-footer-color;
+	@include hairline(top, "color-shade-4");
 
 	&.darker {
 		background-color: $touch-footer-darker-bg;

src/scss/components/_touch-header.scss
@@ -9,9 +9,9 @@
 	color: $touch-header-color;
 	height: $touch-header-height;
 	position: relative;
+	padding: $border-width 0;
 	@include hairline(top, "color-shade-4");
 	@include hairline(bottom, "color-shade-4");
-	padding: $border-width 0;
 
 	&.darker {
 		background-color: $touch-header-darker-bg;

src/scss/components/item/_details.scss
@@ -149,8 +149,8 @@
 	}
 
 	@include touch-and-bp-only(lg) {
-		@include hairline(left, "color-shade-4");
 		flex: 0 0 $item-details-width-lg-touch;
+		@include hairline(left, "color-shade-4");
 	}
 
 	@include mouse-and-bp-only(lg) {

commit 47ceab0d7b2aff6b9f1ea909b612a541f66a9ff9

Fix regression: double "..." button on small devices
* Regression in 18bfe04c
* Extended playwright to include a screenshot for this case

src/js/component/touch-header/searchbar.jsx
@@ -4,7 +4,6 @@ import CSSTransition from 'react-transition-group/cjs/CSSTransition';
 import { Button } from 'web-common/components';
 
 import Search from '../../component/search';
-import { ItemActionsTouch } from '../item/actions'
 import { navigateExitSearch, triggerSearchMode, } from '../../actions';
 
 const SearchBar = () => {
@@ -33,7 +32,6 @@ const SearchBar = () => {
 				<Button onClick={ handleCancelSearchClick } className="btn-link">
 					Cancel
 				</Button>
-				<ItemActionsTouch />
 			</div>
 		</CSSTransition>
 	);

src/scss/components/_searchbar.scss
@@ -13,8 +13,7 @@
 	padding: 0 $default-padding-x-touch;
 	background: var(--color-toolbar);
 	z-index: $z-index-search-bar;
-	@include hairline(top, "color-shade-4");
-	@include hairline(bottom, "color-shade-4");
+	position: relative;
 
 	.input-group.search {
 		margin: 0 $default-padding-x-touch 0 (-$default-padding-x-touch + $space-min);

test/playwright/items.mobile.test.js
@@ -1,5 +1,5 @@
 import { getPort, getServer } from '../utils/fixed-state-server.js';
-import { BrowserStackManager, mobileContexts } from '../utils/browserstack.js';
+import { BrowserStackManager, mobileContexts, singleColumnContexts } from '../utils/browserstack.js';
 import { screenshot } from '../utils/screenshot.js';
 import { expect } from '@playwright/test';
 import { waitForLoad } from '../utils/common.js';
@@ -39,6 +39,17 @@ describe('Mobile Snapshots', () => {
 			const itemsList = page.getByRole('list', { name: 'items' });
 			expect(await itemsList.getByRole('listitem').count()).toBe(7);
 			expect(await screenshot(page, `mobile-items-list-${browserName}`)).toBeTruthy();
+
+			if (singleColumnContexts.includes(browserName)) {
+				// on small screens, enable search mode and take a screenshot
+				const toggleSearch = await page.getByRole('button', { name: 'Toggle search' });
+				await toggleSearch.click();
+				expect(await page.getByRole('searchbox', { name: 'Title, Creator, Year' })).toBeVisible();
+				// avoid flaky screenshot with half-faded search bar
+				await page.waitForFunction(() => document.querySelector('.searchbar').classList.contains('fade-enter-done'));
+				expect(await screenshot(page, `mobile-items-list-search-enabled-${browserName}`)).toBeTruthy();
+			}
+
 			await page.close();
 		});
 

test/utils/browserstack.js
@@ -114,6 +114,11 @@ const contexts = {
 	},
 };
 
+export const singleColumnContexts = [
+	'iphone-emulator',
+	'pixel-7-chrome',
+];
+
 export const mobileContexts = [
 	'iphone-emulator',
 	'ipad-emulator',