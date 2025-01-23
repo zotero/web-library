aa9860a46a7cb5a76e99f48e9695f0dc7b6de8e2
Tom Najdek <tom@doppnet.com>
2025-01-23 16:58:45 +0100

    Make the generate fixtures script more resilient. Fix subprocesses not terminated.

--- scripts/generate-fixtures.js
+++ scripts/generate-fixtures.js
@@ -5,6 +5,7 @@ import { chromium } from 'playwright';
 import { stateProcessSymbols, stateToJSON } from '../test/utils/state.js';
 import secret from '../.secret.json' with { type: "json" };
 import child_process from "child_process";
+import psTree from 'ps-tree';
 
 // This script can be used to generate/update state fixtures used in tests.
 // It starts the web library configured to use the test library and generates state
@@ -19,7 +20,7 @@ import child_process from "child_process";
 
 const URL = 'http://localhost:8001/';
 const ROOT = join(dirname(fileURLToPath(import.meta.url)), "..");
-const NPMST_TIMEOUT = 30000;
+const NPMST_TIMEOUT = 120000; // allow for the worst-case scenario where submodules must be built
 
 const fixtures = [
 	[`${URL}groups/5119976/animals/items/X9WEHDAN/item-list`, 'desktop-test-group-item-view'],
@@ -34,6 +35,7 @@ const fixtures = [
 	[`${URL}testuser/tags/to%20read/search/pathfinding/titleCreatorYear/items/J489T6X3,3JCLFUG4/item-list`, 'desktop-test-user-search-selected'],
 	[`${URL}testuser/collections/CSB4KZUU/items/3JCLFUG4/attachment/37V7V4NT/item-details`, 'desktop-test-user-attachment-in-collection-view'],
 	[`${URL}testuser/trash`, 'desktop-test-user-trash-view'],
+	[`${URL}testuser/collections/CSB4KZUU/items/UMPPCXU4`, 'desktop-test-user-top-level-attachment-view'],
 	[`${URL}testuser/collections/CSB4KZUU/items/3JCLFUG4/item-details`, 'mobile-test-user-item-details-view'],
 	[`${URL}testuser/collections/WTTJ2J56/item-list`, 'mobile-test-user-item-list-view'],
 	[`${URL}testuser/trash/items/Z7B4P73I/item-details`, 'mobile-test-user-trash-collection-details-view'],
@@ -61,10 +63,12 @@ const config = JSON.stringify({
 });
 
 const index = await fs.readFile(join(ROOT, 'src', 'html', 'index.html'), 'utf8');
-const controller = new AbortController();
+let subprocess;
 let indexChanged = false;
+let interrupted = false;
 
 process.on('SIGINT', function () {
+	interrupted = true;
 	if (indexChanged) {
 		restoreIndex();
 	}
@@ -82,24 +86,43 @@ async function restoreIndex() {
 	await fs.writeFile(join(ROOT, 'src', 'html', 'index.html'), index);
 }
 
+class MisconfiguredServer extends Error {
+	constructor(message) {
+		super(message);
+		this.name = 'MisconfiguredServer';
+	}
+}
+
 async function checkOrStartServer() {
 	try {
 		const response = await fetch(URL);
 		const text = await response.text();
-		if(new RegExp(`apiKey: "${secret.apiKey}"`, 'g').test(text)) {
+		if (new RegExp(`apiKey: "${secret.apiKey}"`, 'g').test(text)) {
 			return true;
 		}
-		throw new Error("Server is running, but not with correct config");
-	} catch(e) {
+		throw new MisconfiguredServer("Server is running, but not with correct config. Either change the config or stop the server and run this script again.");
+	} catch (e) {
+		if (e instanceof MisconfiguredServer) {
+			throw e;
+		}
 		console.log('Server does not seem to be running, running "npm start"');
-		const { signal } = controller;
-		child_process.spawn("sh", ["-c", "npm start"], { cwd: ROOT, signal });
+		subprocess = child_process.spawn("sh", ["-c", "npm start"], { cwd: ROOT });
 		const start = Date.now();
+		let jsFileFound = false;
 		do {
+			if (interrupted) {
+				throw new Error("Interrupted");
+			}
 			process.stdout.write(`Waiting for server to start (${((Date.now() - start) / 1000).toFixed(1)}s/${NPMST_TIMEOUT / 1000}s)...`);
 			await new Promise(resolve => setTimeout(resolve, 100));
 			process.stdout.clearLine(1);
 			process.stdout.cursorTo(0);
+
+			if (!(await fs.pathExists(join(ROOT, 'build', 'static', 'zotero-web-library.js')))) {
+				continue;
+			}
+			jsFileFound = true;
+
 			try {
 				const response = await fetch(URL);
 				const text = await response.text();
@@ -108,13 +131,17 @@ async function checkOrStartServer() {
 				} else {
 					throw new Error("Server started, but not with correct config");
 				}
-			} catch(e) {
+			} catch (e) {
 				if (e.code === 'ECONNREFUSED') {
 					continue;
 				}
 			}
-		} while(Date.now() - start < NPMST_TIMEOUT);
-		throw new Error("Server did not start in time");
+		} while (Date.now() - start < NPMST_TIMEOUT);
+		if (jsFileFound) {
+			throw new Error("Server did not start in time");
+		} else {
+			throw new Error("npmst failed to build zotero-web-library.js");
+		}
 	}
 }
 
@@ -153,10 +180,14 @@ async function makeFixture(stateURL, name) {
 	await page.goto(stateURL);
 
 	while (requestsCount > 0 || Date.now() - lastRequestStarted < 5000) {
-		if(requestsCount > 0) {
+		if (requestsCount > 0) {
 			process.stdout.write(`Waiting for ${requestsCount} requests to finish...`);
 		} else {
 			process.stdout.write(`Waiting to see if more requests start...(last request ${((Date.now() - lastRequestStarted) / 1000).toFixed(1)}s ago)`);
+		}
+		if (interrupted) {
+			throw new Error("Interrupted");
+
 		}
 		await new Promise(resolve => setTimeout(resolve, 100));
 		process.stdout.clearLine();
@@ -192,15 +223,30 @@ async function makeFixture(stateURL, name) {
 
 (async () => {
 	await configureIndex();
+	var currentFixtureName;
 	try {
 		await checkOrStartServer();
 		for (const [url, name] of fixtures) {
+			currentFixtureName = name;
 			await makeFixture(url, name);
 		}
-	} catch(e) {
+	} catch (e) {
+		if (currentFixtureName) {
+			console.error(`Error generating fixture for "${currentFixtureName}"`);
+		}
 		console.error(e);
 	} finally {
 		await restoreIndex();
-		controller.abort();
+		console.log('cleaning up');
+		psTree(subprocess.pid, (err, children) => {
+			children.forEach(function (p) {
+				try {
+					process.kill(p.PID, 'SIGINT');
+				} catch (e) {
+					// ignore
+				}
+			});
+		});
+		subprocess.kill('SIGINT');
 	}
 })();
