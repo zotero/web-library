69f2f4c23387e525423bb454655207f43cfb5c50
Tom Najdek <tom@doppnet.com>
2025-05-29 15:31:59 +0200

    Fix "Add Related" button not disabled in read-only libraries

--- src/js/component/item-details/tabs.jsx
+++ src/js/component/item-details/tabs.jsx
@@ -329,6 +329,7 @@ const ItemDetailsTabs = () => {
 								key={ 'related-' + item.key }
 								id={ `${id}-related` }
 								isActive={ activeTab === 'related' }
+								isReadOnly={ isReadOnly }
 							/>
 						) }
 					</Fragment>
