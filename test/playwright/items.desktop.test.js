52435dbb966c67a4d87fe5113f8ebacb2ac44b9c
Tom Najdek <tom@doppnet.com>
2025-05-30 11:02:17 +0200

    Add snapshot tests for "Add Related" modal. Update fixtures.

--- test/playwright/items.desktop.test.js
+++ test/playwright/items.desktop.test.js
@@ -1,7 +1,7 @@
 import { getPort, getServer } from '../utils/fixed-state-server.js';
 import { BrowserStackManager, desktopContexts } from '../utils/browserstack.js';
 import { screenshot } from '../utils/screenshot.js';
-import { waitForLoad } from '../utils/common.js';
+import { wait, waitForLoad } from '../utils/common.js';
 
 jest.setTimeout(60000);
 
@@ -38,7 +38,28 @@ describe('Desktop Snapshots', () => {
 			await page.waitForLoadState('networkidle');
 			const itemsList = await page.getByRole('grid', { name: 'items' });
 			expect(await itemsList.getByRole('row').count()).toBe(8); // 7 items + header row
+			await wait(500); // avoid flaky screenshot with missing icons
 			expect(await screenshot(page, `desktop-items-list-${browserName}`)).toBeTruthy();
+			await page.close();
+		});
+
+		test(`should render "Add Related" modal on "${browserName}"`, async () => {
+			server = await getServer('desktop-test-user-item-view', port);
+			context = await browsers.getBrowserContext(browserName);
+			const page = await context.newPage();
+			await page.goto(`http://localhost:${port}/testuser/collections/WTTJ2J56/items/VR82JUX8/item-details`);
+			await waitForLoad(page);
+
+			const relatedTab = await page.getByRole('tab', { name: 'Related' });
+			await relatedTab.click();
+
+			const addRelatedButton = await page.getByRole('button', { name: 'Add Related Item' });
+			await addRelatedButton.click();
+
+			await page.getByRole('dialog', { name: 'Add Related Items' });
+			await page.waitForFunction(() => document.querySelector('.add-related-modal').classList.contains('ReactModal__Content--after-open'));
+			await wait(500); // avoid flaky screenshot with missing icons
+			expect(await screenshot(page, `desktop-item-add-related-${browserName}`)).toBeTruthy();
 			await page.close();
 		});
 	});
--- test/playwright/items.mobile.test.js
+++ test/playwright/items.mobile.test.js
@@ -1,8 +1,8 @@
 import { getPort, getServer } from '../utils/fixed-state-server.js';
-import { BrowserStackManager, mobileContexts, singleColumnContexts } from '../utils/browserstack.js';
+import { BrowserStackManager, mobileContexts, singleColumnMobileContexts } from '../utils/browserstack.js';
 import { screenshot } from '../utils/screenshot.js';
 import { expect } from '@playwright/test';
-import { waitForLoad } from '../utils/common.js';
+import { waitForLoad, wait } from '../utils/common.js';
 
 jest.setTimeout(60000);
 
@@ -38,9 +38,10 @@ describe('Mobile Snapshots', () => {
 			await waitForLoad(page);
 			const itemsList = page.getByRole('list', { name: 'items' });
 			await expect(itemsList.getByRole('listitem')).toHaveCount(7);
+			await wait(500); // avoid flaky screenshot with missing icons
 			expect(await screenshot(page, `mobile-items-list-${browserName}`)).toBeTruthy();
 
-			if (singleColumnContexts.includes(browserName)) {
+			if (singleColumnMobileContexts.includes(browserName)) {
 				// on small screens, enable search mode and take a screenshot
 				const toggleSearch = await page.getByRole('button', { name: 'Toggle search' });
 				await toggleSearch.click();
@@ -60,6 +61,7 @@ describe('Mobile Snapshots', () => {
 			await page.goto(`http://localhost:${port}/testuser/collections/CSB4KZUU/items/3JCLFUG4/item-details`);
 			await waitForLoad(page);
 			await expect(await page.getByRole('heading', { name: 'Cooperative pathfinding' })).toBeVisible();
+			await wait(500); // avoid flaky screenshot with missing icons
 			expect(await screenshot(page, `mobile-item-details-${browserName}`)).toBeTruthy();
 			await page.close();
 		});
@@ -71,14 +73,32 @@ describe('Mobile Snapshots', () => {
 			await page.goto(`http://localhost:${port}/testuser/trash/items/Z7B4P73I/item-details`);
 			await waitForLoad(page);
 
-			if (singleColumnContexts.includes(browserName)) {
+			if (singleColumnMobileContexts.includes(browserName)) {
 				await page.getByRole('button', { name: 'Collection Trash Options' }).click();
 			}
 
-			const role = singleColumnContexts.includes(browserName) ? 'menuitem' : 'button';
+			const role = singleColumnMobileContexts.includes(browserName) ? 'menuitem' : 'button';
 			await expect(page.getByRole(role, { name: 'Restore to Library' })).toBeVisible();
 			await expect(page.getByRole(role, { name: 'Delete Permanently' })).toBeVisible();
+			await wait(500); // avoid flaky screenshot with missing icons
 			expect(await screenshot(page, `mobile-trash-collection-details-${browserName}`)).toBeTruthy();
+			await page.close();
+		});
+
+		test(`should render "Add Related" modal on "${browserName}"`, async () => {
+			server = await getServer('mobile-test-user-item-details-view-edit', port);
+			context = await browsers.getBrowserContext(browserName);
+			const page = await context.newPage();
+			await page.goto(`http://localhost:${port}/testuser/collections/CSB4KZUU/items/3JCLFUG4/item-details`);
+			await waitForLoad(page);
+
+			const addRelatedButton = await page.getByRole('button', { name: 'Add Related Item' });
+			await addRelatedButton.dispatchEvent('click'); // Use dispatchEvent to avoid switching to desktop mode on ipad-pro-landscape-emulator
+
+			await page.getByRole('dialog', { name: 'Add Related Items' });
+			await page.waitForFunction(() => document.querySelector('.add-related-modal').classList.contains('ReactModal__Content--after-open'));
+			await wait(500); // ensure animation has settled and icons are loaded
+			expect(await screenshot(page, `mobile-item-add-related-${browserName}`)).toBeTruthy();
 			await page.close();
 		});
 	});
