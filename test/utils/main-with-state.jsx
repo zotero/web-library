6ad33742868d81c5087e40ce03f6c006789d4701
Tom Najdek <tom@doppnet.com>
2025-08-27 12:52:30 +0200

    Adjust snapshot tests to work with locally generated bibliographies

--- test/utils/main-with-state.jsx
+++ test/utils/main-with-state.jsx
@@ -3,6 +3,8 @@ export { createRoot } from 'react-dom/client';
 import { ConnectedRouter } from 'connected-react-router'
 import { Provider } from 'react-redux'
 import PropTypes from 'prop-types';
+import { configureZoteroShim } from 'web-common/zotero';
+import schema from 'zotero-schema/schema.json';
 
 import { JSONtoState } from '../utils/state';
 import { MainZotero } from '../../src/js/component/main';
@@ -11,6 +13,9 @@ import { setupStore } from '../../src/js/store'
 export const MainWithState = ({ state = {} }) => {
 	state = JSONtoState(state);
 	const { store, history } = setupStore(state);
+	const mockIntl = { locale: navigator?.language || 'en-US', formatMessage: ({ id, defaultMessage }) => defaultMessage || id };
+	configureZoteroShim(schema, mockIntl);
+
 	return (
 		<Provider store={store}>
 			<ConnectedRouter history={history}>
