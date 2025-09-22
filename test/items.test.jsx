zotero/web-library

commit 0c0c5001bccd6e4e0321dc60858fb39a760d815f

Add support for creating bibliographies for collections

src/js/actions/items-read.js
@@ -344,6 +344,16 @@ const fetchAllChildItems = (itemKey, queryOptions = {}, overrides) => {
 	}
 }
 
+const fetchItemKeys = (type, libraryKey, queryConfig = {}, queryOptions = {}) => {
+	return async (dispatch, getState) => {
+		const config = getState().config;
+		const api = getApiForItems({ config, libraryKey }, type, queryConfig);
+		const response = await api.get({ ...escapeBooleanSearches(queryOptions, 'tag'), format: 'keys' });
+		const keys = (await response.getData().text()).split("\n").filter(k => k !== "");
+		return keys;
+	}
+}
+
 const findRowIndexInSource = () => {
 	return async (dispatch, getState) => {
 		const state = getState();
@@ -354,7 +364,6 @@ const findRowIndexInSource = () => {
 		}
 
 		const libraryKey = state.current.libraryKey;
-		const config = state.config;
 		const { collectionKey, isTrash, isMyPublications, itemsSource, search: q, qmode,
 			tags: tag = [] } = state.current;
 		const { field: sortBy, sort: sortDirection } = state.preferences.columns.find(
@@ -398,9 +407,7 @@ const findRowIndexInSource = () => {
 		}
 
 		try {
-			const api = getApiForItems({ config, libraryKey }, type, queryConfig);
-			const response = await api.get({ ...escapeBooleanSearches(queryOptions, 'tag'), format: 'keys' });
-			const keys = (await response.getData().text()).split("\n");
+			await fetchItemKeys(type, libraryKey, queryConfig, queryOptions);
 			if(keys.indexOf(itemKey) !== -1) {
 				return keys.indexOf(itemKey);
 			}
@@ -410,6 +417,7 @@ const findRowIndexInSource = () => {
 		}
 	}
 }
+
 
 const querySecondary = ({ libraryKey, collectionKey, isTrash, isMyPublications, q, tag, qmode }) => {
 	return {
@@ -423,6 +431,7 @@ export {
 	fetchAllItemsSince,
 	fetchChildItems,
 	fetchItemDetails,
+	fetchItemKeys,
 	fetchItemsByKeys,
 	fetchItemsInCollection,
 	fetchItemsQuery,
@@ -433,7 +442,7 @@ export {
 	fetchTrashItems,
 	findRowIndexInSource,
 	getAttachmentUrl,
+	querySecondary,
 	sortItems,
 	sortItemsSecondary,
-	querySecondary
 };

src/js/actions/cite.js
@@ -78,9 +78,6 @@ export const bibliographyFromItems = (itemKeys, libraryKey) => {
 	};
 };
 
-export const bibliographyFromCollection = (collectionKey, libraryKey, { style = 'chicago-note-bibliography', locale = 'en-US' }) => {
-	throw new Error("TODO: Implement bibliographyFromCollection");
-};
 
 export const citationFromItems = (itemKeys, modifiers, libraryKey) => {
 	return async (dispatch, getState) => {

src/js/component/libraries/collection-tree.jsx
@@ -8,7 +8,7 @@ import { isTriggerEvent, noop, omit, pick } from 'web-common/utils';
 
 import Editable from '../editable';
 import Node from './node';
-import { COLLECTION_RENAME, COLLECTION_ADD, MOVE_COLLECTION } from '../../constants/modals';
+import { BIBLIOGRAPHY, COLLECTION_RENAME, COLLECTION_ADD, MOVE_COLLECTION } from '../../constants/modals';
 import { createAttachmentsFromDropped, toggleModal, updateCollection, updateCollectionsTrash, navigate } from '../../actions';
 import { stopPropagation, getUniqueId } from '../../utils.js';
 import { PICKS_SINGLE_COLLECTION, PICKS_MULTIPLE_COLLECTIONS, PICKS_SINGLE_ITEM, PICKS_MULTIPLE_ITEMS } from '../../constants/picker-modes';
@@ -332,11 +332,12 @@ LevelWrapper.propTypes = {
 
 
 const DotMenu = memo(props => {
-	const { collection, dotMenuFor, focusBySelector, opened, parentLibraryKey, setDotMenuFor, setOpened, setRenaming,
+	const { collection, dotMenuFor, focusBySelector, isReadOnly, opened, parentLibraryKey, setDotMenuFor, setOpened, setRenaming,
 		addVirtual } = props;
 	const dispatch = useDispatch();
 	const currentLibraryKey = useSelector(state => state.current.libraryKey);
 	const currentCollectionKey = useSelector(state => state.current.collectionKey);
+	const hasItems = useSelector(state => (state.libraries[parentLibraryKey]?.itemsByCollection?.[collection.key]?.totalResults ?? Infinity) > 0); // This should always be known but if we somehow don't know, we assume collection has items
 	const isTouchOrSmall = useSelector(state => state.device.isTouchOrSmall);
 
 	const isOpen = dotMenuFor === collection.key;
@@ -433,9 +434,9 @@ const DotMenu = memo(props => {
 	// 	dispatch(toggleModal(EXPORT, true, { collectionKey: collection.key, libraryKey: parentLibraryKey } ));
 	// });
 
-	// const handleBibliographyClick = useCallback(() => {
-	// 	dispatch(toggleModal(BIBLIOGRAPHY, true, { collectionKey: collection.key, libraryKey: parentLibraryKey } ));
-	// });
+	const handleBibliographyClick = useCallback(() => {
+		dispatch(toggleModal(BIBLIOGRAPHY, true, { collectionKey: collection.key, libraryKey: parentLibraryKey } ));
+	}, [collection.key, dispatch, parentLibraryKey]);
 
 	return (
         <Dropdown
@@ -456,6 +457,7 @@ const DotMenu = memo(props => {
 			<DropdownMenu>
 				<Fragment>
 				<DropdownItem
+					disabled={isReadOnly}
 					onBlur={ stopPropagation }
 					onMouseDown={ stopPropagation }
 					onClick={ handleRenameClick }
@@ -464,6 +466,7 @@ const DotMenu = memo(props => {
 					Rename
 				</DropdownItem>
 				<DropdownItem
+					disabled={isReadOnly}
 					onBlur={ stopPropagation }
 					onMouseDown={ stopPropagation }
 					onClick={ handleSubcollectionClick }
@@ -472,6 +475,7 @@ const DotMenu = memo(props => {
 					New Subcollection
 				</DropdownItem>
 				<DropdownItem
+					disabled={isReadOnly}
 					onBlur={ stopPropagation }
 					onMouseDown={ stopPropagation }
 					onClick={ handleMoveCollectionClick }
@@ -481,6 +485,7 @@ const DotMenu = memo(props => {
 				</DropdownItem>
 				<DropdownItem divider />
 				<DropdownItem
+					disabled={isReadOnly}
 					onBlur={stopPropagation}
 					onMouseDown={stopPropagation}
 					onClick={handleDeleteClick}
@@ -488,6 +493,16 @@ const DotMenu = memo(props => {
 				>
 					Delete
 				</DropdownItem>
+				<DropdownItem divider />
+					<DropdownItem
+						disabled={!hasItems}
+						onBlur={stopPropagation}
+						onMouseDown={stopPropagation}
+						onClick={handleBibliographyClick}
+						onKeyDown={handleBibliographyClick}
+					>
+						Create Bibliography
+					</DropdownItem>
 				</Fragment>
 			</DropdownMenu>
 		</Dropdown>
@@ -762,16 +777,17 @@ const CollectionNode = memo(props => {
 						>
 							{ collectionName }
 						</div>
-						{(!isReadOnly && !pickerMode) ? (
+						{pickerMode ? null : (
 							<DotMenu
 								collection = { collection }
 								setRenaming = { setRenaming }
 								parentLibraryKey = { parentLibraryKey }
+								isReadOnly={isReadOnly}
 								{ ...pick(rest, ['addVirtual', 'commitAdd', 'cancelAdd',
 								'dotMenuFor', 'focusBySelector', 'onFocusNext', 'opened',
 								'setDotMenuFor', 'setOpened']) }
 							/>
-						) : null }
+						)}
 					</Fragment>
 				)
 			}

src/js/component/modal/bibliography.jsx
@@ -7,7 +7,7 @@ import cx from 'classnames';
 
 import { BIBLIOGRAPHY } from '../../constants/modals';
 import { stripTagsUsingDOM } from '../../common/format';
-import { toggleModal, fetchCSLStyle, bibliographyFromItems, triggerSelectMode, fetchItemsByKeys } from '../../actions';
+import { toggleModal, fetchItemKeys, fetchCSLStyle, bibliographyFromItems, triggerSelectMode, fetchItemsByKeys } from '../../actions';
 import CitationOptions from '../citation-options';
 import Modal from '../ui/modal';
 import RadioSet from '../form/radio-set';
@@ -23,6 +23,7 @@ const BibliographyModal = () => {
 	const libraryKey = useSelector(state => state.modal.libraryKey);
 	const requestsPending = useSelector(state => state.libraries[libraryKey]?.sync?.requestsPending);
 	const itemKeys = useSelector(state => state.modal.itemKeys);
+	const prevItemKeys = usePrevious(itemKeys);
 	const itemsLookup = useSelector(state => state.libraries[libraryKey]?.dataObjects);
 	const hasMissingItems = (itemKeys || []).some(key => !(key in itemsLookup));
 	const hadMissingItems = usePrevious(hasMissingItems);
@@ -35,7 +36,7 @@ const BibliographyModal = () => {
 	const prevCitationStyle = usePrevious(citationStyle);
 	const prevCitationLocale = usePrevious(citationLocale);
 
-	const [isItemsReady, setIsItemsReady] = useState(!hasMissingItems);
+	const [isItemsReady, setIsItemsReady] = useState(!hasMissingItems && !collectionKey);
 	const [requestedAction, setRequestedAction] = useState('clipboard');
 	const [isClipboardCopied, setIsClipboardCopied] = useState(false);
 	const [isHtmlCopied, setIsHtmlCopied] = useState(false);
@@ -50,20 +51,11 @@ const BibliographyModal = () => {
 	const makeOutput = useCallback(async () => {
 		try {
 			setIsUpdating(true);
-			var nextOutput;
-			if (collectionKey) {
-				throw new Error('TODO: Collection support.');
-				// nextOutput = await dispatch(bibliographyFromCollection(
-				// 	collectionKey, libraryKey, { style: citationStyle, locale: citationLocale }
-				// ));
-			} else {
-				nextOutput = await dispatch(bibliographyFromItems(itemKeys, libraryKey,));
-			}
-			setOutput(nextOutput);
+			setOutput(await dispatch(bibliographyFromItems(itemKeys, libraryKey)));
 		} finally {
 			setIsUpdating(false);
 		}
-	}, [collectionKey, dispatch, itemKeys, libraryKey]);
+	}, [dispatch, itemKeys, libraryKey]);
 
 	const copyToClipboard = useCallback(bibliographyToCopy => {
 		const bibliographyText = stripTagsUsingDOM(bibliographyToCopy);
@@ -120,19 +112,24 @@ const BibliographyModal = () => {
 		} else {
 			copyToClipboard(output);
 		}
-		dispatch(toggleModal(BIBLIOGRAPHY, false));
+		dispatch(toggleModal(BIBLIOGRAPHY, false, { itemKeys, libraryKey }));
 		dispatch(triggerSelectMode(false, true));
-	}, [output, copyToClipboard, dispatch, requestedAction]);
+	}, [requestedAction, dispatch, itemKeys, libraryKey, output, copyToClipboard]);
 
 	const handleCancel = useCallback(async () => {
-		dispatch(toggleModal(BIBLIOGRAPHY, false));
+		dispatch(toggleModal(BIBLIOGRAPHY, false, { itemKeys, libraryKey }));
 		setOutput('');
-	}, [dispatch]);
+	}, [dispatch, itemKeys, libraryKey]);
 
 	const handleBibliographyClick = useCallback(() => {
-		dispatch(toggleModal(BIBLIOGRAPHY, false));
+		dispatch(toggleModal(BIBLIOGRAPHY, false, { itemKeys, libraryKey }));
 		dispatch(toggleModal('COPY_CITATION', true, { itemKeys, libraryKey }));
 	}, [dispatch, itemKeys, libraryKey]);
+
+	const fetchCollectionItemKeys = useCallback(async () => {
+		const itemKeys = await dispatch(fetchItemKeys('ITEMS_IN_COLLECTION', libraryKey, { collectionKey }));
+		dispatch(toggleModal(BIBLIOGRAPHY, true, { libraryKey, itemKeys }));
+	}, [collectionKey, dispatch, libraryKey]);
 
 	useEffect(() => {
 		document.addEventListener('copy', handleCopy, true);
@@ -172,9 +169,9 @@ const BibliographyModal = () => {
 	useEffect(() => {
 		if (!isItemsReady && hasMissingItems && requestsPending === 0) {
 			const missingKeys = itemKeys.filter(key => !(key in itemsLookup));
-			dispatch(fetchItemsByKeys(missingKeys));
+			dispatch(fetchItemsByKeys(missingKeys, {}, { current: { libraryKey } }));
 		}
-	}, [dispatch, hasMissingItems, isItemsReady, itemKeys, itemsLookup, requestsPending]);
+	}, [dispatch, hasMissingItems, isItemsReady, itemKeys, itemsLookup, libraryKey, requestsPending]);
 
 	// regenerate bibliography when modal opens with some items initially missing and have been fetched
 	useEffect(() => {
@@ -184,12 +181,27 @@ const BibliographyModal = () => {
 		}
 	}, [hadMissingItems, hasMissingItems, makeOutput]);
 
-
 	useEffect(() => {
 		if (!isDropdownOpen && wasDropdownOpen) {
 			setIsHtmlCopied(false);
 		}
 	}, [isDropdownOpen, wasDropdownOpen]);
+
+	useEffect(() => {
+		if (collectionKey) {
+			fetchCollectionItemKeys();
+		}
+	}, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+	useEffect(() => {
+		if(itemKeys !== prevItemKeys) {
+			const newIsItemsReady = !(itemKeys || []).some(key => !(key in itemsLookup));
+			setIsItemsReady(newIsItemsReady);
+			if(styleXml && newIsItemsReady) {
+				makeOutput();
+			}
+		}
+	}, [itemKeys, prevItemKeys, itemsLookup, makeOutput, styleXml]);
 
 	const className = cx({
 		'bibliography-modal': true,
@@ -201,7 +213,7 @@ const BibliographyModal = () => {
 		<Modal
 			className={className}
 			contentLabel={'Bibliography'}
-			isBusy={!isItemsReady}
+			isBusy={!isItemsReady || !!collectionKey /* show only spinner if collectionKey is set, while we fetch keys and re-open modal with itemKeys set instead */ }
 			isOpen={isOpen}
 			onRequestClose={handleCancel}
 			overlayClassName={cx({ 'modal-centered modal-slide': isTouchOrSmall })}

src/js/component/modal/copy-citation.jsx
@@ -689,10 +689,10 @@ const CopyCitationModal = () => {
 		if (!state.isItemsReady && hasMissingItems && requestsPending === 0) {
 			const missingKeys = itemKeys.filter(key => !(key in itemsLookup));
 			if (missingKeys.length) {
-				dispatch(fetchItemsByKeys(missingKeys));
+				dispatch(fetchItemsByKeys(missingKeys, {}, { current: { libraryKey } }));
 			}
 		}
-	}, [dispatch, hasMissingItems, itemKeys, itemsLookup, requestsPending, state.isItemsReady]);
+	}, [dispatch, hasMissingItems, itemKeys, itemsLookup, libraryKey, requestsPending, state.isItemsReady]);
 
 	useEffect(() => {
 		if (hadMissingItems && !hasMissingItems) {

commit 566eb854aef705ecd6ff076520437144f04f8391

Fix a regression where an item would not scroll into view
Resolves #629
Regression was in 0c0c500
Also introduced a test to cover this scenario

src/js/actions/items-read.js
@@ -407,7 +407,7 @@ const findRowIndexInSource = () => {
 		}
 
 		try {
-			await fetchItemKeys(type, libraryKey, queryConfig, queryOptions);
+			keys = await dispatch(fetchItemKeys(type, libraryKey, queryConfig, queryOptions));
 			if(keys.indexOf(itemKey) !== -1) {
 				return keys.indexOf(itemKey);
 			}

test/items.test.jsx
@@ -30,6 +30,7 @@ import testUserSearchResults from './fixtures/response/test-user-search-results.
 import testUserSearchResultsTags from './fixtures/response/test-user-search-results-tags.json';
 import testUserItemsGolden from './fixtures/response/test-user-get-items-golden.json';
 import testUserItemsGoldenTags from './fixtures/response/test-user-get-items-golden-tags.json';
+import minimalState from './fixtures/state/minimal.json';
 
 const state = JSONtoState(stateRaw);
 const libraryViewState = JSONtoState(stateLibraryView);
@@ -417,4 +418,64 @@ describe('Items', () => {
 		const row = screen.getByRole('row', { name: 'Retriever' });
 		expect(getByText(row, '🐕')).toBeInTheDocument();
 	});
+
+	test("Loads items around the item from the URL", async () => {
+		let counter = 0;
+		server.use(
+			http.get('https://api.zotero.org/users/475425/settings/tagColors', () => {
+				return HttpResponse.json({ "version": 1, "value": [{ "name": "green", "color": "#00FF00" }, { "name": "violet", "color": "#8A2BE2" }] });
+			}),
+			http.get('https://api.zotero.org/users/475425/collections', async ({ request }) => {
+				return HttpResponse.json([], {
+					headers: { 'Total-Results': '0' },
+				});
+			}),
+			http.get('https://api.zotero.org/users/475425/items', ({ request }) => {
+				const url = new URL(request.url);
+				const itemKey = url.searchParams.get('itemKey');
+				expect(itemKey).toBe('QWERTY12');
+				return HttpResponse.json([{ "key": "QWERTY12", "version": 1, itemType: "book", title: "", creators: [] }]);
+			}),
+			http.get('https://api.zotero.org/users/475425/items/top', ({ request }) => {
+				const url = new URL(request.url);
+				const format = url.searchParams.get('format');
+
+				if(counter === 0) {
+					// First request to items should be for keys, return 5000 + QWERTY12
+					expect(format).toBe('keys');
+					const arr = Array.from({ length: 5000 }, (_, i) => `ABCD${String(i + 1).padStart(4, '0')}`);
+					arr.splice(1489, 0, 'QWERTY12');
+					counter++;
+					return HttpResponse.text(arr.join('\n'), {
+						headers: { 'Total-Results': '5001', 'Content-Type': 'text/plain' },
+					});
+				} else {
+					// Second request to items should be for json, starting around where the item was found
+					expect(format).toBe('json');
+					expect(parseInt(url.searchParams.get('start'))).toBeGreaterThan(1400);
+					counter++;
+					return HttpResponse.json(testUserItemsGolden, {
+						headers: {
+							'Total-Results': '5001',
+						},
+					});
+				}
+			}),
+			http.get('https://api.zotero.org/users/475425/items/top/tags', () => {
+				return HttpResponse.json([], {
+					headers: {
+						'Total-Results': '0',
+					},
+				});
+			})
+		);
+
+		delete window.location;
+		window.location = new URL('http://localhost/testuser/items/QWERTY12/library');
+		renderWithProviders(<MainZotero />, { preloadedState: minimalState });
+		await waitForPosition();
+		// Ensure that we fetched items twice: first keys, then json.
+		await waitFor(() => expect(counter).toBeGreaterThan(1), { timeout: 3000 });
+	});
 });
+