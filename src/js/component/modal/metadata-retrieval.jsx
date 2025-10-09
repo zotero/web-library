zotero/web-library

commit e5b23eb56dbb9338b76124b151f1c36b83a90f9d

Update dependencies and fix compatibility. Resolve #584, #573

src/js/component/modal/metadata-retrieval.jsx
@@ -107,11 +107,10 @@ MetadataRetrievalTitleCell.propTypes = {
 }
 
 const MetadataRetrievalTableRow = memo(props => {
-	const { data, index, style } = props;
+	const { index, style, columns, getItemData } = props;
 	const id = useId();
 	const labelledById = `${id}-title`;
 
-	const { columns, getItemData } = data;
 	const itemData = getItemData(index);
 
 	return (
@@ -152,9 +151,11 @@ const MetadataRetrievalTableRow = memo(props => {
 
 MetadataRetrievalTableRow.displayName = 'MetadataRetrievalTableRow';
 MetadataRetrievalTableRow.propTypes = {
+	columns: PropTypes.array,
 	data: PropTypes.object.isRequired,
+	getItemData: PropTypes.func.isRequired,
 	index: PropTypes.number.isRequired,
-	style: PropTypes.object.isRequired
+	style: PropTypes.object.isRequired,
 }
 
 

jest.config.mjs
@@ -6,30 +6,27 @@
 export default {
 	coverageProvider: "v8",
 	maxWorkers: process.env.CI ? '50%' : '100%',
-	projects: [{
-		displayName: "components",
-		moduleDirectories: [
-			"src/js/",
-			"modules",
-			"node_modules",
-		],
-		moduleNameMapper: {
-			'^web-worker:(?:..?/)+(.*)$': '<rootDir>/test/mocks/$1',
-			'^(?:.*?)/zotero-streaming-client$': '<rootDir>/test/mocks/noop.js',
-			'^react-virtualized-auto-sizer$': '<rootDir>/test/mocks/react-virtualized-auto-sizer.js',
-		},
-		restoreMocks: true,
-		setupFiles: ['./test/utils/jest-setup.js'],
-		testEnvironment: "./test/utils/env-with-fetch.js",
-		testEnvironmentOptions: {
-			customExportConditions: [''],
-		},
-		testMatch: [
-			"<rootDir>/test/*.test.js?(x)",
-		],
-		transformIgnorePatterns: [
-			"/node_modules/(?!react-dnd|dnd-core|@react-dnd|dnd-multi-backend|rdndmb-html5-to-touch)",
-			"\\.pnp\\.[^\\/]+$"
-		],
-	}],
+	moduleDirectories: [
+		"src/js/",
+		"modules",
+		"node_modules",
+	],
+	moduleNameMapper: {
+		'^web-worker:(?:..?/)+(.*)$': '<rootDir>/test/mocks/$1',
+		'^(?:.*?)/zotero-streaming-client$': '<rootDir>/test/mocks/noop.js',
+		'^react-virtualized-auto-sizer$': '<rootDir>/test/mocks/react-virtualized-auto-sizer.js',
+	},
+	restoreMocks: true,
+	setupFiles: ['./test/utils/jest-setup.js'],
+	testEnvironment: "./test/utils/env-with-fetch.js",
+	testEnvironmentOptions: {
+		customExportConditions: [''],
+	},
+	testMatch: [
+		"<rootDir>/test/*.test.js?(x)",
+	],
+	transformIgnorePatterns: [
+		"/node_modules/(?!until-async|balanced-match|react-dnd|dnd-core|@react-dnd|dnd-multi-backend|rdndmb-html5-to-touch)",
+		"\\.pnp\\.[^\\/]+$"
+	],
 };

rollup.config.js
@@ -88,6 +88,8 @@ const config = {
 				'node_modules/@reduxjs/**',
 				'node_modules/immer/**',
 				'node_modules/reselect/**',
+				'node_modules/react-window/**',
+
 			],
 			babelHelpers: 'bundled'
 		}),

src/js/component/common/list.jsx
@@ -1,11 +1,11 @@
-import { memo, useRef } from 'react';
+import { memo } from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 import AutoSizer from 'react-virtualized-auto-sizer';
-import InfiniteLoader from "react-window-infinite-loader";
-import { FixedSizeList as ReactWindowList } from 'react-window';
+import { useInfiniteLoader } from "react-window-infinite-loader";
 import { noop } from 'web-common/utils';
 
+import ReactWindowList from './react-window-list';
 import ListRow from './list-row';
 import { alwaysTrue } from '../../utils';
 import { SCROLL_BUFFER, LIST_ROW_HEIGHT } from '../../constants/constants';
@@ -20,8 +20,8 @@ const List = props => {
 		getItemData,
 		isReady = true,
 		itemCount,
+		listRef = null,
 		listClassName = null,
-		loaderRef = null,
 		onDoubleClick = noop,
 		isItemLoaded = alwaysTrue,
 		onLoadMore = noop,
@@ -34,7 +34,6 @@ const List = props => {
 	} = props;
 
 	const ListItemComponent = listItemComponent || ListRow;
-	const listRef = useRef(null);
 
 	const itemData = {
 		selectedIndexes,
@@ -44,41 +43,34 @@ const List = props => {
 		...extraItemData
 	};
 
+	const onRowsRendered = useInfiniteLoader({
+		isRowLoaded: isItemLoaded,
+		rowCount: itemCount,
+		loadMoreRows: onLoadMore
+	});
+
 	return (
 		<div className={cx("items-list-wrap", containerClassName)}>
 			{ isReady && (
 				<AutoSizer>
 					{({ height, width }) => (
-						<InfiniteLoader
-							ref={loaderRef}
-							listRef={listRef}
-							isItemLoaded={isItemLoaded}
-							itemCount={itemCount}
-							loadMoreItems={onLoadMore}
+						<div
+							aria-label={ariaLabel}
+							className={cx('items-list', listClassName)}
+							role={role}
+							aria-rowcount={totalResults}
 						>
-							{({ onItemsRendered, ref }) => (
-								<div
-									aria-label={ariaLabel}
-									className={cx('items-list', listClassName)}
-									role={role}
-									aria-rowcount={totalResults}
-								>
-									<ReactWindowList
-										initialScrollOffset={Math.max(scrollToRow - SCROLL_BUFFER, 0) * LIST_ROW_HEIGHT}
-										height={height}
-										itemCount={itemCount}
-										itemData={itemData}
-										itemSize={LIST_ROW_HEIGHT}
-										onItemsRendered={onItemsRendered}
-										ref={r => { ref(r); listRef.current = r; props.listRef && (props.listRef.current = r); }}
-
-										width={width}
-									>
-										{ ListItemComponent }
-									</ReactWindowList>
-								</div>
-							)}
-						</InfiniteLoader>
+							<ReactWindowList
+								rowComponent={ListItemComponent}
+								initialScrollToRow={Math.max(scrollToRow - SCROLL_BUFFER, 0)}
+								rowCount={itemCount}
+								rowProps={itemData}
+								rowHeight={LIST_ROW_HEIGHT}
+								onRowsRendered={onRowsRendered}
+								listRef={listRef}
+								style={{ width, height }}
+							/>
+						</div>
 					)}
 				</AutoSizer>
 			) }
@@ -99,9 +91,6 @@ List.propTypes = {
 	listClassName: PropTypes.string,
 	listItemComponent: PropTypes.elementType,
 	listRef: PropTypes.shape({
-		current: PropTypes.object,
-	}),
-	loaderRef: PropTypes.shape({
 		current: PropTypes.object,
 	}),
 	onDoubleClick: PropTypes.func,

src/js/component/common/react-window-list.jsx
@@ -1,0 +1,56 @@
+import PropTypes from 'prop-types';
+import { useCallback, useRef } from 'react';
+import { List as UpstreamReactWindowList } from '@tnajdek/react-window';
+
+// This wrapper adds support for initialScrollToRow, and will skip calls to onRowsRendered until after the initial scroll
+const ReactWindowList = ({ initialScrollToRow, listRef: listRefFromProps, onRowsRendered, ...rest}) => {
+	const listRef = useRef(null);
+	const isScrolled = useRef(false);
+
+	const handleListRef = useCallback(r => {
+		listRef.current = r;
+		if (listRef) {
+			listRef.current = r;
+		}
+		if (listRefFromProps) {
+			if(typeof listRefFromProps === 'function') {
+				listRefFromProps(r);
+			} else if (typeof listRefFromProps === 'object') {
+				listRefFromProps.current = r;
+			}
+		}
+		if (initialScrollToRow && r && !isScrolled.current) {
+			r.scrollToRow({ index: initialScrollToRow, behavior: 'instant' });
+			setTimeout(() => {
+				isScrolled.current = true;
+			}, 0);
+		}
+	}, [listRefFromProps, initialScrollToRow]);
+
+	const handleRowsRendered = useCallback(args => {
+		if(initialScrollToRow && !isScrolled.current) {
+			// ignore calls to onRowsRendered until after the initial scroll
+			return;
+		}
+		onRowsRendered?.(args);
+	}, [initialScrollToRow, onRowsRendered]);
+
+	return <UpstreamReactWindowList
+		{...rest}
+		onRowsRendered={ handleRowsRendered }
+		listRef={handleListRef}
+	/>;
+};
+
+ReactWindowList.displayName = 'ReactWindowList';
+ReactWindowList.propTypes = {
+	initialScrollToRow: PropTypes.number,
+	listRef: PropTypes.oneOfType([
+		PropTypes.func,
+		PropTypes.shape({ current: PropTypes.any }),
+	]),
+	onRowsRendered: PropTypes.func,
+};
+
+export default ReactWindowList;
+

src/js/component/common/table-row.jsx
@@ -6,13 +6,12 @@ import { TitleCell, AttachmentCell, GenericDataCell, PlaceholderCell } from './t
 import { noop } from 'web-common/utils';
 
 const TableRow = props => {
-	const { data, index, style, className = "item" } = props;
+	const { index, style, className = "item", columns, selectedIndexes, getItemData, onSelect = noop, onDoubleClick = noop } = props;
 	const id = useId();
 	const labelledById = `${id}-title`;
 	const ref = useRef();
 	const ignoreClicks = useRef({});
 
-	const { columns, selectedIndexes, getItemData, onSelect = noop, onDoubleClick = noop } = data;
 	const itemData = getItemData(index);
 	const isSelected = selectedIndexes.includes(index);
 	const selectedItemKeysLength = selectedIndexes.length;

src/js/component/common/table.jsx
@@ -3,10 +3,10 @@ import { useSelector } from 'react-redux';
 import cx from 'classnames';
 import PropTypes from 'prop-types';
 import AutoSizer from 'react-virtualized-auto-sizer';
-import InfiniteLoader from "react-window-infinite-loader";
-import { FixedSizeList as ReactWindowList } from 'react-window';
+import { useInfiniteLoader } from "react-window-infinite-loader";
 import { noop } from 'web-common/utils';
 
+import ReactWindowList from './react-window-list';
 import { alwaysTrue, resizeVisibleColumns } from '../../utils';
 import { useThrottledCallback } from 'use-debounce';
 import HeaderRow from './table-header-row';
@@ -36,7 +36,6 @@ const Table = props => {
 		isReady = true,
 		isItemLoaded = alwaysTrue,
 		itemCount,
-		loaderRef = null,
 		onChangeSortOrder = noop,
 		onColumnsReorder = noop,
 		onColumnsResize = noop,
@@ -69,9 +68,7 @@ const Table = props => {
 
 	const containerRef = useRef(null);
 	const tableRef = useRef(null);
-	const listRef = useRef(null);
 	const outerRef = useRef(null);
-	const innerRef = useRef(null);
 
 	const resizing = useRef(null);
 	const reordering = useRef(null);
@@ -81,14 +78,41 @@ const Table = props => {
 	const [isReordering, setIsReordering] = useState(false);
 	const [reorderTarget, setReorderTarget] = useState(null);
 
-	const handleTableFocus = useCallback(ev => {
+	const onRowsRendered = useInfiniteLoader({
+		isRowLoaded: isItemLoaded,
+		loadMoreRows: onLoadMore,
+		rowCount: itemCount,
+	});
+
+	const handleListRef = useCallback(r => {
+		if (props.listRef) {
+			if (typeof props.listRef === 'function') {
+				props.listRef(r);
+			}
+			else if (typeof props.listRef === 'object') {
+				props.listRef.current = r;
+			}
+		}
+
+		// With react-window 2.x it's no longer possible to get the ref to the outer element it
+		// renders, so instead we query for it as soon as it signals listRef is ready
+		outerRef.current = tableRef.current?.querySelector('[role="rowgroup"]');
+
 		// The overflow list is made focusable by default in most browsers and to prevent this we
 		// need to set `tabIndex` to -1. However, since this element is rendered by react-window, we
 		// cannot pass it as a prop. As a workaround, we set the tabIndex attribute directly here.
 		// This fixes issue #519.
 		outerRef.current?.setAttribute?.('tabindex', '-1');
-		onReceiveFocus(ev);
-	}, [onReceiveFocus]);
+
+		if (props.outerRef) {
+			if (typeof props.outerRef === 'function') {
+				props.outerRef(outerRef.current);
+			}
+			else if (typeof props.outerRef === 'object') {
+				props.outerRef.current = outerRef.current;
+			}
+		}
+	}, [props]);
 
 	const handleResize = useCallback(ev => {
 		const columnDom = ev.target.closest(['[data-colindex]']);
@@ -241,60 +265,47 @@ const Table = props => {
 			{isReady && (
 				<AutoSizer>
 					{({ height, width }) => (
-						<InfiniteLoader
-							isItemLoaded={isItemLoaded}
-							itemCount={itemCount}
-							loadMoreItems={onLoadMore}
-							ref={loaderRef}
+						<div
+							tabIndex={0}
+							onFocus={onReceiveFocus}
+							onBlur={onReceiveBlur}
+							onKeyDown={onKeyDown}
+							ref={ r => { tableRef.current = r; props.tableRef && (props.tableRef.current = r); } }
+							className={cx('items-table', tableClassName)}
+							style={getColumnCssVars(columns, width, scrollbarWidth)}
+							role="grid"
+							aria-multiselectable="true"
+							aria-readonly="true"
+							aria-label={ariaLabel}
+							data-width={width}
+							aria-rowcount={totalResults}
 						>
-							{({ onItemsRendered, ref }) => (
-								<div
-									tabIndex={0}
-									onFocus={handleTableFocus}
-									onBlur={onReceiveBlur}
-									onKeyDown={onKeyDown}
-									ref={ r => { tableRef.current = r; props.tableRef && (props.tableRef.current = r); } }
-									className={cx('items-table', tableClassName)}
-									style={getColumnCssVars(columns, width, scrollbarWidth)}
-									role="grid"
-									aria-multiselectable="true"
-									aria-readonly="true"
-									aria-label={ariaLabel}
-									data-width={width}
-									aria-rowcount={totalResults}
-								>
-									<HeaderRow
-										ref={headerRef}
-										columns={columns}
-										width={width}
-										onResize={handleResize}
-										onReorder={handleReorder}
-										sortBy={sortBy}
-										sortDirection={sortDirection}
-										isResizing={isResizing}
-										isReordering={isReordering}
-										reorderTarget={reorderTarget}
-										onChangeSortOrder={onChangeSortOrder}
-									/>
-									<ReactWindowList
-										initialScrollOffset={Math.max(scrollToRow - SCROLL_BUFFER, 0) * ROW_HEIGHT}
-										outerElementType={TableBody}
-										className={cx("items-table-body", bodyClassName)}
-										height={height - ROW_HEIGHT} // add margin for HeaderRow
-										itemCount={itemCount}
-										itemData={itemData}
-										itemSize={ROW_HEIGHT}
-										onItemsRendered={onItemsRendered}
-										ref={r => { ref(r); listRef.current = r; props.listRef && (props.listRef.current = r); }}
-										outerRef={r => { outerRef.current = r; props.outerRef && (props.outerRef.current = r); } }
-										innerRef={r => { innerRef.current = r; props.innerRef && (props.innerRef.current = r); } }
-										width={width}
-									>
-										{RowComponent}
-									</ReactWindowList>
-								</div>
-							)}
-						</InfiniteLoader>
+							<HeaderRow
+								ref={headerRef}
+								columns={columns}
+								width={width}
+								onResize={handleResize}
+								onReorder={handleReorder}
+								sortBy={sortBy}
+								sortDirection={sortDirection}
+								isResizing={isResizing}
+								isReordering={isReordering}
+								reorderTarget={reorderTarget}
+								onChangeSortOrder={onChangeSortOrder}
+							/>
+							<ReactWindowList
+								rowComponent={RowComponent}
+								initialScrollToRow={Math.max(scrollToRow - SCROLL_BUFFER, 0)}
+								tagName={TableBody}
+								className={cx("items-table-body", bodyClassName)}
+								rowHeight={ROW_HEIGHT} // add margin for HeaderRow
+								rowCount={itemCount}
+								rowProps={itemData}
+								onRowsRendered={onRowsRendered}
+								listRef={handleListRef}
+								style={{ width, height: `${height - ROW_HEIGHT}px` }}
+							/>
+						</div>
 					)}
 				</AutoSizer>
 			)}
@@ -318,12 +329,10 @@ Table.propTypes = {
 	getItemData: PropTypes.func.isRequired,
 	handleKeyDown: PropTypes.func,
 	headerRef: PropTypes.shape({ current: PropTypes.instanceOf(Element) }),
-	innerRef: PropTypes.shape({ current: PropTypes.instanceOf(Element) }),
 	isItemLoaded: PropTypes.func,
 	isReady: PropTypes.bool,
 	itemCount: PropTypes.number.isRequired,
 	listRef: PropTypes.shape({ current: PropTypes.instanceOf(Element) }),
-	loaderRef: PropTypes.shape({ current: PropTypes.instanceOf(Object) }),
 	outerRef: PropTypes.shape({ current: PropTypes.instanceOf(Element) }),
 	onChangeSortOrder: PropTypes.func,
 	onColumnsReorder: PropTypes.func,

src/js/component/item-details/attachments.jsx
@@ -284,7 +284,7 @@ const Attachment = memo(props => {
 	const isTouchOrSmall = useSelector(state => state.device.isTouchOrSmall);
 
 	const [isFocused, setIsFocused] = useState(false);
-	const [_, drag] = useDrag({ // eslint-disable-line no-unused-vars
+	const [_, dragRef] = useDrag({ // eslint-disable-line no-unused-vars
 		type: ATTACHMENT,
 		item: { itemKey, libraryKey },
 		end: (item, monitor) => {
@@ -341,7 +341,9 @@ const Attachment = memo(props => {
 		setIsFocused(false);
 	}, [isTouchOrSmall, receiveBlur]);
 
-	return drag(
+	dragRef(ref);
+
+	return (
 		<li
 			aria-labelledby={id.current}
 			aria-current={isSelected}

commit af812a79084d00f5c0bfe9eaac440919c8aee7c6

Fix a couple of edge case issues related to metadata retrieval
* Fix metadata retrieval broken on touch devices (regression in e5b23eb5)
* Fix auto-rename fails if metadata retrieval triggered immediately after file upload

src/js/component/modal/metadata-retrieval.jsx
@@ -13,8 +13,7 @@ import Items from '../common/items';
 import { GenericDataCell, default as Cell } from '../common/table-cell';
 
 const MetadataRetrievalListItem = props => {
-	const { data, index, style, className } = props;
-	const { getItemData } = data;
+	const { className, getItemData, index, style } = props;
 	const itemData = getItemData(index);
 	const { parentItemTitle, title, iconName, } = itemData;
 	const colorScheme = useSelector(state => state.preferences.colorScheme);
@@ -59,10 +58,11 @@ const MetadataRetrievalListItem = props => {
 }
 
 MetadataRetrievalListItem.propTypes = {
-	data: PropTypes.object.isRequired,
-	index: PropTypes.number.isRequired,
-	style: PropTypes.object.isRequired,
-	className: PropTypes.string
+    className: PropTypes.string,
+    data: PropTypes.object.isRequired,
+    getItemData: PropTypes.func,
+    index: PropTypes.number.isRequired,
+    style: PropTypes.object.isRequired
 };
 
 
@@ -202,7 +202,7 @@ const MetadataRetrievalModal = () => {
 	}, [recognizeEntries]);
 
 	const handleCancel = useCallback(() => {
-		if(isTouchOrSmall && !isDone) {
+		if (isTouchOrSmall && !isDone) {
 			return;
 		}
 		// if recognition is done, clear the modal and the recognition state when closing
@@ -251,13 +251,13 @@ const MetadataRetrievalModal = () => {
 								</h4>
 							</div>
 							<div className="modal-header-right">
-								{ isDone && (
-								<Button
-									className="btn-link"
-									onClick={handleCancel}
-								>
-									Close
-								</Button>
+								{isDone && (
+									<Button
+										className="btn-link"
+										onClick={handleCancel}
+									>
+										Close
+									</Button>
 								)}
 							</div>
 						</Fragment>
@@ -296,7 +296,7 @@ const MetadataRetrievalModal = () => {
 					containerClassName="recognize-list"
 					listItemComponent={MetadataRetrievalListItem}
 					rowComponent={MetadataRetrievalTableRow}
-					{ ...sharedProps }
+					{...sharedProps}
 				/>
 			</div>
 		</Modal>

src/js/reducers/libraries/data-objects.js
@@ -172,9 +172,11 @@ const dataObjects = (state = {}, action, { bestAttachmentReverseLookup, meta, ta
 					md5: action.response.getData().md5sum,
 					[Symbol.for('links')]: {
 						...(state[action.itemKey][Symbol.for('links')] || {}),
-						//@TODO: could copy actual value from action.response.registerResponse for responses with register step
-						//		 (i.e. where file did not exist before)
-						enclosure: true
+						enclosure: {
+							length: action.response.getData().fileSize,
+							title: action.response.getData().fileName,
+							type: action.fileData.contentType,
+						}
 					}
 				}
 			}