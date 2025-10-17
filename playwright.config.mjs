48f57a3a5e164b727e077496c547bf913aef55f2
Tom Najdek <tom@doppnet.com>
2025-10-17 13:41:30 +0200

    Reduce Playwright screenshot test flakiness

--- playwright.config.mjs
+++ playwright.config.mjs
@@ -9,13 +9,14 @@ const commonContextDesc = {
 export default defineConfig({
 	testDir: './test/playwright',
 	timeout: (process.env.CI ? 30 : 10) * 1000,
-	retries: process.env.CI ? 2 : 0,
+	retries: process.env.CI ? 3 : 0,
 	workers: process.env.CI ? 1 : undefined,
 	outputDir: './playwright',
 	fullyParallel: true,
 	expect: {
 		toHaveScreenshot: {
-			maxDiffPixelRatio: 0.01,
+			maxDiffPixelRatio: 0.02,
+			threshold: 0.3,
 			animations: 'disabled'
 		}
 	},
