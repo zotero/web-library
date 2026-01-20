zotero/web-library

commit 6bc5619a5571f0666b9eb0bc84ceb06a8844e81e

Fix emoji flags aren't recognized in items table #643

src/js/common/item.js
@@ -1,6 +1,6 @@
 import { getZotero } from 'web-common/zotero';
 
-import { cleanURL, get, containsEmoji, extractEmoji } from '../utils';
+import { cleanURL, get, extractEmoji } from '../utils';
 import { dateLocalized } from './format';
 import { itemTypesWithIcons } from '../../../data/item-types-with-icons.json';
 
@@ -147,8 +147,9 @@ const getDerivedData = (mappings, item, itemTypes, tagColors) => {
 		if(!(item?.tags ?? []).some(({ tag }) => tag === name)) {
 			return;
 		}
-		if (containsEmoji(name)) {
-			emojis.push(extractEmoji(name));
+		const emoji = extractEmoji(name);
+		if (emoji.length > 0) {
+			emojis.push(emoji);
 		} else {
 			colors.push(color);
 		}
@@ -156,7 +157,7 @@ const getDerivedData = (mappings, item, itemTypes, tagColors) => {
 
 	// non-colored tags containing emoji, sorted alphabetically (item.tags should already be sorted)
 	(item?.tags ?? []).forEach(({ tag }) => {
-		if (!(tag in tagColors.lookup) && containsEmoji(tag)) {
+		if (!(tag in tagColors.lookup)) {
 			emojis.push(extractEmoji(tag));
 		}
 	});

src/js/utils.js
@@ -517,19 +517,25 @@ const getPrevSibling = (elem, selector) => {
 	}
 };
 
-// https://github.com/zotero/zotero/blob/214a668286d35a630db293bb835f544693698297/chrome/content/zotero/xpcom/utilities_internal.js#L399-L408
-const containsEmoji = str => {
-	let re = /\p{Extended_Pictographic}/gu;
-	return !!str.match(re);
-}
 
-// https://github.com/abaevbog/zotero/blob/f8fd90945663d4745306d88be298633f8c7229de/chrome/content/zotero/xpcom/data/tags.js#L1000
+// https://github.com/zotero/zotero/blob/d1d4065dae77786cf3312d88f86502182a63c3ee/chrome/content/zotero/xpcom/data/tags.js#L844
 const extractEmoji = str => {
-	// Split by anything that is not an emoji, Zero Width Joiner, or Variation Selector-16
-	// And return first continuous span of emojis
-	let re = /[^\p{Extended_Pictographic}\u200D\uFE0F]+/gu; //eslint-disable-line no-misleading-character-class
-	return str.split(re).filter(Boolean)[0] || null;
+	try {
+		// Either match RGI_Emoji (which includes country flags) or any character followed by the Variation Selector-16
+		// Requires "v" flag which is only available in modern browsers
+		let re = new RegExp(String.raw`(?:\p{RGI_Emoji}(?!\uFE0F)|.\uFE0F)+`, "gv");
+		return str.match(re)?.[0] ?? [];
+	} catch {
+		// Fallback for older browsers; will not extract country flags
+		let re = new RegExp(String.raw`(?:\p{Extended_Pictographic}(?!\uFE0F)|.\uFE0F)+`, "gu");
+		return str.match(re)?.[0] ?? [];
+	}
 };
+
+const containsEmoji = str => {
+	let emoji = extractEmoji(str);
+	return emoji.length > 0;
+}
 
 const isReaderCompatibleBrowser = () => {
 	if (typeof structuredClone !== "function") {
@@ -581,55 +587,55 @@ const alwaysTrue = () => true;
 
 export {
 	alwaysTrue,
-    applyChangesToVisibleColumns,
+	applyChangesToVisibleColumns,
 	binarySearch,
-    cede,
-    clamp,
-    cleanURL,
-    compare,
-    compareItem,
+	cede,
+	clamp,
+	cleanURL,
+	compare,
+	compareItem,
 	containsEmoji,
-    deduplicate,
-    deduplicateByHash,
-    deduplicateByKey,
-    enumerateObjects,
+	deduplicate,
+	deduplicateByHash,
+	deduplicateByKey,
+	enumerateObjects,
 	extractEmoji,
-    get,
-    getAbortController,
-    getDOIURL,
-    getFieldNameFromSortKey,
-    getItemCanonicalUrl,
-    getItemFromCanonicalUrl,
+	get,
+	getAbortController,
+	getDOIURL,
+	getFieldNameFromSortKey,
+	getItemCanonicalUrl,
+	getItemFromCanonicalUrl,
 	getItemFromApiUrl,
-    getLibraryKeyFromTopic,
-    getNextSibling,
-    getPrevSibling,
-    getRequestTypeFromItemsSource,
-    getScrollContainerPageCount,
-    getSortKeyValue,
-    getUniqueId,
-    indexByGeneratedKey,
-    indexByKey,
-    isLikeURL,
+	getLibraryKeyFromTopic,
+	getNextSibling,
+	getPrevSibling,
+	getRequestTypeFromItemsSource,
+	getScrollContainerPageCount,
+	getSortKeyValue,
+	getUniqueId,
+	indexByGeneratedKey,
+	indexByKey,
+	isLikeURL,
 	isReaderCompatibleBrowser,
-    JSONTryParse,
-    localStorageWrapper,
+	JSONTryParse,
+	localStorageWrapper,
 	makeRequestsUpTo,
 	mapItemKeysToRelations,
-    mapRelationsToItemKeys,
+	mapRelationsToItemKeys,
 	mergeItemKeysRelations,
 	maxByKey,
-    openDelayedURL,
-    parseBase64File,
-    processIdentifierMultipleItems,
-    removeRelationByItemKey,
-    resizeVisibleColumns,
-    reverseMap,
-    sortByKey,
-    sortItemsByKey,
-    splice,
-    stopPropagation,
-    transform,
+	openDelayedURL,
+	parseBase64File,
+	processIdentifierMultipleItems,
+	removeRelationByItemKey,
+	resizeVisibleColumns,
+	reverseMap,
+	sortByKey,
+	sortItemsByKey,
+	splice,
+	stopPropagation,
+	transform,
 	unescapeHTML,
-    vec2dist
+	vec2dist
 };

commit 11c574adc697b1ef008f375424672b166102de54

Fix unnecessary HTML tag rendered for every tag in the items table.
Regression in 6bc5619a

src/js/common/item.js
@@ -158,7 +158,10 @@ const getDerivedData = (mappings, item, itemTypes, tagColors) => {
 	// non-colored tags containing emoji, sorted alphabetically (item.tags should already be sorted)
 	(item?.tags ?? []).forEach(({ tag }) => {
 		if (!(tag in tagColors.lookup)) {
-			emojis.push(extractEmoji(tag));
+			const emoji = extractEmoji(tag);
+			if( emoji.length > 0) {
+				emojis.push(emoji);
+			}
 		}
 	});
 

src/js/utils.js
@@ -524,11 +524,11 @@ const extractEmoji = str => {
 		// Either match RGI_Emoji (which includes country flags) or any character followed by the Variation Selector-16
 		// Requires "v" flag which is only available in modern browsers
 		let re = new RegExp(String.raw`(?:(?:\p{RGI_Emoji}|\p{Extended_Pictographic})(?!\uFE0F)|.\uFE0F)+`, "gv");
-		return str.match(re)?.[0] ?? [];
+		return str.match(re)?.[0] ?? '';
 	} catch {
 		// Fallback for older browsers; will not extract country flags
 		let re = new RegExp(String.raw`(?:\p{Extended_Pictographic}(?!\uFE0F)|.\uFE0F)+`, "gu");
-		return str.match(re)?.[0] ?? [];
+		return str.match(re)?.[0] ?? '';
 	}
 };
 